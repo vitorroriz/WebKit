vitorroriz/WebKit

commit 4433278ba6e8807bab110a7ebf65b27e74351bc5

Handle setAppBadge in webpushd rdar://131363431 https://bugs.webkit.org/show_bug.cgi?id=277526
Reviewed by Ben Nham.

A lot of message plumbing to get the "set app badge" logic into webpushd,
alongside other notifications-related bits.

* Source/WebKit/Configurations/WebKit.xcconfig:
* Source/WebKit/NetworkProcess/NetworkProcess.cpp:
(WebKit::NetworkProcess::getAppBadgeForTesting):
* Source/WebKit/NetworkProcess/NetworkProcess.h:
* Source/WebKit/NetworkProcess/NetworkProcess.messages.in:

* Source/WebKit/NetworkProcess/Notifications/NetworkNotificationManager.cpp:
(WebKit::NetworkNotificationManager::setAppBadge):
(WebKit::NetworkNotificationManager::getAppBadgeForTesting):
* Source/WebKit/NetworkProcess/Notifications/NetworkNotificationManager.h:

* Source/WebKit/Platform/spi/ios/UIKitSPI.h:
* Source/WebKit/Resources/SandboxProfiles/ios/com.apple.WebKit.webpushd.sb.in:
* Source/WebKit/Scripts/process-entitlements.sh:

* Source/WebKit/Shared/Notifications/NotificationManagerMessageHandler.h:
* Source/WebKit/Shared/Notifications/NotificationManagerMessageHandler.messages.in:

* Source/WebKit/UIProcess/API/Cocoa/WKWebsiteDataStore.mm:
(-[WKWebsiteDataStore _getAppBadgeForTesting:]):
* Source/WebKit/UIProcess/API/Cocoa/WKWebsiteDataStorePrivate.h:

* Source/WebKit/UIProcess/Notifications/ServiceWorkerNotificationHandler.h:
* Source/WebKit/UIProcess/Notifications/WebNotificationManagerMessageHandler.h:

* Source/WebKit/UIProcess/WebsiteData/WebsiteDataStore.cpp:
(WebKit::WebsiteDataStore::getAppBadgeForTesting):
* Source/WebKit/UIProcess/WebsiteData/WebsiteDataStore.h:

* Source/WebKit/WebProcess/WebProcess.cpp:
(WebKit::WebProcess::setAppBadge):

* Source/WebKit/webpushd/PushClientConnection.h:
* Source/WebKit/webpushd/PushClientConnection.messages.in:
* Source/WebKit/webpushd/PushClientConnection.mm:
(WebPushD::PushClientConnection::setAppBadge):
(WebPushD::PushClientConnection::getAppBadgeForTesting):

* Source/WebKit/webpushd/WebPushDaemon.h:

Source/WebKit/webpushd/WebPushDaemon.mm
@@ -40,6 +40,7 @@
 #import <WebCore/LocalizedStrings.h>
 #import <WebCore/NotificationData.h>
 #import <WebCore/NotificationPayload.h>
+#import <WebCore/SecurityOrigin.h>
 #import <WebCore/SecurityOriginData.h>
 #import <pal/spi/cocoa/LaunchServicesSPI.h>
 #import <span>
@@ -791,6 +792,63 @@ void WebPushDaemon::requestPushPermission(PushClientConnection& connection, cons
     });
 
     [center requestAuthorizationWithOptions:options completionHandler:blockPtr.get()];
+}
+
+void WebPushDaemon::setAppBadge(PushClientConnection& connection, WebCore::SecurityOriginData&& badgeOriginData, std::optional<uint64_t> appBadge)
+{
+    URL appPageURL;
+
+#if PLATFORM(IOS)
+    auto webClipIdentifier = connection.pushPartitionString();
+    RetainPtr webClip = [UIWebClip webClipWithIdentifier:(NSString *)webClipIdentifier];
+    appPageURL = [webClip pageURL];
+#elif PLATFORM(MAC)
+    // FIXME: Establish the app page URL for Mac apps here
+#endif
+
+    if (!appPageURL.isEmpty()) {
+        auto badgeOrigin = badgeOriginData.securityOrigin();
+        auto appOrigin = WebCore::SecurityOrigin::create(appPageURL);
+        if (!badgeOrigin->isSameSiteAs(appOrigin.get()))
+            return;
+    }
+
+#if PLATFORM(IOS)
+    RetainPtr state = adoptNS([[UISApplicationState alloc] initWithBundleIdentifier:platformNotificationCenterBundleIdentifier(connection).get()]);
+    state.get().badgeValue = appBadge ? [NSNumber numberWithUnsignedLongLong:*appBadge] : nil;
+#elif PLATFORM(MAC)
+    String bundleIdentifier = connection.pushPartitionString().isEmpty() ? connection.hostAppCodeSigningIdentifier() : connection.pushPartitionString();
+    RetainPtr center = adoptNS([[m_userNotificationCenterClass alloc]  initWithBundleIdentifier:(NSString *)bundleIdentifier]);
+    if (!center)
+        return;
+
+    UNMutableNotificationContent *content = [UNMutableNotificationContent new];
+    content.badge = appBadge ? [NSNumber numberWithLongLong:*appBadge] : nil;
+    UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:NSUUID.UUID.UUIDString content:content trigger:nil];
+    [center addNotificationRequest:request withCompletionHandler:^(NSError *error) {
+        if (error)
+            WEBPUSHDAEMON_RELEASE_LOG_ERROR(Push, "Error attempting to set badge count for web app %s", connection.pushPartitionString().utf8().data());
+    }];
+#endif // PLATFORM(MAC)
+}
+
+void WebPushDaemon::getAppBadgeForTesting(PushClientConnection& connection, CompletionHandler<void(std::optional<uint64_t>)>&& completionHandler)
+{
+#if PLATFORM(IOS)
+    UNUSED_PARAM(connection);
+    completionHandler(std::nullopt);
+#else
+    RELEASE_ASSERT(m_userNotificationCenterClass == _WKMockUserNotificationCenter.class);
+
+    String bundleIdentifier = connection.pushPartitionString().isEmpty() ? connection.hostAppCodeSigningIdentifier() : connection.pushPartitionString();
+    RetainPtr center = adoptNS([[_WKMockUserNotificationCenter alloc] initWithBundleIdentifier:(NSString *)bundleIdentifier]);
+    NSNumber *centerBadge = [center getAppBadgeForTesting];
+
+    if (centerBadge)
+        completionHandler([centerBadge unsignedLongLongValue]);
+    else
+        completionHandler(std::nullopt);
+#endif
 }
 
 #endif // HAVE(FULL_FEATURED_USER_NOTIFICATIONS)

Tools/TestWebKitAPI/Tests/WebKitCocoa/WebPushDaemon.mm
@@ -587,6 +587,7 @@ self.addEventListener("push", async (event) => {
     try {
         if (showNotifications) {
             await self.registration.showNotification("notification");
+            navigator.setAppBadge(42);
         }
         if (!event.data) {
             globalPort.postMessage("Received: null data");
@@ -785,6 +786,19 @@ public:
         NSError *error = nil;
         id obj = [m_webView objectByCallingAsyncFunction:@"return await getNotifications()" withArguments:@{ } error:&error];
         return error ?: obj;
+    }
+
+    NSNumber *getAppBadge()
+    {
+        __block bool done = false;
+        __block NSNumber *result = nil;
+        [m_webView.get().configuration.websiteDataStore _getAppBadgeForTesting:^(NSNumber *badge) {
+            result = badge;
+            done = true;
+        }];
+
+        TestWebKitAPI::Util::run(&done);
+        return result;
     }
 
     void closeAllNotifications()
@@ -1598,6 +1612,9 @@ TEST_F(WebPushDBuiltInTest, ShowAndGetNotifications)
     });
     TestWebKitAPI::Util::run(&done);
 
+    // No badge had been set, so confirm its `nil`
+    EXPECT_FALSE(view->getAppBadge());
+
     done = false;
     sender.sendWithAsyncReplyWithoutUsingIPCConnection(Messages::PushClientConnection::InjectPushMessageForTesting(message), ^(const String& error) {
         if (!error.isEmpty())
@@ -1625,6 +1642,10 @@ TEST_F(WebPushDBuiltInTest, ShowAndGetNotifications)
 
     result = view->getNotifications();
     EXPECT_TRUE([result isEqualToString:@""]);
+
+    // The push message handler should set the app badge to 42
+    EXPECT_TRUE([view->getAppBadge() isEqual:@42]);
+
 }
 #endif // HAVE(FULL_FEATURED_USER_NOTIFICATIONS)
 

Source/WebKit/Configurations/WebKit.xcconfig
@@ -248,7 +248,11 @@ WK_USERNOTIFICATIONS_LDFLAGS_iphoneos = -framework UserNotifications;
 WK_USERNOTIFICATIONS_LDFLAGS_iphonesimulator = -framework UserNotifications;
 WK_USERNOTIFICATIONS_LDFLAGS_macosx = -weak_framework UserNotifications;
 
-FRAMEWORK_AND_LIBRARY_LDFLAGS = -lobjc -framework CFNetwork -framework CoreAudio -framework CoreFoundation -framework CoreGraphics -framework CoreText -framework Foundation -framework ImageIO -framework IOKit -framework IOSurface -framework WebKitLegacy -lnetwork -framework Metal $(WK_APPSERVERSUPPORT_LDFLAGS) $(WK_ACCESSIBILITY_LDFLAGS) $(WK_APPKIT_LDFLAGS) $(WK_APPLE_PUSH_SERVICE_LDFLAGS) $(WK_BACKBOARD_SERVICES_LDFLAGS) $(WK_RUNNINGBOARD_SERVICES_LDFLAGS) $(WK_AUTHKIT_LDFLAGS) $(WK_CARBON_LDFLAGS) $(WK_CONTACTS_LDFLAGS) $(WK_CORE_PREDICTION_LDFLAGS) $(WK_CORE_SERVICES_LDFLAGS) $(WK_GRAPHICS_SERVICES_LDFLAGS) $(WK_LIBNETWORKEXTENSION_LDFLAGS) $(WK_LIBSANDBOX_LDFLAGS) $(WK_LIBWEBRTC_LDFLAGS) $(WK_MOBILE_CORE_SERVICES_LDFLAGS) $(WK_MOBILE_GESTALT_LDFLAGS) $(WK_MOBILE_KEY_BAG_LDFLAGS) $(WK_PDFKIT_LDFLAGS) $(WK_PROTOTYPE_TOOLS_LDFLAGS) $(WK_SAFE_BROWSING_LDFLAGS) $(WK_SECURITY_INTERFACE_LDFLAGS) $(WK_UIKIT_LDFLAGS) $(WK_MRUIKIT_LDFLAGS) $(WK_UNIFORM_TYPE_IDENTIFIERS_LDFLAGS) $(WK_URL_FORMATTING_LDFLAGS) $(WK_WEBINSPECTORUI_LDFLAGS) $(WK_COORDINATOR_LDFLAGS) $(WK_INSTALL_COORDINATION_LDFLAGS) $(WK_APP_STORE_DAEMON_LDFLAGS) $(WK_POWERLOG_LDFLAGS) $(WK_FRONTBOARDSERVICES_LDFLAGS) $(WK_LIBBSM_LDFLAGS) $(WK_NETWORK_EXTENSION_LDFLAGS) $(WK_BROWSERENGINEKIT_LDFLAGS) $(WK_WRITING_TOOLS_LDFLAGS) $(WK_USERNOTIFICATIONS_LDFLAGS);
+WK_UIKITSERVICES_LDFLAGS = $(WK_UIKITSERVICES_LDFLAGS_$(WK_PLATFORM_NAME));
+WK_UIKITSERVICES_LDFLAGS_iphoneos = -framework UIKitServices;
+WK_UIKITSERVICES_LDFLAGS_iphonesimulator = -framework UIKitServices;
+
+FRAMEWORK_AND_LIBRARY_LDFLAGS = -lobjc -framework CFNetwork -framework CoreAudio -framework CoreFoundation -framework CoreGraphics -framework CoreText -framework Foundation -framework ImageIO -framework IOKit -framework IOSurface -framework WebKitLegacy -lnetwork -framework Metal $(WK_APPSERVERSUPPORT_LDFLAGS) $(WK_ACCESSIBILITY_LDFLAGS) $(WK_APPKIT_LDFLAGS) $(WK_APPLE_PUSH_SERVICE_LDFLAGS) $(WK_BACKBOARD_SERVICES_LDFLAGS) $(WK_RUNNINGBOARD_SERVICES_LDFLAGS) $(WK_AUTHKIT_LDFLAGS) $(WK_CARBON_LDFLAGS) $(WK_CONTACTS_LDFLAGS) $(WK_CORE_PREDICTION_LDFLAGS) $(WK_CORE_SERVICES_LDFLAGS) $(WK_GRAPHICS_SERVICES_LDFLAGS) $(WK_LIBNETWORKEXTENSION_LDFLAGS) $(WK_LIBSANDBOX_LDFLAGS) $(WK_LIBWEBRTC_LDFLAGS) $(WK_MOBILE_CORE_SERVICES_LDFLAGS) $(WK_MOBILE_GESTALT_LDFLAGS) $(WK_MOBILE_KEY_BAG_LDFLAGS) $(WK_PDFKIT_LDFLAGS) $(WK_PROTOTYPE_TOOLS_LDFLAGS) $(WK_SAFE_BROWSING_LDFLAGS) $(WK_SECURITY_INTERFACE_LDFLAGS) $(WK_UIKIT_LDFLAGS) $(WK_MRUIKIT_LDFLAGS) $(WK_UNIFORM_TYPE_IDENTIFIERS_LDFLAGS) $(WK_URL_FORMATTING_LDFLAGS) $(WK_WEBINSPECTORUI_LDFLAGS) $(WK_COORDINATOR_LDFLAGS) $(WK_INSTALL_COORDINATION_LDFLAGS) $(WK_APP_STORE_DAEMON_LDFLAGS) $(WK_POWERLOG_LDFLAGS) $(WK_FRONTBOARDSERVICES_LDFLAGS) $(WK_LIBBSM_LDFLAGS) $(WK_NETWORK_EXTENSION_LDFLAGS) $(WK_BROWSERENGINEKIT_LDFLAGS) $(WK_WRITING_TOOLS_LDFLAGS) $(WK_USERNOTIFICATIONS_LDFLAGS) $(WK_UIKITSERVICES_LDFLAGS);
 
 // Prevent C++ standard library basic_stringstream, operator new, delete and their related exception types from being exported as weak symbols.
 UNEXPORTED_SYMBOL_LDFLAGS = -Wl,-unexported_symbol -Wl,__ZTISt9bad_alloc -Wl,-unexported_symbol -Wl,__ZTISt9exception -Wl,-unexported_symbol -Wl,__ZTSSt9bad_alloc -Wl,-unexported_symbol -Wl,__ZTSSt9exception -Wl,-unexported_symbol -Wl,__ZdlPvS_ -Wl,-unexported_symbol -Wl,__ZnwmPv -Wl,-unexported_symbol -Wl,__Znwm -Wl,-unexported_symbol -Wl,__ZTVNSt3__117bad_function_callE -Wl,-unexported_symbol -Wl,__ZTCNSt3__118basic_stringstreamIcNS_11char_traitsIcEENS_9allocatorIcEEEE0_NS_13basic_istreamIcS2_EE -Wl,-unexported_symbol -Wl,__ZTCNSt3__118basic_stringstreamIcNS_11char_traitsIcEENS_9allocatorIcEEEE0_NS_14basic_iostreamIcS2_EE -Wl,-unexported_symbol -Wl,__ZTCNSt3__118basic_stringstreamIcNS_11char_traitsIcEENS_9allocatorIcEEEE16_NS_13basic_ostreamIcS2_EE -Wl,-unexported_symbol -Wl,__ZTTNSt3__118basic_stringstreamIcNS_11char_traitsIcEENS_9allocatorIcEEEE -Wl,-unexported_symbol -Wl,__ZTVNSt3__115basic_stringbufIcNS_11char_traitsIcEENS_9allocatorIcEEEE -Wl,-unexported_symbol -Wl,__ZTVNSt3__118basic_stringstreamIcNS_11char_traitsIcEENS_9allocatorIcEEEE -Wl,-unexported_symbol -Wl,__ZTCNSt3__118basic_stringstreamIcNS_11char_traitsIcEENS_9allocatorIcEEEE8_NS_13basic_ostreamIcS2_EE;

Source/WebKit/NetworkProcess/NetworkProcess.cpp
@@ -2626,6 +2626,18 @@ void NetworkProcess::hasPushSubscriptionForTesting(PAL::SessionID sessionID, URL
     callback(false);
 }
 
+void NetworkProcess::getAppBadgeForTesting(PAL::SessionID sessionID, CompletionHandler<void(std::optional<uint64_t>)>&& callback)
+{
+#if ENABLE(WEB_PUSH_NOTIFICATIONS)
+    if (auto* session = networkSession(sessionID)) {
+        session->notificationManager().getAppBadgeForTesting(WTFMove(callback));
+        return;
+    }
+#endif
+
+    callback(std::nullopt);
+}
+
 #if ENABLE(INSPECTOR_NETWORK_THROTTLING)
 
 void NetworkProcess::setEmulatedConditions(PAL::SessionID sessionID, std::optional<int64_t>&& bytesPerSecondLimit)

Source/WebKit/NetworkProcess/NetworkProcess.h
@@ -425,6 +425,8 @@ public:
     void setStorageSiteValidationEnabled(PAL::SessionID, bool);
     void setPersistedDomains(PAL::SessionID, HashSet<RegistrableDomain>&&);
 
+    void getAppBadgeForTesting(PAL::SessionID, CompletionHandler<void(std::optional<uint64_t>)>&&);
+
 private:
     // CheckedPtr interface
     uint32_t ptrCount() const final { return CanMakeCheckedPtr::ptrCount(); }

Source/WebKit/NetworkProcess/NetworkProcess.messages.in
@@ -246,4 +246,6 @@ messages -> NetworkProcess LegacyReceiver {
 
     SetStorageSiteValidationEnabled(PAL::SessionID sessionID, bool enabled)
     SetPersistedDomains(PAL::SessionID sessionsID, HashSet<WebCore::RegistrableDomain> domains)
+
+    GetAppBadgeForTesting(PAL::SessionID sessionID) -> (std::optional<uint64_t> badge)
 }

Source/WebKit/NetworkProcess/Notifications/NetworkNotificationManager.cpp
@@ -136,6 +136,14 @@ void NetworkNotificationManager::requestPermission(WebCore::SecurityOriginData&&
     m_connection->sendWithAsyncReplyWithoutUsingIPCConnection(Messages::PushClientConnection::RequestPushPermission { WTFMove(origin) }, WTFMove(completionHandler));
 }
 
+void NetworkNotificationManager::setAppBadge(const WebCore::SecurityOriginData& origin, std::optional<uint64_t> badge)
+{
+    if (!m_connection)
+        return;
+
+    m_connection->sendWithoutUsingIPCConnection(Messages::PushClientConnection::SetAppBadge { origin, badge });
+}
+
 void NetworkNotificationManager::subscribeToPushService(URL&& scopeURL, Vector<uint8_t>&& applicationServerKey, CompletionHandler<void(Expected<WebCore::PushSubscriptionData, WebCore::ExceptionData>&&)>&& completionHandler)
 {
     if (!m_connection) {
@@ -216,6 +224,16 @@ void NetworkNotificationManager::removePushSubscriptionsForOrigin(WebCore::Secur
     }
 
     m_connection->sendWithAsyncReplyWithoutUsingIPCConnection(Messages::PushClientConnection::RemovePushSubscriptionsForOrigin(WTFMove(origin)), WTFMove(completionHandler));
+}
+
+void NetworkNotificationManager::getAppBadgeForTesting(CompletionHandler<void(std::optional<uint64_t>)>&& completionHandler)
+{
+    if (!m_connection) {
+        completionHandler(std::nullopt);
+        return;
+    }
+
+    m_connection->sendWithAsyncReplyWithoutUsingIPCConnection(Messages::PushClientConnection::GetAppBadgeForTesting(), WTFMove(completionHandler));
 }
 
 } // namespace WebKit

Source/WebKit/NetworkProcess/Notifications/NetworkNotificationManager.h
@@ -68,6 +68,8 @@ public:
 
     void getNotifications(const URL& registrationURL, const String& tag, CompletionHandler<void(Expected<Vector<WebCore::NotificationData>, WebCore::ExceptionData>&&)>&&);
 
+    void getAppBadgeForTesting(CompletionHandler<void(std::optional<uint64_t>)>&&);
+
 private:
     NetworkNotificationManager(NetworkSession&, const String& webPushMachServiceName, WebPushD::WebPushDaemonConnectionConfiguration&&);
 
@@ -77,6 +79,7 @@ private:
     void didDestroyNotification(const WTF::UUID& notificationID) final;
     void pageWasNotifiedOfNotificationPermission() final { }
     void requestPermission(WebCore::SecurityOriginData&&, CompletionHandler<void(bool)>&&) final;
+    void setAppBadge(const WebCore::SecurityOriginData&, std::optional<uint64_t> badge) final;
 
     NetworkSession& m_networkSession;
     std::unique_ptr<WebPushD::Connection> m_connection;

commit 80de1e713f2fc6444ebf77dcfa28d348c26e2f59

Leak of UNMutableNotificationContent in WebPushD::WebPushDaemon::setAppBadge() <https://bugs.webkit.org/show_bug.cgi?id=300171> <rdar://161957380>
Reviewed by Chris Dumez.

Regressed in 281776@main.

* Source/WebKit/webpushd/WebPushDaemon.mm:
(WebPushD::WebPushDaemon::setAppBadge):
- Use RetainPtr/adoptNS() to fix leak.

Canonical link: https://commits.webkit.org/301025@main

Source/WebKit/webpushd/WebPushDaemon.mm
@@ -1237,9 +1237,9 @@ void WebPushDaemon::setAppBadge(PushClientConnection& connection, WebCore::Secur
     if (!center)
         return;
 
-    UNMutableNotificationContent *content = [UNMutableNotificationContent new];
-    content.badge = appBadge ? [NSNumber numberWithLongLong:*appBadge] : nil;
-    RetainPtr request = [UNNotificationRequest requestWithIdentifier:NSUUID.UUID.UUIDString content:content trigger:nil];
+    RetainPtr content = adoptNS([UNMutableNotificationContent new]);
+    content.get().badge = appBadge ? [NSNumber numberWithLongLong:*appBadge] : nil;
+    RetainPtr request = [UNNotificationRequest requestWithIdentifier:NSUUID.UUID.UUIDString content:content.get() trigger:nil];
     RetainPtr debugDescription = identifier.debugDescription().createNSString().get();
     [center addNotificationRequest:request.get() withCompletionHandler:^(NSError *error) {
         if (error) {