23b03092aa6d3c8805bc743ae46964eb0e9dea6a
Commit Queue <commit-queue@webkit.org>
2025-10-07 02:06:36 -0700

    Unreviewed, reverting 300866@main. https://bugs.webkit.org/show_bug.cgi?id=300304
    since it triggers a PLT regression
    
    Reverted changeset:
    
    "Crash under VTIsHardwareDecodeSupported"
    
    https://bugs.webkit.org/show_bug.cgi?id=299949
    https://commits.webkit.org/300866@main
    Canonical link: https://commits.webkit.org/301122@main

--- Source/WebCore/platform/graphics/cocoa/VP9UtilitiesCocoa.mm
+++ Source/WebCore/platform/graphics/cocoa/VP9UtilitiesCocoa.mm
@@ -196,12 +196,7 @@ bool vp9HardwareDecoderAvailable()
     if (auto vp9HardwareDecoderOverride = VP9TestingOverrides::singleton().vp9HardwareDecoderEnabledOverride())
         return *vp9HardwareDecoderOverride;
 
-    static dispatch_once_t onceToken;
-    static bool decoderAvailable;
-    dispatch_once(&onceToken, ^{
-        decoderAvailable = canLoad_VideoToolbox_VTIsHardwareDecodeSupported() && VTIsHardwareDecodeSupported(kCMVideoCodecType_VP9);
-    });
-    return decoderAvailable;
+    return canLoad_VideoToolbox_VTIsHardwareDecodeSupported() && VTIsHardwareDecodeSupported(kCMVideoCodecType_VP9);
 }
 
 bool vp9HardwareDecoderAvailableInProcess()
@@ -209,12 +204,7 @@ bool vp9HardwareDecoderAvailableInProcess()
     if (auto disabledForTesting = VP9TestingOverrides::singleton().hardwareDecoderDisabled())
         return !*disabledForTesting;
 
-    static dispatch_once_t onceToken;
-    static bool decoderAvailable;
-    dispatch_once(&onceToken, ^{
-        decoderAvailable = canLoad_VideoToolbox_VTIsHardwareDecodeSupported() && VTIsHardwareDecodeSupported(kCMVideoCodecType_VP9);
-    });
-    return decoderAvailable;
+    return canLoad_VideoToolbox_VTIsHardwareDecodeSupported() && VTIsHardwareDecodeSupported(kCMVideoCodecType_VP9);
 }
 
 static bool isVP9CodecConfigurationRecordSupported(const VPCodecConfigurationRecord& codecConfiguration)
