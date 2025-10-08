7f2816ca93ddf9c4ebd7bd9a6a7135e47e241e6b
Basuke Suzuki <basuke@apple.com>
2025-10-08 10:14:30 -0700

    Reverting 299282@main (f1e55dd8fe6f) https://bugs.webkit.org/show_bug.cgi?id=298244 rdar://159679987
    Reviewed by Sihui Liu.
    
    The commit changed the timing of the test code execution from after window.load to
    before. That confused the RemoteLayerTreeDrawingArea's IPC orders and made this test
    failed flakily.
    
    [ iOS ] imported/w3c/web-platform-tests/css/css-scroll-anchoring/shadow-dom-subscroller.html is a constant failure
    
    Reverted the change.
    
    * LayoutTests/TestExpectations:
    * LayoutTests/http/wpt/dom/inline-module-script-timing-expected.txt: Removed.
    * LayoutTests/http/wpt/dom/inline-module-script-timing.html: Removed.
    * LayoutTests/http/wpt/dom/inline-module-script-with-imports-expected.txt: Removed.
    * LayoutTests/http/wpt/dom/inline-module-script-with-imports.html: Removed.
    * LayoutTests/http/wpt/dom/resources/delayed-module.js: Removed.
    * LayoutTests/imported/w3c/web-platform-tests/html/semantics/scripting-1/the-script-element/microtasks/evaluation-order-2-expected.txt:
    * LayoutTests/imported/w3c/web-platform-tests/navigation-api/navigate-event/cross-origin-traversal-redirect-expected.txt:
    * LayoutTests/platform/ios/TestExpectations:
    * Source/WebCore/html/parser/HTMLDocumentParser.cpp:
    (WebCore::HTMLDocumentParser::notifyFinished):
    
    Canonical link: https://commits.webkit.org/301215@main

--- LayoutTests/http/wpt/dom/resources/delayed-module.js
+++
@@ -1,8 +0,0 @@
-// This module simulates a delayed import that might take time to load
-// to test that document readyState waits for all imports to complete
-
-console.log('delayed-module.js loading...');
-
-export const delayedExport = "delayed-value";
-
-console.log(`delayed-module.js loaded with value: ${delayedExport}`);
