7bc69ce3ea264109df8b1533a927563840b7cf2a
Kimmo Kinnunen <kkinnunen@apple.com>
2025-10-10 03:40:33 -0700

    GCGLErrorCode flags value 2 is unused https://bugs.webkit.org/show_bug.cgi?id=300495 rdar://162351187
    Reviewed by Dan Glastonbury.
    
    Make the GCGLErrorCode values tightly packed by using the value 2 and
    adjusting subsequent values.
    
    * Source/WebCore/platform/graphics/GraphicsTypesGL.h:
    
    Canonical link: https://commits.webkit.org/301311@main

--- Source/WebCore/platform/graphics/GraphicsTypesGL.h
+++ Source/WebCore/platform/graphics/GraphicsTypesGL.h
@@ -82,10 +82,10 @@ using GCGLMTLRasterizationRateMapANGLE = void*;
 // Order in inverse of in GL specification, so that iteration is in GL specification order.
 enum class GCGLErrorCode : uint8_t {
     ContextLost = 1,
-    InvalidFramebufferOperation = 1 << 2,
-    OutOfMemory = 1 << 3,
-    InvalidOperation = 1 << 4,
-    InvalidValue = 1 << 5,
-    InvalidEnum = 1 << 6
+    InvalidFramebufferOperation = 1 << 1,
+    OutOfMemory = 1 << 2,
+    InvalidOperation = 1 << 3,
+    InvalidValue = 1 << 4,
+    InvalidEnum = 1 << 5
 };
 using GCGLErrorCodeSet = OptionSet<GCGLErrorCode>;
