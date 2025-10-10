vitorroriz/WebKit

commit 9f0d6c1f3935637aa86546cc0643676f0b34af41

Add SPI for connecting to webpushd https://bugs.webkit.org/show_bug.cgi?id=279142 rdar://135297138
Reviewed by Ben Nham.

This will make it easier to test webpushd functionalities.

Tests: WebPushD.WKWebPushDaemonConnectionRequestPushPermission
       WebPushD.WKWebPushDaemonConnectionPushSubscription

* Source/WebKit/Shared/API/APIObject.h:
* Source/WebKit/Shared/API/Cocoa/WebKitPrivate.h:
* Source/WebKit/Shared/Cocoa/APIObject.mm:
(API::Object::newObject):
* Source/WebKit/Sources.txt:
* Source/WebKit/SourcesCocoa.txt:
* Source/WebKit/UIProcess/API/APIWebPushDaemonConnection.cpp: Added.
(API::WebPushDaemonConnection::WebPushDaemonConnection):
(API::WebPushDaemonConnection::getPushPermissionState):
(API::WebPushDaemonConnection::requestPushPermission):
(API::WebPushDaemonConnection::setAppBadge):
(API::WebPushDaemonConnection::subscribeToPushService):
(API::WebPushDaemonConnection::unsubscribeFromPushService):
(API::WebPushDaemonConnection::getPushSubscription):
(API::WebPushDaemonConnection::getNextPendingPushMessage):
* Source/WebKit/UIProcess/API/APIWebPushDaemonConnection.h: Added.
* Source/WebKit/UIProcess/API/APIWebPushMessage.h: Copied from Source/WebKit/Shared/API/Cocoa/WebKitPrivate.h.
* Source/WebKit/UIProcess/API/APIWebPushSubscriptionData.h: Copied from Source/WebKit/Shared/API/Cocoa/WebKitPrivate.h.
* Source/WebKit/UIProcess/API/Cocoa/_WKWebPushDaemonConnection.h: Added.
* Source/WebKit/UIProcess/API/Cocoa/_WKWebPushDaemonConnection.mm: Added.
(-[_WKWebPushDaemonConnectionConfiguration init]):
(-[_WKWebPushDaemonConnection initWithConfiguration:]):
(toWKPermissionsState):
(-[_WKWebPushDaemonConnection getPushPermissionStateForOrigin:completionHandler:]):
(-[_WKWebPushDaemonConnection requestPushPermissionForOrigin:completionHandler:]):
(-[_WKWebPushDaemonConnection setAppBadge:origin:]):
(-[_WKWebPushDaemonConnection subscribeToPushServiceForScope:applicationServerKey:completionHandler:]):

Source/WebKit/UIProcess/API/Cocoa/_WKWebPushDaemonConnection.h
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2024 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#import <WebKit/WKFoundation.h>
+
+typedef NS_ENUM(NSInteger, _WKWebPushPermissionState) {
+    _WKWebPushPermissionStateDenied,
+    _WKWebPushPermissionStateGranted,
+    _WKWebPushPermissionStatePrompt,
+};
+
+@class _WKWebPushMessage;
+@class _WKWebPushSubscriptionData;
+
+NS_ASSUME_NONNULL_BEGIN
+
+WK_EXTERN
+@interface _WKWebPushDaemonConnectionConfiguration : NSObject
+
+- (instancetype)init;
+@property (nonatomic, copy) NSString *machServiceName;
+@property (nonatomic, copy) NSString *partition;
+@property (nonatomic, copy) NSString *bundleIdentifier;
+
+@end
+
+WK_EXTERN
+@interface _WKWebPushDaemonConnection : NSObject
+
++ (instancetype)new NS_UNAVAILABLE;
+- (instancetype)init NS_UNAVAILABLE;
+- (instancetype)initWithConfiguration:(_WKWebPushDaemonConnectionConfiguration *)configuration;
+
+- (void)getPushPermissionStateForOrigin:(NSURL *)origin completionHandler:(void (^)(_WKWebPushPermissionState))completionHandler;
+- (void)requestPushPermissionForOrigin:(NSURL *)origin completionHandler:(void (^)(BOOL))completionHandler;
+- (void)setAppBadge:(NSUInteger *)badge origin:(NSURL *)origin;
+- (void)subscribeToPushServiceForScope:(NSURL *)scopeURL applicationServerKey:(NSData *)key completionHandler:(void (^)(_WKWebPushSubscriptionData *, NSError *))completionHandler;
+- (void)unsubscribeFromPushServiceForScope:(NSURL *)scopeURL completionHandler:(void (^)(BOOL unsubscribed, NSError *))completionHandler;
+- (void)getSubscriptionForScope:(NSURL *)scopeURL completionHandler:(void (^)(_WKWebPushSubscriptionData *, NSError *))completionHandler;
+- (void)getNextPendingPushMessage:(void (^)(_WKWebPushMessage *))completionHandler;
+
+@end
+
+NS_ASSUME_NONNULL_END

Source/WebKit/UIProcess/API/Cocoa/_WKWebPushDaemonConnection.mm
@@ -1,0 +1,158 @@
+/*
+ * Copyright (C) 2024 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#import "config.h"
+#import "_WKWebPushDaemonConnectionInternal.h"
+
+#import "APIWebPushMessage.h"
+#import "APIWebPushSubscriptionData.h"
+#import "WKError.h"
+#import "_WKWebPushSubscriptionDataInternal.h"
+#import <WebCore/ExceptionData.h>
+#import <WebCore/PushPermissionState.h>
+#import <wtf/BlockPtr.h>
+#import <wtf/CompletionHandler.h>
+#import <wtf/RetainPtr.h>
+#import <wtf/cocoa/VectorCocoa.h>
+
+@implementation _WKWebPushDaemonConnectionConfiguration
+
+- (instancetype)init
+{
+    self = [super init];
+    if (!self)
+        return nil;
+
+#if ENABLE(RELOCATABLE_WEBPUSHD)
+    _machServiceName = @"com.apple.webkit.webpushd.relocatable.service";
+#else
+    _machServiceName = @"com.apple.webkit.webpushd.service";
+#endif
+    return self;
+}
+
+@end
+
+@implementation _WKWebPushDaemonConnection
+
+- (instancetype)initWithConfiguration:(_WKWebPushDaemonConnectionConfiguration *)configuration
+{
+    if (!(self = [super init]))
+        return nil;
+
+    API::Object::constructInWrapper<API::WebPushDaemonConnection>(self, configuration.machServiceName, configuration.partition, configuration.bundleIdentifier);
+
+    return self;
+}
+
+static _WKWebPushPermissionState toWKPermissionsState(WebCore::PushPermissionState state)
+{
+    switch (state) {
+    case WebCore::PushPermissionState::Denied:
+        return _WKWebPushPermissionStateDenied;
+    case WebCore::PushPermissionState::Granted:
+        return _WKWebPushPermissionStateGranted;
+    case WebCore::PushPermissionState::Prompt:
+        return _WKWebPushPermissionStatePrompt;
+    }
+
+    return _WKWebPushPermissionStateDenied;
+}
+
+- (void)getPushPermissionStateForOrigin:(NSURL *)originURL completionHandler:(void (^)(_WKWebPushPermissionState))completionHandler
+{
+    _connection->getPushPermissionState(originURL, [completionHandlerCopy = makeBlockPtr(completionHandler)](auto result) {
+        completionHandlerCopy(toWKPermissionsState(result));
+    });
+}
+
+- (void)requestPushPermissionForOrigin:(NSURL *)originURL completionHandler:(void (^)(BOOL))completionHandler
+{
+    _connection->requestPushPermission(originURL, [completionHandlerCopy = makeBlockPtr(completionHandler)] (bool result) {
+        completionHandlerCopy(result);
+    });
+}
+
+- (void)setAppBadge:(NSUInteger *)badge origin:(NSURL *)originURL
+{
+    std::optional<uint64_t> badgeValue = badge ? std::optional<uint64_t> { (uint64_t)badge } : std::nullopt;
+    _connection->setAppBadge(originURL, badgeValue);
+}
+
+- (void)subscribeToPushServiceForScope:(NSURL *)scopeURL applicationServerKey:(NSData *)applicationServerKey completionHandler:(void (^)(_WKWebPushSubscriptionData *, NSError *))completionHandler
+{
+    auto key = makeVector(applicationServerKey);
+    _connection->subscribeToPushService(scopeURL, WTFMove(key), [completionHandlerCopy = makeBlockPtr(completionHandler)] (auto result) {
+        if (result)
+            return completionHandlerCopy(wrapper(API::WebPushSubscriptionData::create(WTFMove(result.value()))).get(), nil);
+
+        // FIXME: This error can be used to create DOMException; we may consider adding a new value to WKErrorCode for it.
+        auto error = [NSError errorWithDomain:@"WKErrorDomain" code:WKErrorUnknown userInfo:@{ NSLocalizedDescriptionKey:result.error().message }];
+        completionHandlerCopy(nil, error);
+    });
+}
+
+- (void)unsubscribeFromPushServiceForScope:(NSURL *)scopeURL completionHandler:(void (^)(BOOL unsubscribed, NSError *))completionHandler
+{
+    _connection->unsubscribeFromPushService(scopeURL, [completionHandlerCopy = makeBlockPtr(completionHandler)] (auto result) {
+        if (result)
+            return completionHandlerCopy(result.value(), nil);
+
+        auto error = [NSError errorWithDomain:@"WKErrorDomain" code:WKErrorUnknown userInfo:@{ NSLocalizedDescriptionKey:result.error().message }];
+        completionHandlerCopy(false, error);
+    });
+}
+
+- (void)getSubscriptionForScope:(NSURL *)scopeURL completionHandler:(void (^)(_WKWebPushSubscriptionData *, NSError *))completionHandler
+{
+    _connection->getPushSubscription(scopeURL, [completionHandlerCopy = makeBlockPtr(completionHandler)] (auto result) {
+        if (result) {
+            if (auto data = result.value())
+                return completionHandlerCopy(wrapper(API::WebPushSubscriptionData::create(WTFMove(*data))).get(), nil);
+
+            return completionHandlerCopy(nil, nil);
+        }
+
+        auto error = [NSError errorWithDomain:@"WKErrorDomain" code:WKErrorUnknown userInfo:@{ NSLocalizedDescriptionKey:result.error().message }];
+        completionHandlerCopy(nil, error);
+    });
+}
+
+- (void)getNextPendingPushMessage:(void (^)(_WKWebPushMessage *))completionHandler
+{
+    _connection->getNextPendingPushMessage([completionHandlerCopy = makeBlockPtr(completionHandler)] (auto result) {
+        if (!result)
+            return completionHandlerCopy(nil);
+
+        return completionHandlerCopy(wrapper(API::WebPushMessage::create(WTFMove(result.value()))).get());
+    });
+}
+
+- (API::Object&)_apiObject
+{
+    return *_connection;
+}
+
+@end

Source/WebKit/Shared/API/APIObject.h
@@ -190,6 +190,9 @@ public:
 #endif
 #endif
         WebResourceLoadStatisticsManager,
+        WebPushDaemonConnection,
+        WebPushMessage,
+        WebPushSubscriptionData,
         WebsiteDataRecord,
         WebsiteDataStore,
         WebsiteDataStoreConfiguration,

Source/WebKit/Shared/Cocoa/APIObject.mm
@@ -96,6 +96,9 @@
 #import "_WKVisitedLinkStoreInternal.h"
 #import "_WKWebAuthenticationAssertionResponseInternal.h"
 #import "_WKWebAuthenticationPanelInternal.h"
+#import "_WKWebPushDaemonConnectionInternal.h"
+#import "_WKWebPushMessageInternal.h"
+#import "_WKWebPushSubscriptionDataInternal.h"
 #import "_WKWebsiteDataStoreConfigurationInternal.h"
 
 #if ENABLE(INSPECTOR_EXTENSIONS)
@@ -445,6 +448,18 @@ ALLOW_DEPRECATED_DECLARATIONS_END
         break;
 #endif
 #endif // ENABLE(WK_WEB_EXTENSIONS)
+
+    case Type::WebPushDaemonConnection:
+        wrapper = [_WKWebPushDaemonConnection alloc];
+        break;
+
+    case Type::WebPushMessage:
+        wrapper = [_WKWebPushMessage alloc];
+        break;
+
+    case Type::WebPushSubscriptionData:
+        wrapper = [_WKWebPushSubscriptionData alloc];
+        break;
 
     case Type::WebsiteDataRecord:
         wrapper = [WKWebsiteDataRecord alloc];

Source/WebKit/UIProcess/API/APIWebPushDaemonConnection.cpp
@@ -1,0 +1,106 @@
+/*
+ * Copyright (C) 2024 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "APIWebPushDaemonConnection.h"
+
+#include "MessageSenderInlines.h"
+#include "PushClientConnectionMessages.h"
+#include "WebPushMessage.h"
+#include <WebCore/ExceptionData.h>
+#include <WebCore/PushPermissionState.h>
+#include <WebCore/PushSubscriptionData.h>
+
+namespace API {
+
+WebPushDaemonConnection::WebPushDaemonConnection(const WTF::String& machServiceName, const WTF::String& partition, const WTF::String& bundleIdentifier)
+#if ENABLE(WEB_PUSH_NOTIFICATIONS)
+    : m_connection(makeUniqueRef<WebKit::WebPushD::Connection>(machServiceName.utf8(), WebKit::WebPushD::WebPushDaemonConnectionConfiguration { { }, bundleIdentifier, partition, { } }))
+#endif
+{
+}
+
+void WebPushDaemonConnection::getPushPermissionState(const WTF::URL& scopeURL, CompletionHandler<void(WebCore::PushPermissionState)>&& completionHandler)
+{
+#if ENABLE(WEB_PUSH_NOTIFICATIONS)
+    m_connection->sendWithAsyncReplyWithoutUsingIPCConnection(Messages::PushClientConnection::GetPushPermissionState(SecurityOriginData::fromURL(scopeURL)), WTFMove(completionHandler));
+#else
+    completionHandler(WebCore::PushPermissionState::Denied);
+#endif
+}
+
+void WebPushDaemonConnection::requestPushPermission(const WTF::URL& scopeURL, CompletionHandler<void(bool)>&& completionHandler)
+{
+#if ENABLE(WEB_PUSH_NOTIFICATIONS)
+    m_connection->sendWithAsyncReplyWithoutUsingIPCConnection(Messages::PushClientConnection::RequestPushPermission(SecurityOriginData::fromURL(scopeURL)), WTFMove(completionHandler));
+#else
+    completionHandler(false);
+#endif
+}
+
+void WebPushDaemonConnection::setAppBadge(const WTF::URL& scopeURL, std::optional<uint64_t> badge)
+{
+#if ENABLE(WEB_PUSH_NOTIFICATIONS)
+    m_connection->sendWithoutUsingIPCConnection(Messages::PushClientConnection::SetAppBadge(SecurityOriginData::fromURL(scopeURL), badge));
+#endif
+}
+
+void WebPushDaemonConnection::subscribeToPushService(const WTF::URL& scopeURL, const Vector<uint8_t>& applicationServerKey, CompletionHandler<void(const Expected<WebCore::PushSubscriptionData, WebCore::ExceptionData>&)>&& completionHandler)
+{
+#if ENABLE(WEB_PUSH_NOTIFICATIONS)
+    m_connection->sendWithAsyncReplyWithoutUsingIPCConnection(Messages::PushClientConnection::SubscribeToPushService(scopeURL, applicationServerKey), WTFMove(completionHandler));
+#else
+    completionHandler(makeUnexpected(WebCore::ExceptionData { WebCore::ExceptionCode::UnknownError, "Cannot subscribe to push service"_s }));
+#endif
+}
+
+void WebPushDaemonConnection::unsubscribeFromPushService(const WTF::URL& scopeURL, CompletionHandler<void(const Expected<bool, WebCore::ExceptionData>&)>&& completionHandler)
+{
+#if ENABLE(WEB_PUSH_NOTIFICATIONS)
+    m_connection->sendWithAsyncReplyWithoutUsingIPCConnection(Messages::PushClientConnection::UnsubscribeFromPushService(scopeURL, std::nullopt), WTFMove(completionHandler));
+#else
+    completionHandler(false);
+#endif
+}
+
+void WebPushDaemonConnection::getPushSubscription(const WTF::URL& scopeURL, CompletionHandler<void(const Expected<std::optional<WebCore::PushSubscriptionData>, WebCore::ExceptionData>&)>&& completionHandler)
+{
+#if ENABLE(WEB_PUSH_NOTIFICATIONS)
+    m_connection->sendWithAsyncReplyWithoutUsingIPCConnection(Messages::PushClientConnection::GetPushSubscription(WTFMove(scopeURL)), WTFMove(completionHandler));
+#else
+    completionHandler(std::optional<WebCore::PushSubscriptionData> { });
+#endif
+}
+
+void WebPushDaemonConnection::getNextPendingPushMessage(CompletionHandler<void(const std::optional<WebKit::WebPushMessage>&)>&& completionHandler)
+{
+#if ENABLE(WEB_PUSH_NOTIFICATIONS)
+    m_connection->sendWithAsyncReplyWithoutUsingIPCConnection(Messages::PushClientConnection::GetPendingPushMessage(), WTFMove(completionHandler));
+#else
+    completionHandler(std::nullopt);
+#endif
+}
+
+};

Source/WebKit/UIProcess/API/APIWebPushDaemonConnection.h
@@ -1,0 +1,64 @@
+/*
+ * Copyright (C) 2024 Apple Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include "APIObject.h"
+#include "WebPushDaemonConnection.h"
+
+namespace WebCore {
+enum class PushPermissionState : uint8_t;
+struct ExceptionData;
+struct PushSubscriptionData;
+}
+
+namespace WebKit {
+namespace WebPushD {
+class Connection;
+}
+struct WebPushMessage;
+}
+
+namespace API {
+
+class WebPushDaemonConnection final : public ObjectImpl<Object::Type::WebPushDaemonConnection> {
+public:
+    WebPushDaemonConnection(const WTF::String& machServiceName, const WTF::String& partition, const WTF::String& bundleIdentifier);
+
+    void getPushPermissionState(const WTF::URL&, CompletionHandler<void(WebCore::PushPermissionState)>&&);
+    void requestPushPermission(const WTF::URL&, CompletionHandler<void(bool)>&&);
+    void setAppBadge(const WTF::URL&, std::optional<uint64_t>);
+    void subscribeToPushService(const WTF::URL&, const Vector<uint8_t>& applicationServerKey, CompletionHandler<void(const Expected<WebCore::PushSubscriptionData, WebCore::ExceptionData>&)>&&);
+    void unsubscribeFromPushService(const WTF::URL&, CompletionHandler<void(const Expected<bool, WebCore::ExceptionData>&)>&&);
+    void getPushSubscription(const WTF::URL&, CompletionHandler<void(const Expected<std::optional<WebCore::PushSubscriptionData>, WebCore::ExceptionData>&)>&&);
+    void getNextPendingPushMessage(CompletionHandler<void(const std::optional<WebKit::WebPushMessage>&)>&&);
+
+private:
+#if ENABLE(WEB_PUSH_NOTIFICATIONS)
+    UniqueRef<WebKit::WebPushD::Connection> m_connection;
+#endif
+};
+
+} // namespace API

Source/WebKit/UIProcess/API/APIWebPushMessage.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Apple Inc. All rights reserved.
+ * Copyright (C) 2024 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -23,26 +23,29 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import <WebKit/WKDownloadDelegatePrivate.h>
-#import <WebKit/WKHistoryDelegatePrivate.h>
-#import <WebKit/WKNavigationPrivate.h>
-#import <WebKit/WKProcessPoolPrivate.h>
-#import <WebKit/WKUIDelegatePrivate.h>
-#import <WebKit/WKWebViewConfigurationPrivate.h>
-#import <WebKit/WKWebViewPrivate.h>
-#import <WebKit/_WKActivatedElementInfo.h>
-#import <WebKit/_WKAttachment.h>
-#import <WebKit/_WKElementAction.h>
-#import <WebKit/_WKFocusedElementInfo.h>
-#import <WebKit/_WKFormInputSession.h>
-#import <WebKit/_WKInputDelegate.h>
-#import <WebKit/_WKPageLoadTiming.h>
-#import <WebKit/_WKProcessPoolConfiguration.h>
-#import <WebKit/_WKTargetedElementInfo.h>
-#import <WebKit/_WKTargetedElementRequest.h>
-#import <WebKit/_WKThumbnailView.h>
-#import <WebKit/_WKVisitedLinkStore.h>
-#import <WebKit/_WKWebPushAction.h>
-#import <WebKit/_WKWebPushDaemonConnection.h>
-#import <WebKit/_WKWebPushMessage.h>
-#import <WebKit/_WKWebPushSubscriptionData.h>
+#pragma once
+
+#include "APIObject.h"
+#include "WebPushMessage.h"
+
+namespace API {
+
+class WebPushMessage final : public ObjectImpl<Object::Type::WebPushMessage> {
+public:
+    static Ref<WebPushMessage> create(WebKit::WebPushMessage&& message)
+    {
+        return adoptRef(*new WebPushMessage(WTFMove(message)));
+    }
+
+    explicit WebPushMessage(WebKit::WebPushMessage&& message)
+        : m_message(WTFMove(message)) { }
+
+    std::optional<Vector<uint8_t>> data() const { return m_message.pushData; };
+    WTF::URL scope() const { return m_message.registrationURL; }
+    WTF::String partition() const { return m_message.pushPartitionString; }
+
+private:
+    WebKit::WebPushMessage m_message;
+};
+
+} // namespace API

Source/WebKit/UIProcess/API/APIWebPushSubscriptionData.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Apple Inc. All rights reserved.
+ * Copyright (C) 2024 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -23,26 +23,30 @@
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import <WebKit/WKDownloadDelegatePrivate.h>
-#import <WebKit/WKHistoryDelegatePrivate.h>
-#import <WebKit/WKNavigationPrivate.h>
-#import <WebKit/WKProcessPoolPrivate.h>
-#import <WebKit/WKUIDelegatePrivate.h>
-#import <WebKit/WKWebViewConfigurationPrivate.h>
-#import <WebKit/WKWebViewPrivate.h>
-#import <WebKit/_WKActivatedElementInfo.h>
-#import <WebKit/_WKAttachment.h>
-#import <WebKit/_WKElementAction.h>
-#import <WebKit/_WKFocusedElementInfo.h>
-#import <WebKit/_WKFormInputSession.h>
-#import <WebKit/_WKInputDelegate.h>
-#import <WebKit/_WKPageLoadTiming.h>
-#import <WebKit/_WKProcessPoolConfiguration.h>
-#import <WebKit/_WKTargetedElementInfo.h>
-#import <WebKit/_WKTargetedElementRequest.h>
-#import <WebKit/_WKThumbnailView.h>
-#import <WebKit/_WKVisitedLinkStore.h>
-#import <WebKit/_WKWebPushAction.h>
-#import <WebKit/_WKWebPushDaemonConnection.h>
-#import <WebKit/_WKWebPushMessage.h>
-#import <WebKit/_WKWebPushSubscriptionData.h>
+#pragma once
+
+#include "APIObject.h"
+#include <WebCore/PushSubscriptionData.h>
+
+namespace API {
+
+class WebPushSubscriptionData final : public ObjectImpl<Object::Type::WebPushSubscriptionData> {
+public:
+    static Ref<WebPushSubscriptionData> create(WebCore::PushSubscriptionData&& data)
+    {
+        return adoptRef(*new WebPushSubscriptionData(WTFMove(data)));
+    }
+
+    explicit WebPushSubscriptionData(WebCore::PushSubscriptionData&& data)
+        : m_data(WTFMove(data)) { }
+
+    WTF::URL endpoint() const { return WTF::URL { m_data.endpoint }; }
+    Vector<uint8_t> applicationServerKey() const { return m_data.serverVAPIDPublicKey; }
+    Vector<uint8_t> clientECDHPublicKey() const { return m_data.clientECDHPublicKey; }
+    Vector<uint8_t> sharedAuthenticationSecret() const { return m_data.sharedAuthenticationSecret; }
+
+private:
+    const WebCore::PushSubscriptionData m_data;
+};
+
+} // namespace API

commit 6a3704d355a10e3145b598af170faef8208175fc

_WKWebPushDaemonConnectionConfiguration leaks instance variables <https://bugs.webkit.org/show_bug.cgi?id=300173> <rdar://161958379>
Reviewed by Geoffrey Garen.

Regressed in 283218@main.

* Source/WebKit/UIProcess/API/Cocoa/_WKWebPushDaemonConnection.h:
(_WKWebPushDaemonConnectionConfiguration.partition):
(_WKWebPushDaemonConnectionConfiguration.bundleIdentifierOverrideForTesting):
- Mark properties as `nullable` since the init method doesn't set them.
* Source/WebKit/UIProcess/API/Cocoa/_WKWebPushDaemonConnection.mm:
(-[_WKWebPushDaemonConnectionConfiguration init]):
- Use setter to make sure `_machServiceName` is retained.
(-[_WKWebPushDaemonConnectionConfiguration dealloc]): Add.
- Release instance variables to fix leaks.

Canonical link: https://commits.webkit.org/301312@main

Source/WebKit/UIProcess/API/Cocoa/_WKWebPushDaemonConnection.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Apple Inc. All rights reserved.
+ * Copyright (C) 2024-2025 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -44,9 +44,9 @@ WK_EXTERN
 
 - (instancetype)init;
 @property (nonatomic, copy) NSString *machServiceName;
-@property (nonatomic, copy) NSString *partition;
+@property (nonatomic, copy, nullable) NSString *partition;
 @property (nonatomic, assign) audit_token_t hostApplicationAuditToken;
-@property (nonatomic, copy) NSString *bundleIdentifierOverrideForTesting;
+@property (nonatomic, copy, nullable) NSString *bundleIdentifierOverrideForTesting;
 @end
 
 WK_EXTERN

Source/WebKit/UIProcess/API/Cocoa/_WKWebPushDaemonConnection.mm
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Apple Inc. All rights reserved.
+ * Copyright (C) 2024-2025 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -51,11 +51,22 @@
         return nil;
 
 #if ENABLE(RELOCATABLE_WEBPUSHD)
-    _machServiceName = @"com.apple.webkit.webpushd.relocatable.service";
+    self.machServiceName = @"com.apple.webkit.webpushd.relocatable.service";
 #else
-    _machServiceName = @"com.apple.webkit.webpushd.service";
+    self.machServiceName = @"com.apple.webkit.webpushd.service";
 #endif
     return self;
+}
+
+- (void)dealloc
+{
+IGNORE_NULL_CHECK_WARNINGS_BEGIN
+    self.machServiceName = nil;
+IGNORE_NULL_CHECK_WARNINGS_END
+    self.partition = nil;
+    self.bundleIdentifierOverrideForTesting = nil;
+
+    [super dealloc];
 }
 
 @end