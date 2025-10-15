10fce329c3429e56c662b9bb8997512ba076fede
Dan Hecht <dan.hecht@apple.com>
2025-10-15 08:14:59 -0700

    [JSC] WASM IPInt SIMD: support v128 in select and select_t instructions https://bugs.webkit.org/show_bug.cgi?id=300772 rdar://162653641
    Reviewed by Yusuke Suzuki.
    
    select and select_t should copy 16-bytes now that IPInt supports v128.
    
    The pre-existing test didn't catch this issue because pushQuad pushes
    the same register twice and the test used the same value in all lanes,
    so the final result was correct in this case. Adjust the test so that
    it can catch this bug. Also add select_t testcases.
    
    * JSTests/wasm/stress/simd-select.js:
    (from.string_appeared_here.import.as.assert.from.string_appeared_here.let.wat.module.func.export.string_appeared_here.param.i32.i32.result.i32.local.v128.v128.v128.const.i32x4.1.2.3.4.local.2.v128.const.i32x4.5.6.7.8.local.3.local.2.local.3.local.0.local.1.i32.eq.select.i32x4.extract_lane.0.func.export.string_appeared_here.param.i32.i32.result.i32.local.v128.v128.v128.const.i32x4.1.2.3.4.local.2.v128.const.i32x4.5.6.7.8.local.3.local.2.local.3.local.0.local.1.i32.eq.select.i32x4.extract_lane.3.func.export.string_appeared_here.param.i32.i32.result.i32.local.v128.v128.v128.const.i32x4.10.20.30.40.local.2.v128.const.i32x4.50.60.70.80.local.3.local.2.local.3.local.0.local.1.i32.eq.select.result.v128.i32x4.extract_lane.0.func.export.string_appeared_here.param.i32.i32.result.i32.local.v128.v128.v128.const.i32x4.10.20.30.40.local.2.v128.const.i32x4.50.60.70.80.local.3.local.2.local.3.local.0.local.1.i32.eq.select.result.v128.i32x4.extract_lane.3.async test):
    (from.string_appeared_here.import.as.assert.from.string_appeared_here.let.wat.module.func.export.string_appeared_here.param.i32.i32.result.i32.local.v128.v128.v128.const.i32x4.1.1.1.1.local.2.v128.const.i32x4.2.2.2.2.local.3.local.2.local.3.local.0.local.1.i32.eq.select.i32x4.extract_lane.0.func.export.string_appeared_here.param.i32.i32.result.i32.local.v128.v128.v128.const.i32x4.1.1.1.1.local.2.v128.const.i32x4.2.2.2.2.local.3.local.2.local.3.local.0.local.1.i32.eq.select.i32x4.extract_lane.3.async test): Deleted.
    * Source/JavaScriptCore/llint/InPlaceInterpreter64.asm:
    
    Canonical link: https://commits.webkit.org/301558@main

--- JSTests/wasm/stress/simd-select.js
+++ JSTests/wasm/stress/simd-select.js
@@ -6,9 +6,9 @@ import * as assert from "../assert.js"
 let wat = `
 (module
     (func (export "test_lower") (param i32 i32) (result i32) (local v128 v128)
-        v128.const i32x4 1 1 1 1
+        v128.const i32x4 1 2 3 4
         local.set 2
-        v128.const i32x4 2 2 2 2
+        v128.const i32x4 5 6 7 8
         local.set 3
 
         local.get 2
@@ -22,9 +22,9 @@ let wat = `
     )
 
     (func (export "test_upper") (param i32 i32) (result i32) (local v128 v128)
-        v128.const i32x4 1 1 1 1
+        v128.const i32x4 1 2 3 4
         local.set 2
-        v128.const i32x4 2 2 2 2
+        v128.const i32x4 5 6 7 8
         local.set 3
 
         local.get 2
@@ -36,16 +36,54 @@ let wat = `
 
         i32x4.extract_lane 3
     )
+
+    (func (export "test_select_t_lower") (param i32 i32) (result i32) (local v128 v128)
+        v128.const i32x4 10 20 30 40
+        local.set 2
+        v128.const i32x4 50 60 70 80
+        local.set 3
+
+        local.get 2
+        local.get 3
+        local.get 0
+        local.get 1
+        i32.eq
+        select (result v128)
+
+        i32x4.extract_lane 0
+    )
+
+    (func (export "test_select_t_upper") (param i32 i32) (result i32) (local v128 v128)
+        v128.const i32x4 10 20 30 40
+        local.set 2
+        v128.const i32x4 50 60 70 80
+        local.set 3
+
+        local.get 2
+        local.get 3
+        local.get 0
+        local.get 1
+        i32.eq
+        select (result v128)
+
+        i32x4.extract_lane 3
+    )
 )
 `
 
 async function test() {
     const instance = await instantiate(wat, {}, { simd: true });
-    const { test_lower, test_upper } = instance.exports;
-    assert.eq(test_lower(42, 42), 1);
-    assert.eq(test_lower(42, 43), 2);
-    assert.eq(test_upper(42, 42), 1);
-    assert.eq(test_upper(42, 43), 2);
+    const { test_lower, test_upper, test_select_t_lower, test_select_t_upper } = instance.exports;
+    for (let i = 0; i < wasmTestLoopCount; i++) {
+        assert.eq(test_lower(42, 42), 1);
+        assert.eq(test_lower(42, 43), 5);
+        assert.eq(test_upper(42, 42), 4);
+        assert.eq(test_upper(42, 43), 8);
+        assert.eq(test_select_t_lower(42, 42), 10);
+        assert.eq(test_select_t_lower(42, 43), 50);
+        assert.eq(test_select_t_upper(42, 42), 40);
+        assert.eq(test_select_t_upper(42, 43), 80);
+    }
 }
 
 await assert.asyncTest(test())
