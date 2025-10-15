vitorroriz/WebKit

commit 22cf38e52362dfec24edcbeb57e472db7dd5b61d

[Model Cleanup] Cleanup Model #ifdefs https://bugs.webkit.org/show_bug.cgi?id=300587
Reviewed by Mike Wyrzykowski.

As a first pass at cleaning up the model code, this replaces #ifdefs
like ENABLE(MODEL_PROCESS), which WebCore should not know about, with
feature specific macros. Specifically, we add:

    - ENABLE(MODEL_CONTEXT)
    - ENABLE(MODEL_ELEMENT_ACCESSIBILITY)
    - ENABLE(MODEL_ELEMENT_ANIMATIONS_CONTROL)
    - ENABLE(MODEL_ELEMENT_BOUNDING_BOX)
    - ENABLE(MODEL_ELEMENT_ENTITY_TRANSFORM)
    - ENABLE(MODEL_ELEMENT_ENVIRONMENT_MAP)
    - ENABLE(MODEL_ELEMENT_PORTAL)
    - ENABLE(MODEL_ELEMENT_STAGE_MODE)
    - ENABLE(MODEL_ELEMENT_STAGE_MODE_INTERACTION)

Additionally:
- The ModelPlayer::identifier() and ModelPlayerClient::didUnload() functions
  have been made unconditional, as they are universally implementable.
- The model accessibility children type has been abstracted into a new
  struct, ModelPlayerAccessibilityChildren to remove cocoa specific types
  from the model interfaces.
- ENABLE(GPUP_MODEL) has been renamed ENABLE(GPU_PROCESS_MODEL) to be
  consistent with other GPU Process enable macros.
- Fixed some sorting issues in wtf/Platform* headers.
- Added FIXMEs for improper use of ENALBE macros in PlatformHave.h
- Fixed define for HAVE_SCENEKIT back to 1.
- Moved ModelPlayerIdentifier out of platform and into Modules/model-element
  to be next to ModelPlayer and friends.

There remains more to do in follow ups:
- Remove usage of ENABLE(GPU_PROCESS_MODEL) in WebCore. This probably means
  moving all the DD prefixed code to WebKit.
- Add better runtime settings for use by HTMLModelElement.idl. It currently
  uses `EnabledBySetting=ModelProcessEnabled` which is improper.

* Source/WTF/wtf/PlatformEnable.h:
* Source/WTF/wtf/PlatformEnableCocoa.h:
* Source/WTF/wtf/PlatformHave.h:
* Source/WebCore/Headers.cmake:
* Source/WebCore/Modules/model-element/DDModelPlayer.h:
* Source/WebCore/Modules/model-element/DDModelPlayer.mm:

Source/WTF/wtf/PlatformHave.h
@@ -965,11 +965,12 @@
 #endif
 
 #if PLATFORM(MAC)
-#define HAVE_SCENEKIT !ENABLE_GPUP_MODEL
+#define HAVE_SCENEKIT 1
 #endif
 
 #if PLATFORM(COCOA)
 #define HAVE_WEBGPU_IMPLEMENTATION 1
+// FIXME: PlatformHave.h should not depend or defined ENABLE macros.
 #if !defined(ENABLE_WEBGPU_SWIFT)
 #define ENABLE_WEBGPU_SWIFT 0
 #endif
@@ -1154,6 +1155,7 @@
 
 #if PLATFORM(MAC) || PLATFORM(IOS) || PLATFORM(MACCATALYST) || PLATFORM(VISION)
 #if defined __has_include && __has_include(<CoreFoundation/CFPriv.h>)
+// FIXME: PlatformHave.h should not depend or defined ENABLE macros.
 #define ENABLE_SYSTEM_CONTENT_PATH_SANDBOX_RULES 1
 #endif
 #endif
@@ -1699,6 +1701,7 @@
 #define HAVE_RE_STEREO_CONTENT_SUPPORT 0
 #endif
 
+// FIXME: PlatformHave.h should not depend or defined ENABLE macros.
 #if !defined(ENABLE_UNPREFIXED_BACKDROP_FILTER) \
     && ((PLATFORM(APPLETV) && __TV_OS_VERSION_MIN_REQUIRED >= 180000) \
     || (PLATFORM(WATCHOS) && __WATCH_OS_VERSION_MIN_REQUIRED >= 120000) \

Source/WebCore/Modules/model-element/HTMLModelElement.cpp
@@ -81,7 +81,7 @@
 #include <wtf/TZoneMallocInlines.h>
 #include <wtf/URL.h>
 
-#if ENABLE(MODEL_PROCESS) || ENABLE(GPUP_MODEL)
+#if ENABLE(MODEL_CONTEXT)
 #include "ModelContext.h"
 #endif
 
@@ -97,12 +97,14 @@ HTMLModelElement::HTMLModelElement(const QualifiedName& tagName, Document& docum
     : HTMLElement(tagName, document, { TypeFlag::HasCustomStyleResolveCallbacks, TypeFlag::HasDidMoveToNewDocument })
     , ActiveDOMObject(document)
     , m_readyPromise { makeUniqueRef<ReadyPromise>(*this, &HTMLModelElement::readyPromiseResolve) }
-#if ENABLE(MODEL_PROCESS) || ENABLE(GPUP_MODEL)
+#if ENABLE(MODEL_ELEMENT_ENTITY_TRANSFORM)
     , m_entityTransform(DOMMatrixReadOnly::create(TransformationMatrix::identity, DOMMatrixReadOnly::Is2D::No))
+#endif
+#if ENABLE(MODEL_ELEMENT_BOUNDING_BOX)
     , m_boundingBoxCenter(DOMPointReadOnly::create({ }))
     , m_boundingBoxExtents(DOMPointReadOnly::create({ }))
 #endif
-#if ENABLE(MODEL_PROCESS)
+#if ENABLE(MODEL_ELEMENT_ENVIRONMENT_MAP)
     , m_environmentMapReadyPromise(makeUniqueRef<EnvironmentMapPromise>())
 #endif
 {
@@ -115,7 +117,7 @@ HTMLModelElement::~HTMLModelElement()
         m_resource = nullptr;
     }
 
-#if ENABLE(MODEL_PROCESS)
+#if ENABLE(MODEL_ELEMENT_ENVIRONMENT_MAP)
     if (m_environmentMapResource) {
         m_environmentMapResource->removeClient(*this);
         m_environmentMapResource = nullptr;
@@ -245,8 +247,10 @@ void HTMLModelElement::setSourceURL(const URL& url)
 
     deleteModelPlayer();
 
-#if ENABLE(MODEL_PROCESS) || ENABLE(GPUP_MODEL)
+#if ENABLE(MODEL_ELEMENT_ENTITY_TRANSFORM)
     m_entityTransform = DOMMatrixReadOnly::create(TransformationMatrix::identity, DOMMatrixReadOnly::Is2D::No);
+#endif
+#if ENABLE(MODEL_ELEMENT_BOUNDING_BOX)
     m_boundingBoxCenter = DOMPointReadOnly::create({ });
     m_boundingBoxExtents = DOMPointReadOnly::create({ });
 #endif
@@ -305,7 +309,7 @@ void HTMLModelElement::dataReceived(CachedResource& resource, const SharedBuffer
 {
     if (&resource == m_resource)
         m_data.append(buffer);
-#if ENABLE(MODEL_PROCESS)
+#if ENABLE(MODEL_ELEMENT_ENVIRONMENT_MAP)
     else if (&resource == m_environmentMapResource)
         m_environmentMapData.append(buffer);
 #endif
@@ -317,7 +321,7 @@ void HTMLModelElement::notifyFinished(CachedResource& resource, const NetworkLoa
 {
     if (&resource == m_resource)
         modelResourceFinished();
-#if ENABLE(MODEL_PROCESS)
+#if ENABLE(MODEL_ELEMENT_ENVIRONMENT_MAP)
     else if (&resource == m_environmentMapResource)
         environmentMapResourceFinished();
 #endif
@@ -356,11 +360,16 @@ void HTMLModelElement::createModelPlayer()
         deleteModelPlayer();
 
     ASSERT(document().page());
-#if ENABLE(MODEL_PROCESS) || ENABLE(GPUP_MODEL)
+
+#if ENABLE(MODEL_ELEMENT_ENTITY_TRANSFORM)
     m_entityTransform = DOMMatrixReadOnly::create(TransformationMatrix::identity, DOMMatrixReadOnly::Is2D::No);
+#endif
+
+#if ENABLE(MODEL_ELEMENT_BOUNDING_BOX)
     m_boundingBoxCenter = DOMPointReadOnly::create({ });
     m_boundingBoxExtents = DOMPointReadOnly::create({ });
 #endif
+
     if (!m_modelPlayerProvider)
         m_modelPlayerProvider = document().page()->modelPlayerProvider();
     if (RefPtr protectedModelPlayerProvider = m_modelPlayerProvider.get())
@@ -371,13 +380,17 @@ void HTMLModelElement::createModelPlayer()
         return;
     }
 
-#if ENABLE(MODEL_PROCESS) || ENABLE(GPUP_MODEL)
+#if ENABLE(MODEL_ELEMENT_ANIMATIONS_CONTROL)
     m_modelPlayer->setAutoplay(autoplay());
     m_modelPlayer->setLoop(loop());
     m_modelPlayer->setPlaybackRate(m_playbackRate, [&](double) { });
+#endif
+
+#if ENABLE(MODEL_ELEMENT_PORTAL)
     m_modelPlayer->setHasPortal(hasPortal());
 #endif
-#if ENABLE(MODEL_PROCESS)
+
+#if ENABLE(MODEL_ELEMENT_STAGE_MODE)
     m_modelPlayer->setStageMode(stageMode());
 #endif
 
@@ -385,7 +398,7 @@ void HTMLModelElement::createModelPlayer()
     // in with load probably doesn't make sense.
     m_modelPlayer->load(*m_model, size);
 
-#if ENABLE(MODEL_PROCESS) || ENABLE(GPUP_MODEL)
+#if ENABLE(MODEL_ELEMENT_ENVIRONMENT_MAP)
     if (m_environmentMapData)
         m_modelPlayer->setEnvironmentMap(m_environmentMapData.takeAsContiguous().get());
     else if (!m_environmentMapURL.isEmpty())
@@ -463,7 +476,7 @@ void HTMLModelElement::reloadModelPlayer()
     RELEASE_LOG(ModelElement, "%p - HTMLModelElement: Reloading previous states to new model player: %p", this, m_modelPlayer.get());
     m_modelPlayer->reload(*m_model, size, *animationState, WTFMove(*transformState));
 
-#if ENABLE(MODEL_PROCESS) || ENABLE(GPUP_MODEL)
+#if ENABLE(MODEL_ELEMENT_ENVIRONMENT_MAP)
     if (m_environmentMapData)
         m_modelPlayer->setEnvironmentMap(m_environmentMapData.takeAsContiguous().get());
     else if (!m_environmentMapURL.isEmpty())
@@ -505,23 +518,20 @@ bool HTMLModelElement::usesPlatformLayer() const
     return m_modelPlayer && m_modelPlayer->layer();
 }
 
+#if ENABLE(GPU_PROCESS_MODEL)
+
+// FIXME: It is a layering violation for WebCore to be concerned with MachSendRights like this.
 const MachSendRight* HTMLModelElement::displayBuffer() const
 {
-#if ENABLE(GPUP_MODEL)
     return m_modelPlayer ? m_modelPlayer->displayBuffer() : nullptr;
-#else
-    return nullptr;
-#endif
 }
 
 GraphicsLayerContentsDisplayDelegate* HTMLModelElement::contentsDisplayDelegate()
 {
-#if ENABLE(GPUP_MODEL)
     return m_modelPlayer ? m_modelPlayer->contentsDisplayDelegate() : nullptr;
-#else
-    return nullptr;
-#endif
 }
+
+#endif
 
 PlatformLayer* HTMLModelElement::platformLayer() const
 {
@@ -554,6 +564,18 @@ void HTMLModelElement::didUpdateLayerHostingContextIdentifier(ModelPlayer& model
         renderer->updateFromElement();
 }
 
+#if ENABLE(GPU_PROCESS_MODEL)
+
+void HTMLModelElement::didUpdateDisplayDelegate(ModelPlayer& modelPlayer) const
+{
+    ASSERT_UNUSED(modelPlayer, &modelPlayer == m_modelPlayer);
+
+    if (CheckedPtr renderer = this->renderer())
+        renderer->updateFromElement();
+}
+
+#endif
+
 void HTMLModelElement::logWarning(ModelPlayer& modelPlayer, const String& warningMessage)
 {
     ASSERT_UNUSED(modelPlayer, &modelPlayer == m_modelPlayer);
@@ -583,6 +605,20 @@ void HTMLModelElement::didFailLoading(ModelPlayer& modelPlayer, const ResourceEr
 
     m_dataMemoryCost.store(0, std::memory_order_relaxed);
     reportExtraMemoryCost();
+}
+
+void HTMLModelElement::didUnload(ModelPlayer& modelPlayer)
+{
+    if (m_modelPlayer != &modelPlayer)
+        return;
+
+    unloadModelPlayer(false);
+
+    if (!isVisible())
+        return;
+
+    // FIXME: rdar://148027600 Prevent infinite reloading of model.
+    startLoadModelTimer();
 }
 
 RefPtr<GraphicsLayer> HTMLModelElement::graphicsLayer() const
@@ -624,7 +660,8 @@ bool HTMLModelElement::isVisible() const
     return !document().hidden() && m_isIntersectingViewport;
 }
 
-#if ENABLE(MODEL_PROCESS) || ENABLE(GPUP_MODEL)
+#if ENABLE(MODEL_CONTEXT)
+
 RefPtr<ModelContext> HTMLModelElement::modelContext() const
 {
     auto modelLayerIdentifier = layerID();
@@ -637,6 +674,10 @@ RefPtr<ModelContext> HTMLModelElement::modelContext() const
 
     return ModelContext::create(*modelLayerIdentifier, *modelContentsLayerHostingContextIdentifier, contentSize(), hasPortal() ? ModelContextDisablePortal::No : ModelContextDisablePortal::Yes, std::nullopt).ptr();
 }
+
+#endif
+
+#if ENABLE(MODEL_ELEMENT_ENTITY_TRANSFORM)
 
 const DOMMatrixReadOnly& HTMLModelElement::entityTransform() const
 {
@@ -670,6 +711,10 @@ void HTMLModelElement::didUpdateEntityTransform(ModelPlayer&, const Transformati
     m_entityTransform = DOMMatrixReadOnly::create(transform, DOMMatrixReadOnly::Is2D::No);
 }
 
+#endif
+
+#if ENABLE(MODEL_ELEMENT_BOUNDING_BOX)
+
 const DOMPointReadOnly& HTMLModelElement::boundingBoxCenter() const
 {
     return m_boundingBoxCenter;
@@ -686,9 +731,12 @@ void HTMLModelElement::didUpdateBoundingBox(ModelPlayer&, const FloatPoint3D& ce
     m_boundingBoxExtents = DOMPointReadOnly::fromFloatPoint(extents);
 }
 
-void HTMLModelElement::didFinishEnvironmentMapLoading(bool succeeded)
+#endif
+
+#if ENABLE(MODEL_ELEMENT_ENVIRONMENT_MAP)
+
+void HTMLModelElement::didFinishEnvironmentMapLoading(ModelPlayer&, bool succeeded)
 {
-#if ENABLE(MODEL_PROCESS)
     if (!m_environmentMapURL.isEmpty() && !m_environmentMapReadyPromise->isFulfilled()) {
         if (succeeded)
             m_environmentMapReadyPromise->resolve();
@@ -698,18 +746,15 @@ void HTMLModelElement::didFinishEnvironmentMapLoading(bool succeeded)
         }
         reportExtraMemoryCost();
     }
-#else
-    UNUSED_PARAM(succeeded);
-#endif
 }
+
+#endif
+
+#if ENABLE(MODEL_ELEMENT_STAGE_MODE_INTERACTION)
 
 bool HTMLModelElement::supportsStageModeInteraction() const
 {
-#if ENABLE(MODEL_PROCESS)
     return stageMode() != StageModeOperation::None;
-#else
-    return false;
-#endif
 }
 
 void HTMLModelElement::beginStageModeTransform(const TransformationMatrix& transform)
@@ -744,21 +789,7 @@ void HTMLModelElement::resetModelTransformAfterDrag()
         m_modelPlayer->resetModelTransformAfterDrag();
 }
 
-void HTMLModelElement::didUnload(ModelPlayer& modelPlayer)
-{
-    if (m_modelPlayer != &modelPlayer)
-        return;
-
-    unloadModelPlayer(false);
-
-    if (!isVisible())
-        return;
-
-    // FIXME: rdar://148027600 Prevent infinite reloading of model.
-    startLoadModelTimer();
-}
-
-#endif // ENABLE(MODEL_PROCESS)
+#endif
 
 // MARK: - Fullscreen support.
 
@@ -796,19 +827,23 @@ void HTMLModelElement::attributeChanged(const QualifiedName& name, const AtomStr
         if (m_modelPlayer)
             m_modelPlayer->setInteractionEnabled(isInteractive());
     }
-#if ENABLE(MODEL_PROCESS) || ENABLE(GPUP_MODEL)
+#if ENABLE(MODEL_ELEMENT_ANIMATIONS_CONTROL)
     else if (name == autoplayAttr)
         updateAutoplay();
     else if (name == loopAttr)
         updateLoop();
+#endif
+#if ENABLE(MODEL_ELEMENT_ENVIRONMENT_MAP)
     else if (name == environmentmapAttr)
         updateEnvironmentMap();
-#if PLATFORM(VISION)
+#endif
+#if ENABLE(MODEL_ELEMENT_STAGE_MODE)
     else if (name == stagemodeAttr)
         updateStageMode();
+#endif
+#if ENABLE(MODEL_ELEMENT_PORTAL)
     else if (document().settings().modelNoPortalAttributeEnabled() && name == noportalAttr)
         updateHasPortal();
-#endif
 #endif
     else
         HTMLElement::attributeChanged(name, oldValue, newValue, attributeModificationReason);
@@ -922,7 +957,8 @@ void HTMLModelElement::setCamera(HTMLModelElementCamera camera, DOMPromiseDeferr
 
 // MARK: - Animations support.
 
-#if ENABLE(MODEL_PROCESS) || ENABLE(GPUP_MODEL)
+#if ENABLE(MODEL_ELEMENT_ANIMATIONS_CONTROL)
+
 void HTMLModelElement::setPlaybackRate(double playbackRate)
 {
     if (m_playbackRate == playbackRate)
@@ -980,23 +1016,6 @@ void HTMLModelElement::updateAutoplay()
         m_modelPlayer->setAutoplay(autoplay());
 }
 
-#if ENABLE(MODEL_PROCESS)
-WebCore::StageModeOperation HTMLModelElement::stageMode() const
-{
-    String attr = attributeWithoutSynchronization(HTMLNames::stagemodeAttr);
-    if (equalLettersIgnoringASCIICase(attr, "orbit"_s))
-        return WebCore::StageModeOperation::Orbit;
-
-    return WebCore::StageModeOperation::None;
-}
-
-void HTMLModelElement::updateStageMode()
-{
-    if (m_modelPlayer)
-        m_modelPlayer->setStageMode(stageMode());
-}
-#endif
-
 bool HTMLModelElement::loop() const
 {
     return hasAttributeWithoutSynchronization(HTMLNames::loopAttr);
@@ -1019,13 +1038,32 @@ void HTMLModelElement::setCurrentTime(double currentTime)
         m_modelPlayer->setCurrentTime(Seconds(currentTime), [&]() { });
 }
 
+#endif
+
+#if ENABLE(MODEL_ELEMENT_STAGE_MODE)
+
+WebCore::StageModeOperation HTMLModelElement::stageMode() const
+{
+    String attr = attributeWithoutSynchronization(HTMLNames::stagemodeAttr);
+    if (equalLettersIgnoringASCIICase(attr, "orbit"_s))
+        return WebCore::StageModeOperation::Orbit;
+
+    return WebCore::StageModeOperation::None;
+}
+
+void HTMLModelElement::updateStageMode()
+{
+    if (m_modelPlayer)
+        m_modelPlayer->setStageMode(stageMode());
+}
+
+#endif
+
+#if ENABLE(MODEL_ELEMENT_PORTAL)
+
 bool HTMLModelElement::hasPortal() const
 {
-#if PLATFORM(VISION)
     return !(document().settings().modelNoPortalAttributeEnabled() && hasAttributeWithoutSynchronization(HTMLNames::noportalAttr));
-#else
-    return true;
-#endif
 }
 
 void HTMLModelElement::updateHasPortal()
@@ -1037,16 +1075,20 @@ void HTMLModelElement::updateHasPortal()
         modelPlayer->setHasPortal(hasPortal());
 }
 
+#endif
+
+#if ENABLE(MODEL_ELEMENT_ENVIRONMENT_MAP)
+
 const URL& HTMLModelElement::environmentMap() const
 {
     return m_environmentMapURL;
-};
+}
 
 void HTMLModelElement::setEnvironmentMap(const URL& url)
 {
     if (url.string() == m_environmentMapURL.string())
         return;
-#if ENABLE(MODEL_PROCESS)
+
     m_environmentMapURL = url;
     m_environmentMapDataMemoryCost.store(0, std::memory_order_relaxed);
 
@@ -1062,14 +1104,11 @@ void HTMLModelElement::setEnvironmentMap(const URL& url)
     }
 
     environmentMapRequestResource();
-#endif
 }
 
 void HTMLModelElement::updateEnvironmentMap()
 {
-#if ENABLE(MODEL_PROCESS)
     setEnvironmentMap(selectEnvironmentMapURL());
-#endif
 }
 
 URL HTMLModelElement::selectEnvironmentMapURL() const
@@ -1089,7 +1128,6 @@ URL HTMLModelElement::selectEnvironmentMapURL() const
 
 void HTMLModelElement::environmentMapRequestResource()
 {
-#if ENABLE(MODEL_PROCESS)
     auto request = createResourceRequest(m_environmentMapURL, FetchOptions::Destination::Environmentmap);
     auto resource = document().protectedCachedResourceLoader()->requestEnvironmentMapResource(WTFMove(request));
     if (!resource.has_value()) {
@@ -1105,12 +1143,10 @@ void HTMLModelElement::environmentMapRequestResource()
 
     m_environmentMapResource = resource.value();
     m_environmentMapResource->addClient(*this);
-#endif
 }
 
 void HTMLModelElement::environmentMapResetAndReject(Exception&& exception)
 {
-#if ENABLE(MODEL_PROCESS)
     m_environmentMapData.reset();
 
     if (m_environmentMapResource) {
@@ -1120,14 +1156,10 @@ void HTMLModelElement::environmentMapResetAndReject(Exception&& exception)
 
     if (!m_environmentMapReadyPromise->isFulfilled())
         m_environmentMapReadyPromise->reject(WTFMove(exception));
-#else
-    UNUSED_PARAM(exception);
-#endif
 }
 
 void HTMLModelElement::environmentMapResourceFinished()
 {
-#if ENABLE(MODEL_PROCESS)
     int status = m_environmentMapResource->response().httpStatusCode();
     if (m_environmentMapResource->loadFailedOrCanceled() || (status && (status < 200 || status > 299))) {
         environmentMapResetAndReject(Exception { ExceptionCode::NetworkError });
@@ -1144,10 +1176,9 @@ void HTMLModelElement::environmentMapResourceFinished()
 
     m_environmentMapResource->removeClient(*this);
     m_environmentMapResource = nullptr;
-#endif
 }
 
-#endif // ENABLE(MODEL_PROCESS)
+#endif
 
 bool HTMLModelElement::shouldDeferLoading() const
 {
@@ -1372,13 +1403,15 @@ void HTMLModelElement::stop()
     deleteModelPlayer();
 }
 
-#if PLATFORM(COCOA)
-Vector<RetainPtr<id>> HTMLModelElement::accessibilityChildren()
+#if ENABLE(MODEL_ELEMENT_ACCESSIBILITY)
+
+ModelPlayerAccessibilityChildren HTMLModelElement::accessibilityChildren()
 {
     if (!m_modelPlayer)
         return { };
     return m_modelPlayer->accessibilityChildren();
 }
+
 #endif
 
 LayoutSize HTMLModelElement::contentSize() const
@@ -1390,12 +1423,14 @@ LayoutSize HTMLModelElement::contentSize() const
 }
 
 #if ENABLE(ARKIT_INLINE_PREVIEW_MAC)
+
 String HTMLModelElement::inlinePreviewUUIDForTesting() const
 {
     if (!m_modelPlayer)
         return emptyString();
     return m_modelPlayer->inlinePreviewUUIDForTesting();
 }
+
 #endif
 
 void HTMLModelElement::collectPresentationalHintsForAttribute(const QualifiedName& name, const AtomString& value, MutableStyleProperties& style)
@@ -1420,7 +1455,7 @@ bool HTMLModelElement::hasPresentationalHintsForAttribute(const QualifiedName& n
 bool HTMLModelElement::isURLAttribute(const Attribute& attribute) const
 {
     return attribute.name() == srcAttr
-#if ENABLE(MODEL_PROCESS) || ENABLE(GPUP_MODEL)
+#if ENABLE(MODEL_ELEMENT_ENVIRONMENT_MAP)
         || attribute.name() == environmentmapAttr
 #endif
         || HTMLElement::isURLAttribute(attribute);
@@ -1472,13 +1507,14 @@ size_t HTMLModelElement::memoryCost() const
 {
     // May be called from GC threads.
     auto cost = m_dataMemoryCost.load(std::memory_order_relaxed);
-#if ENABLE(MODEL_PROCESS) || ENABLE(GPUP_MODEL)
+#if ENABLE(MODEL_ELEMENT_ENVIRONMENT_MAP)
     cost += m_environmentMapDataMemoryCost.load(std::memory_order_relaxed);
 #endif
     return cost;
 }
 
 #if ENABLE(RESOURCE_USAGE)
+
 size_t HTMLModelElement::externalMemoryCost() const
 {
     // For the purposes of Web Inspector, external memory means memory reported as
@@ -1486,6 +1522,7 @@ size_t HTMLModelElement::externalMemoryCost() const
     // 2) not allocated from "Page" category, e.g. from bmalloc.
     return memoryCost();
 }
+
 #endif
 
 void HTMLModelElement::sourceRequestResource()
@@ -1559,14 +1596,6 @@ String HTMLModelElement::modelElementStateForTesting() const
     ASSERT_NOT_REACHED();
     return "Unknown"_s;
 }
-
-#if ENABLE(GPUP_MODEL)
-void HTMLModelElement::didUpdateDisplayDelegate() const
-{
-    if (CheckedPtr renderer = this->renderer())
-        renderer->updateFromElement();
-}
-#endif
 
 }
 

Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
@@ -80,7 +80,7 @@
 #include "AcceleratedEffectStack.h"
 #endif
 
-#if ENABLE(MODEL_PROCESS)
+#if ENABLE(MODEL_CONTEXT)
 #include "ModelContext.h"
 #endif
 
@@ -351,7 +351,7 @@ Ref<PlatformCALayer> GraphicsLayerCA::createPlatformCALayer(PlatformLayer* platf
     return PlatformCALayerCocoa::create(platformLayer, owner);
 }
 
-#if ENABLE(MODEL_PROCESS)
+#if ENABLE(MODEL_CONTEXT)
 Ref<PlatformCALayer> GraphicsLayerCA::createPlatformCALayer(Ref<ModelContext>, PlatformCALayerClient* owner)
 {
     ASSERT_NOT_REACHED_WITH_MESSAGE("GraphicsLayerCARemote::createPlatformCALayer should always be called instead of this, but this symbol is needed to compile WebKitLegacy.");
@@ -1423,7 +1423,7 @@ void GraphicsLayerCA::setContentsToPlatformLayerHost(LayerHostingContextIdentifi
     noteLayerPropertyChanged(ContentsPlatformLayerChanged);
 }
 
-#if ENABLE(MODEL_PROCESS)
+#if ENABLE(MODEL_CONTEXT)
 void GraphicsLayerCA::setContentsToModelContext(Ref<ModelContext> modelContext, ContentsLayerPurpose purpose)
 {
     if (m_contentsLayer && m_contentsLayer->hostingContextIdentifier() == modelContext->modelContentsLayerHostingContextIdentifier())

Source/WebCore/platform/graphics/ca/GraphicsLayerCA.h
@@ -53,7 +53,7 @@ class Image;
 class NativeImage;
 class TransformState;
 
-#if ENABLE(MODEL_PROCESS)
+#if ENABLE(MODEL_CONTEXT)
 class ModelContext;
 #endif
 
@@ -173,7 +173,7 @@ public:
 #endif
     WEBCORE_EXPORT void setContentsToPlatformLayer(PlatformLayer*, ContentsLayerPurpose) override;
     WEBCORE_EXPORT void setContentsToPlatformLayerHost(LayerHostingContextIdentifier) override;
-#if ENABLE(MODEL_PROCESS)
+#if ENABLE(MODEL_CONTEXT)
     WEBCORE_EXPORT void setContentsToModelContext(Ref<ModelContext>, ContentsLayerPurpose) override;
 #endif
     WEBCORE_EXPORT void setContentsToVideoElement(HTMLVideoElement&, ContentsLayerPurpose) override;
@@ -311,7 +311,7 @@ private:
 
     virtual Ref<PlatformCALayer> createPlatformCALayer(PlatformCALayer::LayerType, PlatformCALayerClient* owner);
     virtual Ref<PlatformCALayer> createPlatformCALayer(PlatformLayer*, PlatformCALayerClient* owner);
-#if ENABLE(MODEL_PROCESS)
+#if ENABLE(MODEL_CONTEXT)
     virtual Ref<PlatformCALayer> createPlatformCALayer(Ref<ModelContext>, PlatformCALayerClient*);
 #endif
 #if ENABLE(MODEL_ELEMENT)

commit a6164132d311a388e3bbdf7ee29b8fb6ea5b954c

GPU process model fails to build after 301487@main https://bugs.webkit.org/show_bug.cgi?id=300738 rdar://162637671
Unreviewed build fix when GPU_PROCES_MODEL is 1.

301487@main conflated model process and GPU process model in a few
places, specifically the differences:

* GPU process model does not use a seperate model process
* GPU process model is not hosted, it is composited in the WCP like Canvas
* There are no portals or stage interaction in the GPU process model backend

Fix up the build by adding / shuffling some defines around

* Source/WTF/wtf/PlatformHave.h:
* Source/WebCore/Modules/model-element/HTMLModelElement.cpp:
(WebCore::HTMLModelElement::modelContext const):
(WebCore::HTMLModelElement::setEntityTransform):
* Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp:
* Source/WebCore/platform/graphics/ca/GraphicsLayerCA.h:
* Source/WebCore/rendering/RenderLayerBacking.cpp:
(WebCore::RenderLayerBacking::updateConfiguration):

Canonical link: https://commits.webkit.org/301533@main

Source/WTF/wtf/PlatformHave.h
@@ -965,7 +965,7 @@
 #endif
 
 #if PLATFORM(MAC)
-#define HAVE_SCENEKIT 1
+#define HAVE_SCENEKIT !ENABLE_GPU_PROCESS_MODEL
 #endif
 
 #if PLATFORM(COCOA)

Source/WebCore/Modules/model-element/HTMLModelElement.cpp
@@ -672,7 +672,13 @@ RefPtr<ModelContext> HTMLModelElement::modelContext() const
     if (!modelContentsLayerHostingContextIdentifier)
         return nullptr;
 
-    return ModelContext::create(*modelLayerIdentifier, *modelContentsLayerHostingContextIdentifier, contentSize(), hasPortal() ? ModelContextDisablePortal::No : ModelContextDisablePortal::Yes, std::nullopt).ptr();
+#if ENABLE(MODEL_ELEMENT_PORTAL)
+    auto shouldEnablePortal = hasPortal() ? ModelContextDisablePortal::No : ModelContextDisablePortal::Yes;
+#else
+    auto shouldEnablePortal = ModelContextDisablePortal::Yes;
+#endif
+
+    return ModelContext::create(*modelLayerIdentifier, *modelContentsLayerHostingContextIdentifier, contentSize(), shouldEnablePortal, std::nullopt).ptr();
 }
 
 #endif
@@ -686,8 +692,10 @@ const DOMMatrixReadOnly& HTMLModelElement::entityTransform() const
 
 ExceptionOr<void> HTMLModelElement::setEntityTransform(const DOMMatrixReadOnly& transform)
 {
+#if ENABLE(MODEL_ELEMENT_STAGE_MODE_INTERACTION)
     if (supportsStageModeInteraction())
         return Exception { ExceptionCode::InvalidStateError,  "Transform is read-only unless StageMode is set to 'none'"_s };
+#endif
 
     auto player = m_modelPlayer;
     if (!player) {

Source/WebCore/platform/graphics/ca/GraphicsLayerCA.cpp
@@ -351,7 +351,7 @@ Ref<PlatformCALayer> GraphicsLayerCA::createPlatformCALayer(PlatformLayer* platf
     return PlatformCALayerCocoa::create(platformLayer, owner);
 }
 
-#if ENABLE(MODEL_CONTEXT)
+#if ENABLE(MODEL_CONTEXT) && !ENABLE(GPU_PROCESS_MODEL)
 Ref<PlatformCALayer> GraphicsLayerCA::createPlatformCALayer(Ref<ModelContext>, PlatformCALayerClient* owner)
 {
     ASSERT_NOT_REACHED_WITH_MESSAGE("GraphicsLayerCARemote::createPlatformCALayer should always be called instead of this, but this symbol is needed to compile WebKitLegacy.");
@@ -1423,7 +1423,7 @@ void GraphicsLayerCA::setContentsToPlatformLayerHost(LayerHostingContextIdentifi
     noteLayerPropertyChanged(ContentsPlatformLayerChanged);
 }
 
-#if ENABLE(MODEL_CONTEXT)
+#if ENABLE(MODEL_CONTEXT) && !ENABLE(GPU_PROCESS_MODEL)
 void GraphicsLayerCA::setContentsToModelContext(Ref<ModelContext> modelContext, ContentsLayerPurpose purpose)
 {
     if (m_contentsLayer && m_contentsLayer->hostingContextIdentifier() == modelContext->modelContentsLayerHostingContextIdentifier())

Source/WebCore/platform/graphics/ca/GraphicsLayerCA.h
@@ -173,7 +173,7 @@ public:
 #endif
     WEBCORE_EXPORT void setContentsToPlatformLayer(PlatformLayer*, ContentsLayerPurpose) override;
     WEBCORE_EXPORT void setContentsToPlatformLayerHost(LayerHostingContextIdentifier) override;
-#if ENABLE(MODEL_CONTEXT)
+#if ENABLE(MODEL_CONTEXT) && !ENABLE(GPU_PROCESS_MODEL)
     WEBCORE_EXPORT void setContentsToModelContext(Ref<ModelContext>, ContentsLayerPurpose) override;
 #endif
     WEBCORE_EXPORT void setContentsToVideoElement(HTMLVideoElement&, ContentsLayerPurpose) override;
@@ -311,7 +311,7 @@ private:
 
     virtual Ref<PlatformCALayer> createPlatformCALayer(PlatformCALayer::LayerType, PlatformCALayerClient* owner);
     virtual Ref<PlatformCALayer> createPlatformCALayer(PlatformLayer*, PlatformCALayerClient* owner);
-#if ENABLE(MODEL_CONTEXT)
+#if ENABLE(MODEL_CONTEXT) && !ENABLE(GPU_PROCESS_MODEL)
     virtual Ref<PlatformCALayer> createPlatformCALayer(Ref<ModelContext>, PlatformCALayerClient*);
 #endif
 #if ENABLE(MODEL_ELEMENT)

Source/WebCore/rendering/RenderLayerBacking.cpp
@@ -1321,7 +1321,7 @@ bool RenderLayerBacking::updateConfiguration(const RenderLayer* compositingAnces
         // but this is a runtime decision.
         if (element->usesPlatformLayer())
             m_graphicsLayer->setContentsToPlatformLayer(element->platformLayer(), GraphicsLayer::ContentsLayerPurpose::Model);
-#if ENABLE(MODEL_CONTEXT)
+#if ENABLE(MODEL_CONTEXT) && !ENABLE(GPU_PROCESS_MODEL)
         else if (auto modelContext = element->modelContext(); modelContext && element->document().settings().modelProcessEnabled()) {
             modelContext->setBackgroundColor(rendererBackgroundColor());
             m_graphicsLayer->setContentsToModelContext(*modelContext, GraphicsLayer::ContentsLayerPurpose::HostedModel);