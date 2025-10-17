vitorroriz/WebKit

commit c38095edbbdb85138b057e0099901777e0f8bf40

[CSS ZOOM] Apply zoom factor to border width https://bugs.webkit.org/show_bug.cgi?id=299885 rdar://161669263
Reviewed by Antti Koivisto and Sam Weinig.

We apply the zoom factor at evaluation-time for LineWidth,
BorderImageWidth, and MaskBorderWidth.

Tests: imported/w3c/web-platform-tests/css/css-viewport/zoom/border-image-width-ref.html
       imported/w3c/web-platform-tests/css/css-viewport/zoom/border-image-width.html
       imported/w3c/web-platform-tests/css/css-viewport/zoom/border-width-ref.html
       imported/w3c/web-platform-tests/css/css-viewport/zoom/border-width.html
       imported/w3c/web-platform-tests/css/css-viewport/zoom/mask-border-width-ref.html
       imported/w3c/web-platform-tests/css/css-viewport/zoom/mask-border-width.html

* LayoutTests/TestExpectations:

* LayoutTests/fast/sub-pixel/zoomed-em-border.html:
* LayoutTests/svg/zoom/page/zoom-replaced-intrinsic-ratio-001.htm:

We're disabling evaluation-time zoom flag on above tests because of double zoom.
The issue is that font-dependent units are being affected by the zoom
value during style-resolution, and we apply zoom again during evaluation.
This will be addressed separately as a follow-up:
https://bugs.webkit.org/show_bug.cgi?id=299816

* LayoutTests/imported/w3c/web-platform-tests/css/css-viewport/zoom/border-image-width-expected.html: Added.
* LayoutTests/imported/w3c/web-platform-tests/css/css-viewport/zoom/border-image-width-ref.html: Added.
* LayoutTests/imported/w3c/web-platform-tests/css/css-viewport/zoom/border-image-width.html: Added.
* LayoutTests/imported/w3c/web-platform-tests/css/css-viewport/zoom/border-width-computed-zoom-expected.txt: Added.
* LayoutTests/imported/w3c/web-platform-tests/css/css-viewport/zoom/border-width-computed-zoom.html: Added.
* LayoutTests/imported/w3c/web-platform-tests/css/css-viewport/zoom/border-width-expected.html: Added.
* LayoutTests/imported/w3c/web-platform-tests/css/css-viewport/zoom/border-width-ref.html: Added.

Source/WebCore/rendering/style/StyleRareInheritedData.cpp
@@ -77,6 +77,7 @@ DEFINE_ALLOCATOR_WITH_HEAP_IDENTIFIER(StyleRareInheritedData);
 
 StyleRareInheritedData::StyleRareInheritedData()
     : usedZoom(RenderStyle::initialZoom())
+    , deviceScaleFactor(1.0f)
     , listStyleImage(RenderStyle::initialListStyleImage())
     , textStrokeWidth(RenderStyle::initialTextStrokeWidth())
     , textStrokeColor(RenderStyle::initialTextStrokeColor())
@@ -180,6 +181,7 @@ StyleRareInheritedData::StyleRareInheritedData()
 inline StyleRareInheritedData::StyleRareInheritedData(const StyleRareInheritedData& o)
     : RefCounted<StyleRareInheritedData>()
     , usedZoom(o.usedZoom)
+    , deviceScaleFactor(o.deviceScaleFactor)
     , listStyleImage(o.listStyleImage)
     , textStrokeWidth(o.textStrokeWidth)
     , textStrokeColor(o.textStrokeColor)
@@ -393,7 +395,8 @@ bool StyleRareInheritedData::operator==(const StyleRareInheritedData& o) const
         && listStyleImage == o.listStyleImage
         && listStyleType == o.listStyleType
         && blockEllipsis == o.blockEllipsis
-        && enableEvaluationTimeZoom == o.enableEvaluationTimeZoom;
+        && enableEvaluationTimeZoom == o.enableEvaluationTimeZoom
+        && deviceScaleFactor == o.deviceScaleFactor;
 }
 
 bool StyleRareInheritedData::hasColorFilters() const
@@ -407,6 +410,7 @@ void StyleRareInheritedData::dumpDifferences(TextStream& ts, const StyleRareInhe
     customProperties->dumpDifferences(ts, other.customProperties);
 
     LOG_IF_DIFFERENT(usedZoom);
+    LOG_IF_DIFFERENT(deviceScaleFactor);
 
     LOG_IF_DIFFERENT(listStyleImage);
 

LayoutTests/TestExpectations
@@ -7584,7 +7584,6 @@ fast/canvas/image-buffer-resource-limits.html [ Slow ]
 
 # Standardized CSS zoom tests.
 imported/w3c/web-platform-tests/css/css-viewport/width.html [ ImageOnlyFailure ]
-imported/w3c/web-platform-tests/css/css-viewport/zoom/border-spacing.html [ ImageOnlyFailure ]
 imported/w3c/web-platform-tests/css/css-viewport/zoom/iframe-zoom-nested.html [ ImageOnlyFailure ]
 imported/w3c/web-platform-tests/css/css-viewport/zoom/iframe-zoom.sub.html [ ImageOnlyFailure ]
 imported/w3c/web-platform-tests/css/css-viewport/zoom/line-height.html [ ImageOnlyFailure ]

LayoutTests/fast/sub-pixel/zoomed-em-border.html
@@ -1,3 +1,4 @@
+<!-- webkit-test-runner [ EvaluationTimeZoomEnabled=false ] -->
 <!DOCTYPE html>
 <html>
     <head>

LayoutTests/imported/w3c/web-platform-tests/css/css-backgrounds/animations/border-width-interpolation-expected.txt
@@ -172,28 +172,28 @@ PASS Web Animations: property <border-bottom-width> from [thick] to [15px] at (0
 PASS Web Animations: property <border-bottom-width> from [thick] to [15px] at (1) should be [15px]
 PASS Web Animations: property <border-bottom-width> from [thick] to [15px] at (1.5) should be [20px]
 PASS CSS Transitions: property <border-left-width> from [medium] to [13px] at (-2) should be [0px]
-FAIL CSS Transitions: property <border-left-width> from [medium] to [13px] at (-0.25) should be [0.5px] assert_equals: expected "1px " but got "0.5px "
+PASS CSS Transitions: property <border-left-width> from [medium] to [13px] at (-0.25) should be [0.5px]
 PASS CSS Transitions: property <border-left-width> from [medium] to [13px] at (0) should be [3px]
 PASS CSS Transitions: property <border-left-width> from [medium] to [13px] at (0.3) should be [6px]
 PASS CSS Transitions: property <border-left-width> from [medium] to [13px] at (0.6) should be [9px]
 PASS CSS Transitions: property <border-left-width> from [medium] to [13px] at (1) should be [13px]
 PASS CSS Transitions: property <border-left-width> from [medium] to [13px] at (1.5) should be [18px]
 PASS CSS Transitions with transition: all: property <border-left-width> from [medium] to [13px] at (-2) should be [0px]
-FAIL CSS Transitions with transition: all: property <border-left-width> from [medium] to [13px] at (-0.25) should be [0.5px] assert_equals: expected "1px " but got "0.5px "
+PASS CSS Transitions with transition: all: property <border-left-width> from [medium] to [13px] at (-0.25) should be [0.5px]
 PASS CSS Transitions with transition: all: property <border-left-width> from [medium] to [13px] at (0) should be [3px]
 PASS CSS Transitions with transition: all: property <border-left-width> from [medium] to [13px] at (0.3) should be [6px]
 PASS CSS Transitions with transition: all: property <border-left-width> from [medium] to [13px] at (0.6) should be [9px]
 PASS CSS Transitions with transition: all: property <border-left-width> from [medium] to [13px] at (1) should be [13px]
 PASS CSS Transitions with transition: all: property <border-left-width> from [medium] to [13px] at (1.5) should be [18px]
 PASS CSS Animations: property <border-left-width> from [medium] to [13px] at (-2) should be [0px]
-FAIL CSS Animations: property <border-left-width> from [medium] to [13px] at (-0.25) should be [0.5px] assert_equals: expected "1px " but got "0.5px "
+PASS CSS Animations: property <border-left-width> from [medium] to [13px] at (-0.25) should be [0.5px]
 PASS CSS Animations: property <border-left-width> from [medium] to [13px] at (0) should be [3px]
 PASS CSS Animations: property <border-left-width> from [medium] to [13px] at (0.3) should be [6px]
 PASS CSS Animations: property <border-left-width> from [medium] to [13px] at (0.6) should be [9px]
 PASS CSS Animations: property <border-left-width> from [medium] to [13px] at (1) should be [13px]
 PASS CSS Animations: property <border-left-width> from [medium] to [13px] at (1.5) should be [18px]
 PASS Web Animations: property <border-left-width> from [medium] to [13px] at (-2) should be [0px]
-FAIL Web Animations: property <border-left-width> from [medium] to [13px] at (-0.25) should be [0.5px] assert_equals: expected "1px " but got "0.5px "
+PASS Web Animations: property <border-left-width> from [medium] to [13px] at (-0.25) should be [0.5px]
 PASS Web Animations: property <border-left-width> from [medium] to [13px] at (0) should be [3px]
 PASS Web Animations: property <border-left-width> from [medium] to [13px] at (0.3) should be [6px]
 PASS Web Animations: property <border-left-width> from [medium] to [13px] at (0.6) should be [9px]

LayoutTests/imported/w3c/web-platform-tests/css/css-viewport/zoom/border-image-width-expected.html
@@ -1,0 +1,27 @@
+<!DOCTYPE html>
+<title>CSS zoom applies to border-width when specified and inherited</title>
+<link rel="help" href="https://drafts.csswg.org/css-viewport/">
+
+<style>
+body {
+  --scale: 1;
+}
+.box {
+  width: calc(60px * var(--scale));
+  height: calc(60px * var(--scale));
+  border-image: url("data:image/svg+xml,%3csvg width='100' height='100' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100' height='100' fill='red'/%3e%3c/svg%3e") 30;
+  border-image-width: calc(8px * var(--scale));
+  margin: calc(10px * var(--scale));
+}
+.zoom {
+  --scale: 2;
+}
+</style>
+
+<div class="box"></div>
+
+<div class="zoom">
+  <div class="box"></div>
+</div>
+
+<div class="zoom box"></div>

LayoutTests/imported/w3c/web-platform-tests/css/css-viewport/zoom/border-image-width-ref.html
@@ -1,0 +1,27 @@
+<!DOCTYPE html>
+<title>CSS zoom applies to border-width when specified and inherited</title>
+<link rel="help" href="https://drafts.csswg.org/css-viewport/">
+
+<style>
+body {
+  --scale: 1;
+}
+.box {
+  width: calc(60px * var(--scale));
+  height: calc(60px * var(--scale));
+  border-image: url("data:image/svg+xml,%3csvg width='100' height='100' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100' height='100' fill='red'/%3e%3c/svg%3e") 30;
+  border-image-width: calc(8px * var(--scale));
+  margin: calc(10px * var(--scale));
+}
+.zoom {
+  --scale: 2;
+}
+</style>
+
+<div class="box"></div>
+
+<div class="zoom">
+  <div class="box"></div>
+</div>
+
+<div class="zoom box"></div>

LayoutTests/imported/w3c/web-platform-tests/css/css-viewport/zoom/border-image-width.html
@@ -1,0 +1,24 @@
+<!DOCTYPE html>
+<title>CSS zoom applies to border-image-width when specified and inherited</title>
+<link rel="help" href="https://drafts.csswg.org/css-viewport/">
+<link rel="match" href="border-image-width-ref.html">
+<style>
+.box {
+  width: 60px;
+  height: 60px;
+  border-image: url("data:image/svg+xml,%3csvg width='100' height='100' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100' height='100' fill='red'/%3e%3c/svg%3e") 30;
+  border-image-width: 8px;
+  margin: 10px;
+}
+.zoom {
+  zoom: 2;
+}
+</style>
+
+<div class="box"></div>
+
+<div class="zoom">
+  <div class="box"></div>
+</div>
+
+<div class="zoom box"></div>

LayoutTests/imported/w3c/web-platform-tests/css/css-viewport/zoom/border-width-expected.html
@@ -1,0 +1,38 @@
+<!DOCTYPE html>
+<title>CSS zoom applies to border-width when specified and inherited</title>
+<link rel="help" href="https://drafts.csswg.org/css-viewport/">
+
+<style>
+body {
+  --scale: 1;
+}
+.box {
+  width: calc(60px * var(--scale));
+  height: calc(60px * var(--scale));
+  border: calc(8px * var(--scale)) solid red;
+  margin: calc(10px * var(--scale));
+  display: inline-block;
+  font-size: calc(16px * var(--scale));
+}
+.box-rem {
+  width: calc(60px * var(--scale));
+  height: calc(60px * var(--scale));
+  border: calc(0.5rem * var(--scale)) solid blue;
+  margin: calc(10px * var(--scale));
+  display: inline-block;
+  font-size: calc(1rem * var(--scale));
+}
+.zoom {
+  --scale: 2;
+}
+</style>
+
+<div class="box">unzoomed px</div>
+<div class="box-rem">unzoomed rem</div>
+
+<div class="zoom">
+  <div class="box">zoomed px</div>
+</div>
+
+<div class="zoom box">zoomed inherited px</div>
+<div class="zoom box-rem">zoomed inherited rem</div>

commit 6da2789184a859d723ce27b11afa72f89c5e08db

[WPE] Fix build for ARM 32-bit after 301536@main https://bugs.webkit.org/show_bug.cgi?id=299885
Reviewed by Sam Weinig.

Fix build error in 'StyleRareInheritedData.cpp:74':

error: static assertion failed due to requirement
   74 | static_assert(sizeof(StyleRareInheritedData) <= sizeof(GreaterThanOrSameSizeAsStyleRareInheritedData), "StyleRareInheritedData should bit pack");

note: expression evaluates to '416 <= 408'
   74 | static_assert(sizeof(StyleRareInheritedData) <= sizeof(GreaterThanOrSameSizeAsStyleRareInheritedData), "StyleRareInheritedData should bit pack");

The build error started happening after 301536@main.

This changeset adds a new data field, 'deviceScaleFactor',
to 'StyleRareInheritedData' without updating 'GreaterThanOrSameSizeAsStyleRareInheritedData'.

Still, adding this field alone doesn't fix the build error because
there was another data field, 'textStrokeWidth', pending to be added.

Also, change 'styleImage' data type to 'Style::ImageOrNone'.

* Source/WebCore/rendering/style/StyleRareInheritedData.cpp:

Canonical link: https://commits.webkit.org/301686@main

Source/WebCore/rendering/style/StyleRareInheritedData.cpp
@@ -36,7 +36,9 @@ namespace WebCore {
 
 struct GreaterThanOrSameSizeAsStyleRareInheritedData : public RefCounted<GreaterThanOrSameSizeAsStyleRareInheritedData> {
     float firstFloat;
-    void* styleImage;
+    float secondFloat;
+    Style::ImageOrNone styleImage;
+    Style::WebkitTextStrokeWidth textStrokeWidth;
     Style::Color firstColor;
     Style::Color colors[10];
     Style::ScrollbarColor scrollbarColor;
@@ -44,7 +46,7 @@ struct GreaterThanOrSameSizeAsStyleRareInheritedData : public RefCounted<Greater
     void* ownPtrs[1];
     AtomString atomStrings[5];
     void* refPtrs[3];
-    float secondFloat;
+    float thirdFloat;
     Style::TextEmphasisStyle textEmphasisStyle;
     Style::TextIndent textIndent;
     Style::TextUnderlineOffset offset;