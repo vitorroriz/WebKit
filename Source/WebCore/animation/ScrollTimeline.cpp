abb8f0685c8c878cde5cc75b2fced0c26ff65e5b
Commit Queue <commit-queue@webkit.org>
2025-10-17 06:53:07 -0700

    Unreviewed, reverting 301704@main. https://bugs.webkit.org/show_bug.cgi?id=300980
    mistakenly landed this patch, needs to be reverted (edited)
    
    Reverted changeset:
    
    "Remove use of `RefPtr::unsafeGet()` in `ScrollTimeline::nearestSource()`"
    
    https://bugs.webkit.org/show_bug.cgi?id=300811
    https://commits.webkit.org/301704@main
    Canonical link: https://commits.webkit.org/301706@main

--- Source/WebCore/animation/ScrollTimeline.cpp
+++ Source/WebCore/animation/ScrollTimeline.cpp
@@ -125,16 +125,16 @@ Element* ScrollTimeline::source() const
     case Scroller::Nearest: {
         if (CheckedPtr subjectRenderer = source->renderer()) {
             if (CheckedPtr nearestScrollableContainer = subjectRenderer->enclosingScrollableContainer()) {
-                if (auto* nearestSource = nearestScrollableContainer->element()) {
-                    Ref document = Ref { *nearestSource }->document();
+                if (RefPtr nearestSource = nearestScrollableContainer->element()) {
+                    Ref document = nearestSource->document();
                     RefPtr documentElement = document->documentElement();
                     if (nearestSource != documentElement)
-                        return nearestSource;
+                        return nearestSource.unsafeGet();
                     // RenderObject::enclosingScrollableContainer() will return the document element even in
                     // quirks mode, but the scrolling element in that case is the <body> element, so we must
                     // make sure to return Document::scrollingElement() in case the document element is
                     // returned by enclosingScrollableContainer() but it was not explicitly set as the source.
-                    return &source->element == documentElement ? nearestSource : document->scrollingElement();
+                    return &source->element == documentElement ? nearestSource.unsafeGet() : document->scrollingElement();
                 }
             }
         }
