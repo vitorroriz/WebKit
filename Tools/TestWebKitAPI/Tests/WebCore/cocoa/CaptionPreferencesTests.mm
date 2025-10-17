0a7522e8ec9d6f25a43c20e18d4bfb03e8e0687a
Jer Noble <jer.noble@apple.com>
2025-10-17 09:02:06 -0700

    [Cocoa] More CaptionUserPreferencesMediaAF API tests rdar://162834349 https://bugs.webkit.org/show_bug.cgi?id=300957
    Reviewed by Youenn Fablet.
    
    Add tests for CaptionUserPreferencesMediaAF by shimming more Media Accessibility
    soft linked methods.
    
    * Tools/TestWebKitAPI/Tests/WebCore/cocoa/CaptionPreferencesTests.mm:
    (TestWebKitAPI::MediaAccessibilityShim::resetToDefaultValues):
    (TestWebKitAPI::MediaAccessibilityShim::MediaAccessibilityShim):
    (TestWebKitAPI::MediaAccessibilityShim::~MediaAccessibilityShim):
    (TestWebKitAPI::TEST(CaptionPreferenceTests, FontFace)):
    (TestWebKitAPI::TEST(CaptionPreferenceTests, FontSize)):
    (TestWebKitAPI::TEST(CaptionPreferenceTests, Colors)):
    (TestWebKitAPI::TEST(CaptionPreferenceTests, BorderRadius)):
    (TestWebKitAPI::TEST(CaptionPreferenceTests, TextEdge)):
    
    Canonical link: https://commits.webkit.org/301724@main

--- Tools/TestWebKitAPI/Tests/WebCore/cocoa/CaptionPreferencesTests.mm
+++ Tools/TestWebKitAPI/Tests/WebCore/cocoa/CaptionPreferencesTests.mm
@@ -27,9 +27,11 @@
 
 #import <CoreFoundation/CoreFoundation.h>
 #import <WebCore/CaptionUserPreferencesMediaAF.h>
-#import <WebCore/MediaAccessibilitySoftLink.h>
+#import <WebCore/Color.h>
 #import <WebCore/PageGroup.h>
 #import "SoftLinkShim.h"
+
+#import <WebCore/MediaAccessibilitySoftLink.h>
 
 namespace TestWebKitAPI {
 
@@ -47,6 +49,12 @@ static RetainPtr<resultType> functionName##Result; \
 
 #define DOMAIN_AND_BEHAVIOR MACaptionAppearanceDomain, MACaptionAppearanceBehavior*
 
+#define SOFT_LINK_SHIM_SET_RESULT(functionName, defaultValue) \
+MediaAccessibilityShim::functionName##Result = defaultValue; \
+
+#define SOFT_LINK_SHIM_DEFINE_RESULT(functionName, resultType) \
+resultType MediaAccessibilityShim::functionName##Result; \
+
 class MediaAccessibilityShim {
 public:
     SOFT_LINK_SHIM(MediaAccessibility, MACaptionAppearanceGetDisplayType, MACaptionAppearanceDisplayType, MACaptionAppearanceDomain);
@@ -62,27 +70,43 @@ public:
     SOFT_LINK_SHIM_CF_COPY(MediaAccessibility, MACaptionAppearanceCopySelectedLanguages, CFArrayRef, MACaptionAppearanceDomain);
     SOFT_LINK_SHIM_CF_COPY(MediaAccessibility, MACaptionAppearanceCopyPreferredCaptioningMediaCharacteristics, CFArrayRef, MACaptionAppearanceDomain);
     SOFT_LINK_SHIM_CF_COPY(MediaAccessibility, MACaptionAppearanceCopyFontDescriptorWithStrokeForStyle, CTFontDescriptorRef, DOMAIN_AND_BEHAVIOR, MACaptionAppearanceFontStyle, CFStringRef, CGFloat, CGFloat *);
+    SOFT_LINK_SHIM(MediaAccessibility, MACaptionAppearanceGetTextEdgeStyle, MACaptionAppearanceTextEdgeStyle, DOMAIN_AND_BEHAVIOR);
+
+    void resetToDefaultValues()
+    {
+        SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceGetDisplayType, kMACaptionAppearanceDisplayTypeAutomatic);
+        SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceCopyForegroundColor, CGColorGetConstantColor(kCGColorWhite));
+        SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceCopyBackgroundColor, CGColorGetConstantColor(kCGColorBlack));
+        SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceCopyWindowColor, CGColorGetConstantColor(kCGColorClear));
+        SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceCopyFontDescriptorForStyle, adoptCF(CTFontDescriptorCreateWithNameAndSize(CFSTR(".AppleSystemUIFont"), 10)));
+        SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceGetForegroundOpacity, 1.);
+        SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceGetBackgroundOpacity, 1.);
+        SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceGetWindowOpacity, 1.);
+        SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceGetWindowRoundedCornerRadius, (CGFloat)0.f);
+        SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceCopySelectedLanguages, adoptCF((__bridge CFArrayRef)@[@"English"]));
+        SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceCopyPreferredCaptioningMediaCharacteristics, adoptCF((__bridge CFArrayRef)@[@"MAMediaCharacteristicDescribesMusicAndSoundForAccessibility"]));
+        SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceCopyFontDescriptorWithStrokeForStyle, adoptCF(CTFontDescriptorCreateWithNameAndSize(CFSTR(".AppleSystemUIFont"), 10)));
+        SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceGetRelativeCharacterSize, (CGFloat)1.f);
+        SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceGetTextEdgeStyle, kMACaptionAppearanceTextEdgeStyleNone);
+    }
+    MediaAccessibilityShim() { resetToDefaultValues(); }
+    ~MediaAccessibilityShim() { resetToDefaultValues(); }
 };
 
-#define SOFT_LINK_SHIM_SET_RESULT(functionName, defaultValue) \
-MediaAccessibilityShim::functionName##Result = defaultValue; \
-
-#define SOFT_LINK_SHIM_DEFAULT_RESULT(functionName, defaultValue) \
-auto MediaAccessibilityShim::functionName##Result { defaultValue }; \
-
-SOFT_LINK_SHIM_DEFAULT_RESULT(MACaptionAppearanceGetDisplayType, kMACaptionAppearanceDisplayTypeAutomatic);
-SOFT_LINK_SHIM_DEFAULT_RESULT(MACaptionAppearanceCopyForegroundColor, adoptCF(CGColorGetConstantColor(kCGColorWhite)));
-SOFT_LINK_SHIM_DEFAULT_RESULT(MACaptionAppearanceCopyBackgroundColor, adoptCF(CGColorGetConstantColor(kCGColorBlack)));
-SOFT_LINK_SHIM_DEFAULT_RESULT(MACaptionAppearanceCopyWindowColor, adoptCF(CGColorGetConstantColor(kCGColorClear)));
-SOFT_LINK_SHIM_DEFAULT_RESULT(MACaptionAppearanceCopyFontDescriptorForStyle, adoptCF(CTFontDescriptorCreateWithNameAndSize(CFSTR(".AppleSystemUIFont"), 10)));
-SOFT_LINK_SHIM_DEFAULT_RESULT(MACaptionAppearanceGetForegroundOpacity, 1.);
-SOFT_LINK_SHIM_DEFAULT_RESULT(MACaptionAppearanceGetBackgroundOpacity, 1.);
-SOFT_LINK_SHIM_DEFAULT_RESULT(MACaptionAppearanceGetWindowOpacity, 0.);
-SOFT_LINK_SHIM_DEFAULT_RESULT(MACaptionAppearanceGetWindowRoundedCornerRadius, (CGFloat)0.f);
-SOFT_LINK_SHIM_DEFAULT_RESULT(MACaptionAppearanceCopySelectedLanguages, adoptCF((__bridge CFArrayRef)@[@"English"]));
-SOFT_LINK_SHIM_DEFAULT_RESULT(MACaptionAppearanceCopyPreferredCaptioningMediaCharacteristics, adoptCF((__bridge CFArrayRef)@[@"MAMediaCharacteristicDescribesMusicAndSoundForAccessibility"]));
-SOFT_LINK_SHIM_DEFAULT_RESULT(MACaptionAppearanceCopyFontDescriptorWithStrokeForStyle, adoptCF(CTFontDescriptorCreateWithNameAndSize(CFSTR(".AppleSystemUIFont"), 10)));
-SOFT_LINK_SHIM_DEFAULT_RESULT(MACaptionAppearanceGetRelativeCharacterSize, (CGFloat)1.f);
+SOFT_LINK_SHIM_DEFINE_RESULT(MACaptionAppearanceGetDisplayType, MACaptionAppearanceDisplayType);
+SOFT_LINK_SHIM_DEFINE_RESULT(MACaptionAppearanceCopyForegroundColor, RetainPtr<CGColorRef>);
+SOFT_LINK_SHIM_DEFINE_RESULT(MACaptionAppearanceCopyBackgroundColor, RetainPtr<CGColorRef>);
+SOFT_LINK_SHIM_DEFINE_RESULT(MACaptionAppearanceCopyWindowColor, RetainPtr<CGColorRef>);
+SOFT_LINK_SHIM_DEFINE_RESULT(MACaptionAppearanceGetForegroundOpacity, CGFloat);
+SOFT_LINK_SHIM_DEFINE_RESULT(MACaptionAppearanceGetBackgroundOpacity, CGFloat);
+SOFT_LINK_SHIM_DEFINE_RESULT(MACaptionAppearanceGetWindowOpacity, CGFloat);
+SOFT_LINK_SHIM_DEFINE_RESULT(MACaptionAppearanceGetWindowRoundedCornerRadius, CGFloat);
+SOFT_LINK_SHIM_DEFINE_RESULT(MACaptionAppearanceGetRelativeCharacterSize, CGFloat);
+SOFT_LINK_SHIM_DEFINE_RESULT(MACaptionAppearanceCopyFontDescriptorForStyle, RetainPtr<CTFontDescriptorRef>)
+SOFT_LINK_SHIM_DEFINE_RESULT(MACaptionAppearanceCopySelectedLanguages, RetainPtr<CFArrayRef>);
+SOFT_LINK_SHIM_DEFINE_RESULT(MACaptionAppearanceCopyPreferredCaptioningMediaCharacteristics, RetainPtr<CFArrayRef>);
+SOFT_LINK_SHIM_DEFINE_RESULT(MACaptionAppearanceCopyFontDescriptorWithStrokeForStyle, RetainPtr<CTFontDescriptorRef>);
+SOFT_LINK_SHIM_DEFINE_RESULT(MACaptionAppearanceGetTextEdgeStyle, MACaptionAppearanceTextEdgeStyle);
 
 TEST(CaptionPreferenceTests, ShimTest)
 {
@@ -105,13 +129,85 @@ TEST(CaptionPreferenceTests, FontFace)
     auto preferences = CaptionUserPreferencesMediaAF::create(group);
 
     SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceCopyFontDescriptorForStyle, adoptCF(CTFontDescriptorCreateWithNameAndSize(CFSTR(".AppleSystemUIFontMonospaced-Romulan"), 10)));
-    EXPECT_EQ(preferences->captionsDefaultFontCSS(), "font-family: \"ui-monospace\";"_s);
+    EXPECT_STREQ(preferences->captionsDefaultFontCSS().utf8().data(), "font-family: \"ui-monospace\";");
 
     SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceCopyFontDescriptorForStyle, adoptCF(CTFontDescriptorCreateWithNameAndSize(CFSTR(".AppleSystemUIFont-Klingon"), 10)));
-    EXPECT_EQ(preferences->captionsDefaultFontCSS(), "font-family: \"system-ui\";"_s);
+    EXPECT_STREQ(preferences->captionsDefaultFontCSS().utf8().data(), "font-family: \"system-ui\";");
 
     SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceCopyFontDescriptorForStyle, adoptCF(CTFontDescriptorCreateWithNameAndSize(CFSTR("WingDings"), 10)));
-    EXPECT_EQ(preferences->captionsDefaultFontCSS(), "font-family: \"WingDings\";"_s);
+    EXPECT_STREQ(preferences->captionsDefaultFontCSS().utf8().data(), "font-family: \"WingDings\";");
+}
+
+TEST(CaptionPreferenceTests, FontSize)
+{
+    MediaAccessibilityShim shim;
+
+    PageGroup group { "CaptionPreferenceTests"_s };
+    auto preferences = CaptionUserPreferencesMediaAF::create(group);
+
+    SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceGetRelativeCharacterSize, 2.f);
+    bool important = false;
+    float fontScale = preferences->captionFontSizeScaleAndImportance(important);
+    EXPECT_EQ(fontScale, 0.10f);
+
+    EXPECT_STREQ(preferences->captionsFontSizeCSS().utf8().data(), "font-size: 10cqmin;");
+}
+
+TEST(CaptionPreferenceTests, Colors)
+{
+    MediaAccessibilityShim shim;
+
+    PageGroup group { "CaptionPreferenceTests"_s };
+    auto preferences = CaptionUserPreferencesMediaAF::create(group);
+
+    SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceCopyForegroundColor, cachedCGColor(Color::red));
+    SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceCopyBackgroundColor, cachedCGColor(Color::blue));
+    SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceCopyWindowColor, cachedCGColor(Color::green));
+
+    EXPECT_STREQ(preferences->captionsTextColorCSS().utf8().data(), "color:#ff0000;");
+    EXPECT_STREQ(preferences->captionsBackgroundCSS().utf8().data(), "background-color:#0000ff;");
+    EXPECT_STREQ(preferences->captionsWindowCSS().utf8().data(), "background-color:#00ff00;");
+
+    SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceGetForegroundOpacity, 0.75);
+    SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceGetBackgroundOpacity, 0.5);
+    SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceGetWindowOpacity, 0.25);
+
+    EXPECT_STREQ(preferences->captionsTextColorCSS().utf8().data(), "color:rgba(255, 0, 0, 0.75);");
+    EXPECT_STREQ(preferences->captionsBackgroundCSS().utf8().data(), "background-color:rgba(0, 0, 255, 0.5);");
+    EXPECT_STREQ(preferences->captionsWindowCSS().utf8().data(), "background-color:rgba(0, 255, 0, 0.25);");
+}
+
+TEST(CaptionPreferenceTests, BorderRadius)
+{
+    MediaAccessibilityShim shim;
+
+    PageGroup group { "CaptionPreferenceTests"_s };
+    auto preferences = CaptionUserPreferencesMediaAF::create(group);
+
+    SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceGetWindowRoundedCornerRadius, 8.f);
+    EXPECT_STREQ(preferences->windowRoundedCornerRadiusCSS().utf8().data(), "border-radius:8px;padding:2px;");
+}
+
+TEST(CaptionPreferenceTests, TextEdge)
+{
+    MediaAccessibilityShim shim;
+
+    PageGroup group { "CaptionPreferenceTests"_s };
+    auto preferences = CaptionUserPreferencesMediaAF::create(group);
+
+    EXPECT_STREQ(preferences->captionsTextEdgeCSS().utf8().data(), "");
+
+    SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceGetTextEdgeStyle, kMACaptionAppearanceTextEdgeStyleRaised);
+    EXPECT_STREQ(preferences->captionsTextEdgeCSS().utf8().data(), "text-shadow:-.1em -.1em .16em black;");
+
+    SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceGetTextEdgeStyle, kMACaptionAppearanceTextEdgeStyleDepressed);
+    EXPECT_STREQ(preferences->captionsTextEdgeCSS().utf8().data(), "text-shadow:.1em .1em .16em black;");
+
+    SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceGetTextEdgeStyle, kMACaptionAppearanceTextEdgeStyleDropShadow);
+    EXPECT_STREQ(preferences->captionsTextEdgeCSS().utf8().data(), "text-shadow:0 .1em .16em black;stroke-color:black;paint-order:stroke;stroke-linejoin:round;stroke-linecap:round;");
+
+    SOFT_LINK_SHIM_SET_RESULT(MACaptionAppearanceGetTextEdgeStyle, kMACaptionAppearanceTextEdgeStyleUniform);
+    EXPECT_STREQ(preferences->captionsTextEdgeCSS().utf8().data(), "stroke-color:black;paint-order:stroke;stroke-linejoin:round;stroke-linecap:round;");
 }
 
 }
