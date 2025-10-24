db97275cee41b6b9f14178e295d7243ecdfe257d
Shu-yu Guo <syg@apple.com>
2025-10-23 20:39:25 -0700

    [JSC] Check SAB maxByteLength against MAX_ARRAY_BUFFER_SIZE
    https://bugs.webkit.org/show_bug.cgi?id=301178
    rdar://163099450
    
    Reviewed by Yusuke Suzuki and Dan Hecht.
    
    The SharedArrayBuffer constructor should be checking maxByteLength against
    MAX_ARRAY_BUFFER_SIZE like the ArrayBuffer constructor.
    
    Test: JSTests/stress/shared-array-buffer-large-maxbytelength.js
    Canonical link: https://commits.webkit.org/302068@main

--- Source/JavaScriptCore/runtime/ArrayBuffer.cpp
+++ Source/JavaScriptCore/runtime/ArrayBuffer.cpp
@@ -170,6 +170,7 @@ void ArrayBufferContents::copyTo(ArrayBufferContents& other)
     memcpy(other.data(), data(), m_sizeInBytes);
     other.m_sizeInBytes = m_sizeInBytes;
     RELEASE_ASSERT(other.m_sizeInBytes <= MAX_ARRAY_BUFFER_SIZE);
+    ASSERT(other.m_maxByteLength <= MAX_ARRAY_BUFFER_SIZE);
 }
 
 void ArrayBufferContents::shareWith(ArrayBufferContents& other)
@@ -184,6 +185,7 @@ void ArrayBufferContents::shareWith(ArrayBufferContents& other)
     other.m_maxByteLength = m_maxByteLength;
     other.m_hasMaxByteLength = m_hasMaxByteLength;
     RELEASE_ASSERT(other.m_sizeInBytes <= MAX_ARRAY_BUFFER_SIZE);
+    ASSERT(other.m_maxByteLength <= MAX_ARRAY_BUFFER_SIZE);
 }
 
 Ref<ArrayBuffer> ArrayBuffer::create(size_t numElements, unsigned elementByteSize)
@@ -574,7 +576,7 @@ RefPtr<ArrayBuffer> ArrayBuffer::tryCreateShared(VM& vm, size_t numElements, uns
 {
     CheckedSize sizeInBytes = numElements;
     sizeInBytes *= elementByteSize;
-    if (sizeInBytes.hasOverflowed() || sizeInBytes.value() > MAX_ARRAY_BUFFER_SIZE || (sizeInBytes.value() > maxByteLength))
+    if (sizeInBytes.hasOverflowed() || sizeInBytes.value() > maxByteLength || maxByteLength > MAX_ARRAY_BUFFER_SIZE)
         return nullptr;
 
     auto handle = tryAllocateResizableMemory(&vm, sizeInBytes.value(), maxByteLength);
