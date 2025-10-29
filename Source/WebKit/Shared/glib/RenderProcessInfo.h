vitorroriz/WebKit

commit 76be15a26c9e4509861fb87864afdbadf04e5743

[GTK][WPE] Get render process hardware acceleration information for webkit://gpu from the web process https://bugs.webkit.org/show_bug.cgi?id=301423
Reviewed by Nikolas Zimmermann.

We try to create a PlatformDisplay in the UI process with the same
parameters as the process to get the hardware acceleration information.
We could get the information from the web process using an IPC message
to make sure the data we get is accurate and to allow getting
information for other data we don't currently show because it's not
available in the UI process.

Canonical link: https://commits.webkit.org/302185@main

Source/WebKit/Shared/glib/RenderProcessInfo.h
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2025 Igalia S.L.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <wtf/text/WTFString.h>
+
+namespace WebKit {
+
+struct RenderProcessInfo {
+    String platform;
+    String drmVersion;
+    String glRenderer;
+    String glVendor;
+    String glVersion;
+    String glShadingVersion;
+    String glExtensions;
+    String eglVersion;
+    String eglVendor;
+    String eglExtensions;
+};
+
+} // namespace WebKit

Source/WebKit/Shared/glib/RenderProcessInfo.serialization.in
@@ -1,0 +1,34 @@
+# Copyright (C) 2025 Igalia, S.L.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1.  Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+# 2.  Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+struct WebKit::RenderProcessInfo {
+    String platform;
+    String drmVersion;
+    String glRenderer;
+    String glVendor;
+    String glVersion;
+    String glShadingVersion;
+    String glExtensions;
+    String eglVersion;
+    String eglVendor;
+    String eglExtensions;
+}

commit e36b742a17b74dd86287584684457d32e4dcf332

[GTK][WPE] Add rendering and buffer formats information to webkit://gpu https://bugs.webkit.org/show_bug.cgi?id=301623
Reviewed by Adrian Perez de Castro.

Add information about threaded rendering, MSAA, preferred buffer formats
and supported buffer formats. This patch laso fixes the JSON output
that I broke in 302185@main.

Canonical link: https://commits.webkit.org/302293@main

Source/WebKit/Shared/glib/RenderProcessInfo.h
@@ -25,6 +25,7 @@
 
 #pragma once
 
+#include "RendererBufferFormat.h"
 #include <wtf/text/WTFString.h>
 
 namespace WebKit {
@@ -40,6 +41,10 @@ struct RenderProcessInfo {
     String eglVersion;
     String eglVendor;
     String eglExtensions;
+    unsigned cpuPaintingThreadsCount { 0 };
+    unsigned gpuPaintingThreadsCount { 0 };
+    unsigned msaaSampleCount { 0 };
+    Vector<RendererBufferFormat::Format> supportedBufferFormats;
 };
 
 } // namespace WebKit

Source/WebKit/Shared/glib/RenderProcessInfo.serialization.in
@@ -31,4 +31,8 @@ struct WebKit::RenderProcessInfo {
     String eglVersion;
     String eglVendor;
     String eglExtensions;
+    unsigned cpuPaintingThreadsCount;
+    unsigned gpuPaintingThreadsCount;
+    unsigned msaaSampleCount;
+    Vector<WebKit::RendererBufferFormat::Format> supportedBufferFormats;
 }

Source/WebKit/UIProcess/API/glib/WebKitProtocolHandler.cpp
@@ -84,8 +84,6 @@
 #include <gst/gst.h>
 #endif
 
-WTF_ALLOW_UNSAFE_BUFFER_USAGE_BEGIN // GTK/WPE port
-
 namespace WebKit {
 using namespace WebCore;
 
@@ -212,42 +210,57 @@ static String dmabufRendererWithSupportedBuffers()
 
 #if USE(LIBDRM)
 
-// Cherry-pick function 'drmGetFormatName' from 'https://gitlab.freedesktop.org/mesa/drm/-/blob/main/xf86drm.c'.
-// Function is only available since version '2.4.113'. Debian 11 ships '2.4.104'.
-// FIXME: Remove when Debian 11 support ends.
-static char* webkitDrmGetFormatName(uint32_t format)
+// Base on function 'drmGetFormatName' from 'https://gitlab.freedesktop.org/mesa/drm/-/blob/main/xf86drm.c'.
+static String webkitDrmGetFormatName(uint32_t format)
 {
-    char* str;
-    char code[5];
-    const char* be;
-    size_t strSize, i;
-
-    // Is format big endian?
-    be = (format & (1U<<31)) ? "_BE" : "";
+    bool isBigEndian = (format & (1U << 31));
     format &= ~(1U<<31);
 
     // If format is DRM_FORMAT_INVALID.
     if (!format)
-        return strdup("INVALID");
+        return "INVALID"_s;
 
-    code[0] = (char) ((format >> 0) & 0xFF);
-    code[1] = (char) ((format >> 8) & 0xFF);
-    code[2] = (char) ((format >> 16) & 0xFF);
-    code[3] = (char) ((format >> 24) & 0xFF);
-    code[4] = '\0';
+    std::span<char> buffer;
+    CString code = CString::newUninitialized(4, buffer);
+    buffer[0] = static_cast<char>((format >> 0) & 0xFF);
+    buffer[1] = static_cast<char>((format >> 8) & 0xFF);
+    buffer[2] = static_cast<char>((format >> 16) & 0xFF);
+    buffer[3] = static_cast<char>((format >> 24) & 0xFF);
 
     // Trim spaces at the end.
-    for (i = 3; i > 0 && code[i] == ' '; i--)
-        code[i] = '\0';
+    for (size_t i = 3; i > 0 && buffer[i] == ' '; --i)
+        buffer[i] = '\0';
 
-    strSize = strlen(code) + strlen(be) + 1;
-    str = static_cast<char*>(malloc(strSize));
-    if (!str)
-        return nullptr;
+    return makeString(code, isBigEndian ? "_BE"_s : ""_s);
+}
 
-    snprintf(str, strSize, "%s%s", code, be);
+static String webkitDrmGetModifierName(uint64_t modifier)
+{
+#if HAVE(DRM_GET_FORMAT_MODIFIER_VENDOR) && HAVE(DRM_GET_FORMAT_MODIFIER_NAME)
+    std::unique_ptr<char, decltype(free)*> modifierVendor(drmGetFormatModifierVendor(modifier), free);
+    std::unique_ptr<char, decltype(free)*> modifierName(drmGetFormatModifierName(modifier), free);
+    return makeString(String::fromUTF8(modifierVendor.get()), "_"_s, String::fromUTF8(modifierName.get()));
+#else
+    return { }
+#endif
+}
 
-    return str;
+static String modifierListToString(const Vector<uint64_t, 1>& modifiers)
+{
+    if (modifiers.isEmpty())
+        return { };
+
+#if HAVE(DRM_GET_FORMAT_MODIFIER_VENDOR) && HAVE(DRM_GET_FORMAT_MODIFIER_NAME)
+    StringBuilder builder;
+    for (auto modifier : modifiers) {
+        if (!builder.isEmpty())
+            builder.append(", "_s);
+        builder.append(webkitDrmGetModifierName(modifier));
+    }
+    return makeString(" ("_s, builder.toString(), ")"_s);
+#else
+    return { };
+#endif
 }
 
 static String renderBufferDescription(WebKitURISchemeRequest* request)
@@ -255,25 +268,20 @@ static String renderBufferDescription(WebKitURISchemeRequest* request)
     StringBuilder bufferDescription;
     auto description = webkitWebViewGetRendererBufferDescription(webkit_uri_scheme_request_get_web_view(request));
     if (description.fourcc) {
-        auto* formatName = webkitDrmGetFormatName(description.fourcc);
+        auto formatName = webkitDrmGetFormatName(description.fourcc);
         switch (description.type) {
         case RendererBufferDescription::Type::DMABuf: {
-#if HAVE(DRM_GET_FORMAT_MODIFIER_VENDOR) && HAVE(DRM_GET_FORMAT_MODIFIER_NAME)
-            auto* modifierVendor = drmGetFormatModifierVendor(description.modifier);
-            auto* modifierName = drmGetFormatModifierName(description.modifier);
-            bufferDescription.append("DMA-BUF: "_s, String::fromUTF8(formatName), " ("_s, String::fromUTF8(modifierVendor), "_"_s, String::fromUTF8(modifierName), ")"_s);
-            free(modifierVendor);
-            free(modifierName);
-#else
-            bufferDescription.append("Unknown"_s);
-#endif
+            auto modifierName = webkitDrmGetModifierName(description.modifier);
+            if (!modifierName.isNull())
+                bufferDescription.append("DMA-BUF: "_s, formatName, " ("_s, modifierName, ")"_s);
+            else
+                bufferDescription.append("Unknown"_s);
             break;
         }
         case RendererBufferDescription::Type::SharedMemory:
-            bufferDescription.append("Shared Memory: "_s, String::fromUTF8(formatName));
+            bufferDescription.append("Shared Memory: "_s, formatName);
             break;
         }
-        free(formatName);
         switch (description.usage) {
         case RendererBufferFormat::Usage::Rendering:
             bufferDescription.append(" [Rendering]"_s);
@@ -290,8 +298,61 @@ static String renderBufferDescription(WebKitURISchemeRequest* request)
 
     return bufferDescription.toString();
 }
-#endif
-#endif
+
+#if USE(GBM)
+static String preferredBufferFormats(WebKitURISchemeRequest* request, JSON::Array& jsonArray)
+{
+    auto& page = webkitURISchemeRequestGetWebPage(request);
+    auto formats = page.preferredBufferFormats();
+    StringBuilder builder;
+    builder.append("<ul>"_s);
+    for (const auto& tranche : formats) {
+        auto jsonObject = JSON::Object::create();
+        builder.append("<li>Formats for "_s);
+        switch (tranche.usage) {
+        case RendererBufferFormat::Usage::Rendering:
+            builder.append("<b>rendering</b> using device <i>"_s, !tranche.drmDevice.renderNode.isNull() ? tranche.drmDevice.renderNode : tranche.drmDevice.primaryNode, "</i>"_s);
+            jsonObject->setString("Usage"_s, "Rendering"_s);
+            jsonObject->setString("Device"_s, String::fromUTF8(!tranche.drmDevice.renderNode.isNull() ? tranche.drmDevice.renderNode.span() : tranche.drmDevice.primaryNode.span()));
+            break;
+        case RendererBufferFormat::Usage::Scanout:
+            builder.append("<b>scanout</b> using device <i>"_s, tranche.drmDevice.primaryNode, "</i>"_s);
+            jsonObject->setString("Usage"_s, "Scanout"_s);
+            jsonObject->setString("Device"_s, String::fromUTF8(tranche.drmDevice.primaryNode.span()));
+            break;
+        case RendererBufferFormat::Usage::Mapping:
+            builder.append("<b>mapping</b> using device <i>"_s, tranche.drmDevice.primaryNode, "</i>"_s);
+            jsonObject->setString("Usage"_s, "Mapping"_s);
+            jsonObject->setString("Device"_s, String::fromUTF8(tranche.drmDevice.primaryNode.span()));
+            break;
+        }
+        builder.append("<br>"_s);
+        auto jsonFormats = JSON::Array::create();
+        StringBuilder formatsBuilder;
+        for (const auto& format : tranche.formats) {
+            StringBuilder jsonStringBuilder;
+            if (!formatsBuilder.isEmpty())
+                formatsBuilder.append("<br>"_s);
+            auto formatName = webkitDrmGetFormatName(format.fourcc);
+            formatsBuilder.append("<b>"_s, formatName, "</b>"_s);
+            jsonStringBuilder.append(formatName);
+            auto modifiers = modifierListToString(format.modifiers);
+            if (!modifiers.isNull()) {
+                formatsBuilder.append(modifiers);
+                jsonStringBuilder.append(modifiers);
+            }
+            jsonFormats->pushString(jsonStringBuilder.toString());
+        }
+        builder.append(formatsBuilder.toString());
+        jsonObject->setArray("Formats"_s, WTFMove(jsonFormats));
+        jsonArray.pushObject(WTFMove(jsonObject));
+    }
+    builder.append("</ul>"_s);
+    return builder.toString();
+}
+#endif // USE(GBM)
+#endif // USE(LIBDRM)
+#endif // PLATFORM(GTK) || (PLATFORM(WPE) && ENABLE(WPE_PLATFORM))
 
 static String vblankMonitorType(const DisplayVBlankMonitor& monitor)
 {
@@ -304,6 +365,44 @@ static String vblankMonitorType(const DisplayVBlankMonitor& monitor)
 
     return monitor.type() == DisplayVBlankMonitor::Type::Timer ? "Timer"_s : "DRM"_s;
 }
+
+#if USE(SKIA)
+static String threadedRenderingInfo(const RenderProcessInfo& info)
+{
+    if (!info.cpuPaintingThreadsCount && !info.gpuPaintingThreadsCount)
+        return "Disabled"_s;
+
+    if (!info.gpuPaintingThreadsCount)
+        return makeString("CPU ("_s, info.cpuPaintingThreadsCount, " threads)"_s);
+
+    if (!info.cpuPaintingThreadsCount)
+        return makeString("GPU ("_s, info.gpuPaintingThreadsCount, " threads)"_s);
+
+    return makeString("GPU ("_s, info.gpuPaintingThreadsCount, " threads), CPU ("_s, info.cpuPaintingThreadsCount, " threads)"_s);
+}
+#endif
+
+#if USE(LIBDRM)
+static String supportedBufferFormats(const RenderProcessInfo& info, JSON::Array& jsonArray)
+{
+    StringBuilder builder;
+    for (const auto& format : info.supportedBufferFormats) {
+        StringBuilder jsonStringBuilder;
+        auto formatName = webkitDrmGetFormatName(format.fourcc);
+        if (!builder.isEmpty())
+            builder.append("<br>"_s);
+        builder.append("<b>"_s, formatName, "</b>"_s);
+        jsonStringBuilder.append(formatName);
+        auto modifiers = modifierListToString(format.modifiers);
+        if (!modifiers.isNull()) {
+            builder.append(modifiers);
+            jsonStringBuilder.append(modifiers);
+        }
+        jsonArray.pushString(jsonStringBuilder.toString());
+    }
+    return builder.toString();
+}
+#endif
 
 static String prettyPrintJSON(const String& jsonString)
 {
@@ -393,9 +492,12 @@ void WebKitProtocolHandler::handleGPU(WebKitURISchemeRequest* request, RenderPro
         tablesBuilder.append("<h1>"_s, header, "</h1><table>"_s);
     };
 
-    auto addTableRow = [&](auto& jsonObject, auto key, auto&& value) {
+    auto addTableRow = [&](auto& jsonObject, auto key, auto&& value, RefPtr<JSON::Value>&& jsonValue = nullptr) {
         tablesBuilder.append("<tbody><tr><td><div class=\"titlename\">"_s, key, "</div></td><td>"_s, value, "</td></tr></tbody>"_s);
-        jsonObject->setString(key, value);
+        if (jsonValue)
+            jsonObject->setValue(key, jsonValue.releaseNonNull());
+        else
+            jsonObject->setString(key, value);
     };
 
     auto stopTable = [&] {
@@ -539,22 +641,26 @@ void WebKitProtocolHandler::handleGPU(WebKitURISchemeRequest* request, RenderPro
 #endif
 
     if (policy != "never"_s) {
-        addTableRow(jsonObject, "API"_s, String::fromUTF8(openGLAPI()));
+        addTableRow(hardwareAccelerationObject, "API"_s, String::fromUTF8(openGLAPI()));
 #if PLATFORM(GTK)
-        if (usingDMABufRenderer) {
-            addTableRow(hardwareAccelerationObject, "Renderer"_s, dmabufRendererWithSupportedBuffers());
-#if USE(LIBDRM)
-            addTableRow(hardwareAccelerationObject, "Buffer format"_s, renderBufferDescription(request));
-#endif
-        }
+        bool showBuffersInfo = usingDMABufRenderer;
 #elif PLATFORM(WPE) && ENABLE(WPE_PLATFORM)
-        if (usingWPEPlatformAPI) {
+        bool showBuffersInfo = usingWPEPlatformAPI;
+#else
+        bool showBuffersInfo = false;
+#endif
+        if (showBuffersInfo) {
             addTableRow(hardwareAccelerationObject, "Renderer"_s, dmabufRendererWithSupportedBuffers());
 #if USE(LIBDRM)
+#if USE(GBM)
+            auto jsonFormats = JSON::Array::create();
+            auto formatsString = preferredBufferFormats(request, jsonFormats.get());
+            addTableRow(hardwareAccelerationObject, "Preferred buffer formats"_s, formatsString, WTFMove(jsonFormats));
+#endif
             addTableRow(hardwareAccelerationObject, "Buffer format"_s, renderBufferDescription(request));
 #endif
         }
-#endif
+
         addTableRow(hardwareAccelerationObject, "Native interface"_s, uiProcessContextIsEGL() ? "EGL"_s : "None"_s);
 
         if (uiProcessContextIsEGL() && eglGetCurrentContext() != EGL_NO_CONTEXT)
@@ -573,18 +679,33 @@ void WebKitProtocolHandler::handleGPU(WebKitURISchemeRequest* request, RenderPro
         if (!info.drmVersion.isEmpty())
             addTableRow(hardwareAccelerationObject, "DRM version"_s, info.drmVersion);
 
-        addTableRow(jsonObject, "GL_RENDERER"_s, info.glRenderer);
-        addTableRow(jsonObject, "GL_VENDOR"_s, info.glVendor);
-        addTableRow(jsonObject, "GL_VERSION"_s, info.glVersion);
-        addTableRow(jsonObject, "GL_SHADING_LANGUAGE_VERSION"_s, info.glShadingVersion);
-        addTableRow(jsonObject, "GL_EXTENSIONS"_s, info.glExtensions);
-        addTableRow(jsonObject, "EGL_VERSION"_s, info.eglVersion);
-        addTableRow(jsonObject, "EGL_VENDOR"_s, info.eglVendor);
-        addTableRow(jsonObject, "EGL_EXTENSIONS"_s, info.eglExtensions);
+#if USE(SKIA)
+        addTableRow(hardwareAccelerationObject, "Threaded rendering"_s, threadedRenderingInfo(info));
+        addTableRow(hardwareAccelerationObject, "MSAA"_s, info.msaaSampleCount ? makeString(info.msaaSampleCount, " samples"_s) : String("Disabled"_s));
+#endif
+
+#if USE(LIBDRM)
+        if (!info.supportedBufferFormats.isEmpty()) {
+            auto jsonFormats = JSON::Array::create();
+            auto formatsString = supportedBufferFormats(info, jsonFormats.get());
+            addTableRow(hardwareAccelerationObject, "Supported buffers"_s, formatsString, WTFMove(jsonFormats));
+        }
+#endif
+
+        addTableRow(hardwareAccelerationObject, "GL_RENDERER"_s, info.glRenderer);
+        addTableRow(hardwareAccelerationObject, "GL_VENDOR"_s, info.glVendor);
+        addTableRow(hardwareAccelerationObject, "GL_VERSION"_s, info.glVersion);
+        addTableRow(hardwareAccelerationObject, "GL_SHADING_LANGUAGE_VERSION"_s, info.glShadingVersion);
+        addTableRow(hardwareAccelerationObject, "GL_EXTENSIONS"_s, info.glExtensions);
+        addTableRow(hardwareAccelerationObject, "EGL_VERSION"_s, info.eglVersion);
+        addTableRow(hardwareAccelerationObject, "EGL_VENDOR"_s, info.eglVendor);
+        addTableRow(hardwareAccelerationObject, "EGL_EXTENSIONS"_s, info.eglExtensions);
 
         stopTable();
         jsonObject->setObject("Hardware Acceleration Information (Render process)"_s, WTFMove(hardwareAccelerationObject));
     }
+
+    WTF_ALLOW_UNSAFE_BUFFER_USAGE_BEGIN // GTK/WPE port
 
     auto infoAsString = jsonObject->toJSONString();
     g_string_append_printf(html, "<script>function copyAsJSON() { "
@@ -602,6 +723,9 @@ void WebKitProtocolHandler::handleGPU(WebKitURISchemeRequest* request, RenderPro
 #endif
     g_string_append(html, "<button onclick=\"window.location.href='webkit://gpu/stdout'\">Print in stdout</button>");
     g_string_append_printf(html, "%s</body></html>", tablesBuilder.toString().utf8().data());
+
+    WTF_ALLOW_UNSAFE_BUFFER_USAGE_END
+
     gsize streamLength = html->len;
     GRefPtr<GInputStream> stream = adoptGRef(g_memory_input_stream_new_from_data(g_string_free(html, FALSE), streamLength, g_free));
     webkit_uri_scheme_request_finish(request, stream.get(), streamLength, "text/html");
@@ -612,4 +736,3 @@ void WebKitProtocolHandler::handleGPU(WebKitURISchemeRequest* request, RenderPro
 
 } // namespace WebKit
 
-WTF_ALLOW_UNSAFE_BUFFER_USAGE_END

Source/WebKit/UIProcess/WebPageProxy.h
@@ -2837,6 +2837,10 @@ public:
     bool toolbarsAreVisible() const { return m_toolbarsAreVisible; }
     void setToolbarsAreVisible(bool);
 
+#if USE(GBM) && (PLATFORM(GTK) || PLATFORM(WPE))
+    Vector<RendererBufferFormat> preferredBufferFormats() const;
+#endif
+
 private:
     WebPageProxy(PageClient&, WebProcessProxy&, Ref<API::PageConfiguration>&&);
     void platformInitialize();
@@ -3089,9 +3093,6 @@ private:
 #if PLATFORM(GTK) || PLATFORM(WPE)
     void bindAccessibilityTree(const String&);
     OptionSet<WebCore::PlatformEventModifier> currentStateOfModifierKeys();
-#if USE(GBM)
-    Vector<RendererBufferFormat> preferredBufferFormats() const;
-#endif
 #endif
 
 #if PLATFORM(GTK)

Source/WebKit/WebProcess/WebPage/CoordinatedGraphics/LayerTreeHost.cpp
@@ -32,6 +32,7 @@
 #if USE(COORDINATED_GRAPHICS)
 #include "CoordinatedSceneState.h"
 #include "DrawingArea.h"
+#include "RenderProcessInfo.h"
 #include "WebPageInlines.h"
 #include "WebPageProxyMessages.h"
 #include "WebProcess.h"
@@ -619,6 +620,10 @@ void LayerTreeHost::foreachRegionInDamageHistoryForTesting(Function<void(const R
 
 void LayerTreeHost::fillGLInformation(RenderProcessInfo&& info, CompletionHandler<void(RenderProcessInfo&&)>&& completionHandler)
 {
+#if USE(SKIA)
+    info.cpuPaintingThreadsCount = SkiaPaintingEngine::numberOfCPUPaintingThreads();
+    info.gpuPaintingThreadsCount = SkiaPaintingEngine::numberOfGPUPaintingThreads();
+#endif
     m_compositor->fillGLInformation(WTFMove(info), WTFMove(completionHandler));
 }
 

Source/WebKit/WebProcess/WebPage/glib/WebPageGLib.cpp
@@ -287,6 +287,16 @@ void WebPage::getRenderProcessInfo(CompletionHandler<void(RenderProcessInfo&&)>&
         break;
     }
 
+#if USE(SKIA)
+    info.msaaSampleCount = display->msaaSampleCount();
+#endif
+
+    if (info.platform != "WPE"_s) {
+        info.supportedBufferFormats = display->bufferFormats().map([](const auto& format) -> RendererBufferFormat::Format {
+            return { format.fourcc.value, format.modifiers };
+        });
+    }
+
     static_cast<DrawingAreaCoordinatedGraphics*>(m_drawingArea.get())->fillGLInformation(WTFMove(info), WTFMove(completionHandler));
 }
 