f64f6840f2d5b0f631f6baa8d6d2366ff4755370
Chris Dumez <cdumez@apple.com>
2025-10-29 19:19:53 -0700

    Drop IsDeprecatedWeakRefSmartPointerException for RTCIceTransportBackendClient https://bugs.webkit.org/show_bug.cgi?id=301620
    Reviewed by Geoffrey Garen.
    
    * Source/WebCore/Modules/mediastream/RTCIceTransportBackend.h:
    * Source/WebCore/Modules/mediastream/libwebrtc/LibWebRTCIceTransportBackend.cpp:
    (WebCore::LibWebRTCIceTransportBackendObserver::start):
    (WebCore::LibWebRTCIceTransportBackendObserver::onIceTransportStateChanged):
    (WebCore::LibWebRTCIceTransportBackendObserver::onGatheringStateChanged):
    (WebCore::LibWebRTCIceTransportBackendObserver::processSelectedCandidatePairChanged):
    
    Canonical link: https://commits.webkit.org/302326@main

--- Source/WebCore/Modules/mediastream/libwebrtc/LibWebRTCIceTransportBackend.cpp
+++ Source/WebCore/Modules/mediastream/libwebrtc/LibWebRTCIceTransportBackend.cpp
@@ -118,15 +118,16 @@ void LibWebRTCIceTransportBackendObserver::start()
         // We start observing a bit late and might miss the checking state. Synthesize it as needed.
         if (transportState > webrtc::IceTransportState::kChecking && transportState != webrtc::IceTransportState::kClosed) {
             callOnMainThread([protectedThis = Ref { *this }] {
-                if (protectedThis->m_client)
-                    protectedThis->m_client->onStateChanged(RTCIceTransportState::Checking);
+                if (RefPtr client = protectedThis->m_client.get())
+                    client->onStateChanged(RTCIceTransportState::Checking);
             });
         }
         callOnMainThread([protectedThis = Ref { *this }, transportState, gatheringState = internal->gathering_state()] {
-            if (!protectedThis->m_client)
+            RefPtr client = protectedThis->m_client.get();
+            if (!client)
                 return;
-            protectedThis->m_client->onStateChanged(toRTCIceTransportState(transportState));
-            protectedThis->m_client->onGatheringStateChanged(toRTCIceGatheringState(gatheringState));
+            client->onStateChanged(toRTCIceTransportState(transportState));
+            client->onGatheringStateChanged(toRTCIceGatheringState(gatheringState));
         });
 
         if (auto candidatePair = internal->GetSelectedCandidatePair())
@@ -150,16 +151,16 @@ void LibWebRTCIceTransportBackendObserver::stop()
 void LibWebRTCIceTransportBackendObserver::onIceTransportStateChanged(webrtc::IceTransportInternal* internal)
 {
     callOnMainThread([protectedThis = Ref { *this }, state = internal->GetIceTransportState()] {
-        if (protectedThis->m_client)
-            protectedThis->m_client->onStateChanged(toRTCIceTransportState(state));
+        if (RefPtr client = protectedThis->m_client.get())
+            client->onStateChanged(toRTCIceTransportState(state));
     });
 }
 
 void LibWebRTCIceTransportBackendObserver::onGatheringStateChanged(webrtc::IceTransportInternal* internal)
 {
     callOnMainThread([protectedThis = Ref { *this }, state = internal->gathering_state()] {
-        if (protectedThis->m_client)
-            protectedThis->m_client->onGatheringStateChanged(toRTCIceGatheringState(state));
+        if (RefPtr client = protectedThis->m_client.get())
+            client->onGatheringStateChanged(toRTCIceGatheringState(state));
     });
 }
 
@@ -172,12 +173,13 @@ void LibWebRTCIceTransportBackendObserver::onNetworkRouteChanged(std::optional<w
 void LibWebRTCIceTransportBackendObserver::processSelectedCandidatePairChanged(const webrtc::Candidate& local, const webrtc::Candidate& remote)
 {
     callOnMainThread([protectedThis = Ref { *this }, localSdp = fromStdString(local.ToString()).isolatedCopy(), remoteSdp = fromStdString(remote.ToString()).isolatedCopy(), localFields = convertIceCandidate(local).isolatedCopy(), remoteFields = convertIceCandidate(remote).isolatedCopy()]() mutable {
-        if (!protectedThis->m_client)
+        RefPtr client = protectedThis->m_client.get();
+        if (!client)
             return;
 
         auto local = RTCIceCandidate::create(localSdp, emptyString(), WTFMove(localFields));
         auto remote = RTCIceCandidate::create(remoteSdp, emptyString(), WTFMove(remoteFields));
-        protectedThis->m_client->onSelectedCandidatePairChanged(WTFMove(local), WTFMove(remote));
+        client->onSelectedCandidatePairChanged(WTFMove(local), WTFMove(remote));
     });
 }
 
