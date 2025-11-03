3861878b6d62ccf4b4d9100ecf600b76d1f9efd5
Gavin Phillips <gavin.p@apple.com>
2025-11-03 13:03:36 -0800

    WebKit::InteractionInformationAtPosition should use a Vector for serialization not an opaque NSArray https://bugs.webkit.org/show_bug.cgi?id=301691 rdar://163708795
    Reviewed by Abrar Rahman Protyasha.
    
    Ports InteractionInformationAtPosition to serialize it's DataDetectorResults
    as a Vector<RetainPtr<DDScannerResults>> instead of an opaque NSArray.
    
    * Source/WebKit/Shared/ios/InteractionInformationAtPosition.h:
    * Source/WebKit/Shared/ios/InteractionInformationAtPosition.mm:
    (WebKit::InteractionInformationAtPosition::InteractionInformationAtPosition):
    (WebKit::hostImageOrVideoElementContext):
    (WebKit::InteractionInformationAtPosition::serializableDataDetectorResults const):
    * Source/WebKit/Shared/ios/InteractionInformationAtPosition.serialization.in:
    
    Canonical link: https://commits.webkit.org/302496@main

--- Source/WebKit/Shared/ios/InteractionInformationAtPosition.mm
+++ Source/WebKit/Shared/ios/InteractionInformationAtPosition.mm
@@ -27,11 +27,96 @@
 #import "InteractionInformationAtPosition.h"
 
 #import "ArgumentCodersCocoa.h"
+#import <wtf/cocoa/VectorCocoa.h>
 #import <pal/cocoa/DataDetectorsCoreSoftLink.h>
 
 namespace WebKit {
 
 #if PLATFORM(IOS_FAMILY)
+
+InteractionInformationAtPosition::InteractionInformationAtPosition(InteractionInformationRequest&& request, bool canBeValid, std::optional<bool> hitNodeOrWindowHasDoubleClickListener, Selectability&& selectability, bool isSelected, bool prefersDraggingOverTextSelection, bool isNearMarkedText, bool touchCalloutEnabled, bool isLink, bool isImage,
+#if ENABLE(MODEL_PROCESS)
+    bool isInteractiveModel,
+#endif
+    bool isAttachment, bool isAnimatedImage, bool isAnimating, bool canShowAnimationControls, bool isPausedVideo, bool isElement, bool isContentEditable, Markable<WebCore::ScrollingNodeID>&& containerScrollingNodeID,
+#if ENABLE(DATA_DETECTION)
+    bool isDataDetectorLink,
+#endif
+    bool preventTextInteraction, bool elementContainsImageOverlay, bool isImageOverlayText,
+#if ENABLE(SPATIAL_IMAGE_DETECTION)
+    bool isSpatialImage,
+#endif
+    bool isInPlugin, bool needsPointerTouchCompatibilityQuirk, WebCore::FloatPoint&& adjustedPointForNodeRespondingToClickEvents, URL&& url, URL&& imageURL, String&& imageMIMEType, String&& title, String&& idAttribute, WebCore::IntRect&& bounds,
+#if PLATFORM(MACCATALYST)
+    WebCore::IntRect&& caretRect,
+#endif
+    RefPtr<WebCore::ShareableBitmap>&& image, String&& textBefore, String&& textAfter, CursorContext&& cursorContext, RefPtr<WebCore::TextIndicator>&& textIndicator,
+#if ENABLE(DATA_DETECTION)
+    String&& dataDetectorIdentifier, Vector<RetainPtr<DDScannerResult>>&& dataDetectorResults, WebCore::IntRect&& dataDetectorBounds,
+#endif
+#if ENABLE(ACCESSIBILITY_ANIMATION_CONTROL)
+    Vector<WebCore::ElementAnimationContext>&& animationsAtPoint,
+#endif
+    std::optional<WebCore::ElementContext>&& elementContext, std::optional<WebCore::ElementContext>&& hostImageOrVideoElementContext)
+        : request(WTFMove(request))
+        , canBeValid(canBeValid)
+        , hitNodeOrWindowHasDoubleClickListener(hitNodeOrWindowHasDoubleClickListener)
+        , selectability(selectability)
+        , isSelected(isSelected)
+        , prefersDraggingOverTextSelection(prefersDraggingOverTextSelection)
+        , isNearMarkedText(isNearMarkedText)
+        , touchCalloutEnabled(touchCalloutEnabled)
+        , isLink(isLink)
+        , isImage(isImage)
+#if ENABLE(MODEL_PROCESS)
+        , isInteractiveModel(isInteractiveModel)
+#endif
+        , isAttachment(isAttachment)
+        , isAnimatedImage(isAnimatedImage)
+        , isAnimating(isAnimating)
+        , canShowAnimationControls(canShowAnimationControls)
+        , isPausedVideo(isPausedVideo)
+        , isElement(isElement)
+        , isContentEditable(isContentEditable)
+        , containerScrollingNodeID(WTFMove(containerScrollingNodeID))
+#if ENABLE(DATA_DETECTION)
+        , isDataDetectorLink(isDataDetectorLink)
+#endif
+        , preventTextInteraction(preventTextInteraction)
+        , elementContainsImageOverlay(elementContainsImageOverlay)
+        , isImageOverlayText(isImageOverlayText)
+#if ENABLE(SPATIAL_IMAGE_DETECTION)
+        , isSpatialImage(isSpatialImage)
+#endif
+        , isInPlugin(isInPlugin)
+        , needsPointerTouchCompatibilityQuirk(needsPointerTouchCompatibilityQuirk)
+        , adjustedPointForNodeRespondingToClickEvents(WTFMove(adjustedPointForNodeRespondingToClickEvents))
+        , url(WTFMove(url))
+        , imageURL(WTFMove(imageURL))
+        , imageMIMEType(WTFMove(imageMIMEType))
+        , title(WTFMove(title))
+        , idAttribute(WTFMove(idAttribute))
+        , bounds(WTFMove(bounds))
+#if PLATFORM(MACCATALYST)
+        , caretRect(WTFMove(caretRect))
+#endif
+        , image(WTFMove(image))
+        , textBefore(WTFMove(textBefore))
+        , textAfter(WTFMove(textAfter))
+        , cursorContext(WTFMove(cursorContext))
+        , textIndicator(WTFMove(textIndicator))
+#if ENABLE(DATA_DETECTION)
+        , dataDetectorIdentifier(WTFMove(dataDetectorIdentifier))
+        , dataDetectorResults(createNSArray(WTFMove(dataDetectorResults), [](RetainPtr<DDScannerResult>&& result) { return result.get(); }))
+        , dataDetectorBounds(WTFMove(dataDetectorBounds))
+#endif
+#if ENABLE(ACCESSIBILITY_ANIMATION_CONTROL)
+        , animationsAtPoint(WTFMove(animationsAtPoint))
+#endif
+        , elementContext(WTFMove(elementContext))
+        , hostImageOrVideoElementContext(WTFMove(hostImageOrVideoElementContext))
+{
+}
 
 void InteractionInformationAtPosition::mergeCompatibleOptionalInformation(const InteractionInformationAtPosition& oldInformation)
 {
@@ -44,6 +129,15 @@ void InteractionInformationAtPosition::mergeCompatibleOptionalInformation(const 
     if (oldInformation.request.includeLinkIndicator && !request.includeLinkIndicator)
         textIndicator = oldInformation.textIndicator;
 }
+
+#if ENABLE(DATA_DETECTION)
+Vector<RetainPtr<DDScannerResult>> InteractionInformationAtPosition::serializableDataDetectorResults() const
+{
+    return makeVector(dataDetectorResults.get(), [](DDScannerResult *result) {
+        return std::optional(RetainPtr<DDScannerResult>(result));
+    });
+}
+#endif // ENABLE(DATA_DETECTION)
 
 #endif // PLATFORM(IOS_FAMILY)
 
