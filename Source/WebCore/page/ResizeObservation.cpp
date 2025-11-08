vitorroriz/WebKit

commit 687f8f0e2b6b52578f3ebb78d1d0e0d63555c749

[World Leaks] Investigate leaks in LayoutTests/imported/w3c/web-platform-tests/css/css-sizing/contain-intrinsic-size/ https://bugs.webkit.org/show_bug.cgi?id=300275 rdar://162081020
Reviewed by Ryosuke Niwa.

Document holds a RefPtr<m_resizeObserverForContainIntrinsicSize> which holds a
Vector<GCReachableRef<Element>>, which internally is a vector of RefPtr<Element>
causing a leak, which impacts approximately 15 tests. This is fixed by changing the
vector to a Vector<WeakPtr<Element>>.

Test: fast/css/contain-intrinsic-size-does-not-leak.html

* LayoutTests/fast/css/contain-intrinsic-size-does-not-leak-expected.txt: Added.
* LayoutTests/fast/css/contain-intrinsic-size-does-not-leak.html: Added.
* LayoutTests/fast/css/resources/contain-intrinsic-size-leak-test.html: Added.
* Source/WebCore/bindings/js/JSResizeObserverCustom.cpp:
(WebCore::JSResizeObserver::visitAdditionalChildren):
* Source/WebCore/page/ResizeObserver.cpp:
(WebCore::ResizeObserver::observeInternal):
(WebCore::ResizeObserver::gatherObservations):
(WebCore::ResizeObserver::deliverObservations):
(WebCore::ResizeObserver::isReachableFromOpaqueRoots const):
(WebCore::ResizeObserver::removeAllTargets):
(WebCore::ResizeObserver::removeObservation):
* Source/WebCore/page/ResizeObserver.h:
(WebCore::ResizeObserver::WTF_REQUIRES_LOCK):
(WebCore::ResizeObserver::WTF_RETURNS_LOCK):

Canonical link: https://commits.webkit.org/301243@main

Source/WebCore/page/ResizeObserver.cpp
@@ -92,7 +92,10 @@ void ResizeObserver::observeInternal(Element& target, const ResizeObserverBoxOpt
     // Per the specification, we should dispatch at least one observation for the target. For this reason, we make sure to keep the
     // target alive until this first observation. This, in turn, will keep the ResizeObserver's JS wrapper alive via
     // isReachableFromOpaqueRoots(), so the callback stays alive.
-    m_targetsWaitingForFirstObservation.append(target);
+    {
+        Locker locker { m_observationTargetsLock };
+        m_targetsWaitingForFirstObservation.append(target);
+    }
 
     if (m_document && isJSCallback()) {
         m_document->addResizeObserver(*this);
@@ -144,7 +147,10 @@ size_t ResizeObserver::gatherObservations(size_t deeperThan)
                 LOG_WITH_STREAM(ResizeObserver, stream << "ResizeObserver " << this << " gatherObservations - recording observation " << observation.get());
 
                 m_activeObservations.append(observation.get());
-                m_activeObservationTargets.append(*observation->protectedTarget());
+                {
+                    Locker locker { m_observationTargetsLock };
+                    m_activeObservationTargets.append(*observation->protectedTarget());
+                }
                 minObservedDepth = std::min(depth, minObservedDepth);
             } else
                 m_hasSkippedObservations = true;
@@ -162,9 +168,14 @@ void ResizeObserver::deliverObservations()
         return ResizeObserverEntry::create(observation->target(), observation->computeContentRect(), observation->borderBoxSize(), observation->contentBoxSize());
     });
     m_activeObservations.clear();
-    auto activeObservationTargets = std::exchange(m_activeObservationTargets, { });
 
-    auto targetsWaitingForFirstObservation = std::exchange(m_targetsWaitingForFirstObservation, { });
+    Vector<WeakPtr<Element, WeakPtrImplWithEventTargetData>> activeObservationTargets;
+    Vector<WeakPtr<Element, WeakPtrImplWithEventTargetData>> targetsWaitingForFirstObservation;
+    {
+        Locker locker { m_observationTargetsLock };
+        activeObservationTargets = std::exchange(m_activeObservationTargets, { });
+        targetsWaitingForFirstObservation = std::exchange(m_targetsWaitingForFirstObservation, { });
+    }
 
     if (isNativeCallback()) {
         std::get<NativeResizeObserverCallback>(m_JSOrNativeCallback)(entries, *this);
@@ -193,13 +204,19 @@ bool ResizeObserver::isReachableFromOpaqueRoots(JSC::AbstractSlotVisitor& visito
         if (auto* target = observation->target(); target && containsWebCoreOpaqueRoot(visitor, target))
             return true;
     }
-    for (auto& target : m_activeObservationTargets) {
-        SUPPRESS_UNCOUNTED_ARG {
-            if (containsWebCoreOpaqueRoot(visitor, target.get()))
-                return true;
-        }
+
+    Locker locker { m_observationTargetsLock };
+
+    for (const auto& weakTarget : m_activeObservationTargets) {
+        RefPtr target = weakTarget.get();
+        if (target && containsWebCoreOpaqueRoot(visitor, target.get()))
+            return true;
     }
-    return !m_targetsWaitingForFirstObservation.isEmpty();
+    for (const auto& weakTarget : m_targetsWaitingForFirstObservation) {
+        if (auto* element = weakTarget.get(); element && containsWebCoreOpaqueRoot(visitor, element))
+            return true;
+    }
+    return false;
 }
 
 bool ResizeObserver::removeTarget(Element& target)
@@ -218,17 +235,23 @@ void ResizeObserver::removeAllTargets()
         bool removed = removeTarget(*observation->protectedTarget());
         ASSERT_UNUSED(removed, removed);
     }
-    m_activeObservationTargets.clear();
+    {
+        Locker locker { m_observationTargetsLock };
+        m_activeObservationTargets.clear();
+        m_targetsWaitingForFirstObservation.clear();
+    }
     m_activeObservations.clear();
-    m_targetsWaitingForFirstObservation.clear();
     m_observations.clear();
 }
 
 bool ResizeObserver::removeObservation(const Element& target)
 {
-    m_targetsWaitingForFirstObservation.removeFirstMatching([&target](auto& pendingTarget) {
-        return pendingTarget.ptr() == &target;
-    });
+    {
+        Locker locker { m_observationTargetsLock };
+        m_targetsWaitingForFirstObservation.removeFirstMatching([&target](auto& pendingTarget) {
+            return pendingTarget.get() == &target;
+        });
+    }
     return m_observations.removeFirstMatching([&target](auto& observation) {
         return observation->target() == &target;
     });

LayoutTests/fast/css/contain-intrinsic-size-does-not-leak-expected.txt
@@ -1,0 +1,10 @@
+Tests that ResizeObserver with contain-intrinsic-size does not leak the document object
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS The iframe document didn't leak.
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

LayoutTests/fast/css/contain-intrinsic-size-does-not-leak.html
@@ -1,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<script src="../../resources/js-test.js"></script>
+<script src="../../resources/document-leak-test.js"></script>
+<script>
+    description("Tests that ResizeObserver with contain-intrinsic-size does not leak the document object");
+    onload = () => runDocumentLeakTest({ frameURL: "resources/contain-intrinsic-size-leak-test.html", framesToCreate: 20 });
+</script>
+</body>
+</html>

LayoutTests/fast/css/resources/contain-intrinsic-size-leak-test.html
@@ -1,0 +1,23 @@
+<!DOCTYPE html>
+
+<div style="height: 2000px;"></div>
+
+<div style="contain-intrinsic-size: auto 1px;" id="test">
+  <div style="height: 50px;"></div>
+</div>
+
+<script>
+function finalize() {
+  el.style.contentVisibility = "auto";
+  window.parent.postMessage("ready", "*");
+}
+
+var el = document.getElementById("test");
+var observer = new ResizeObserver(function() {
+  requestAnimationFrame(finalize);
+  observer.unobserve(el);
+});
+
+observer.observe(el);
+el.offsetWidth;
+</script>

Source/WebCore/bindings/js/JSResizeObserverCustom.cpp
@@ -29,6 +29,7 @@
 
 #include "Element.h"
 #include "JSNodeCustom.h"
+#include "WebCoreOpaqueRootInlines.h"
 #include <JavaScriptCore/JSCInlines.h>
 
 namespace WebCore {
@@ -39,6 +40,18 @@ void JSResizeObserver::visitAdditionalChildren(Visitor& visitor)
     ResizeObserverCallback* callback = wrapped().callbackConcurrently();
     if (callback)
         callback->visitJSFunction(visitor);
+
+    Locker locker { wrapped().observationTargetsLock() };
+
+    for (const auto& weakTarget : wrapped().activeObservationTargets()) {
+        SUPPRESS_UNCHECKED_LOCAL if (auto* element = weakTarget.get())
+            addWebCoreOpaqueRoot(visitor, element);
+    }
+
+    for (const auto& weakTarget : wrapped().targetsWaitingForFirstObservation()) {
+        SUPPRESS_UNCHECKED_LOCAL if (auto* element = weakTarget.get())
+            addWebCoreOpaqueRoot(visitor, element);
+    }
 }
 
 DEFINE_VISIT_ADDITIONAL_CHILDREN(JSResizeObserver);

Source/WebCore/page/ResizeObserver.h
@@ -76,6 +76,10 @@ public:
 
     void resetObservationSize(Element&);
 
+    const Vector<WeakPtr<Element, WeakPtrImplWithEventTargetData>>& activeObservationTargets() const WTF_REQUIRES_LOCK(m_observationTargetsLock) { return m_activeObservationTargets; }
+    const Vector<WeakPtr<Element, WeakPtrImplWithEventTargetData>>& targetsWaitingForFirstObservation() const WTF_REQUIRES_LOCK(m_observationTargetsLock) { return m_targetsWaitingForFirstObservation; }
+    Lock& observationTargetsLock() WTF_RETURNS_LOCK(m_observationTargetsLock) { return m_observationTargetsLock; }
+
     ResizeObserverCallback* callbackConcurrently();
     bool isReachableFromOpaqueRoots(JSC::AbstractSlotVisitor&) const;
 
@@ -94,9 +98,10 @@ private:
     Vector<Ref<ResizeObservation>> m_observations;
 
     Vector<Ref<ResizeObservation>> m_activeObservations;
-    Vector<GCReachableRef<Element>> m_activeObservationTargets;
-    Vector<GCReachableRef<Element>> m_targetsWaitingForFirstObservation;
+    Vector<WeakPtr<Element, WeakPtrImplWithEventTargetData>> m_activeObservationTargets WTF_GUARDED_BY_LOCK(m_observationTargetsLock);
+    Vector<WeakPtr<Element, WeakPtrImplWithEventTargetData>> m_targetsWaitingForFirstObservation WTF_GUARDED_BY_LOCK(m_observationTargetsLock);
 
+    mutable Lock m_observationTargetsLock;
     bool m_hasSkippedObservations { false };
 };
 

commit a1c0f13ff6a03165d4f4fce97103a52783d9e18b

Regression(301243@main?) Potential null dereference of m_target in ResizeObservation::computeTargetLocation() https://bugs.webkit.org/show_bug.cgi?id=302197 rdar://164271295
Reviewed by Ryosuke Niwa.

From the crash, we can tell that we're doing a null dereference of m_target
in ResizeObservation::computeTargetLocation(), m_target being a WeakPtr.

I suspect this is a regression from 301243@main. The targets used to be kept
alive via the `m_activeObservationTargets` Vector, which used to contain
`GCReacheableRef<Element>` types. 301243@main updated the Vector to contain
`WeakPtr<Element>` and then relied on `JSResizeObserver::visitAdditionalChildren()`
to visit the targets in the Vector. Something must be wrong with the leak fix in
301243@main. In particular, I think that updating the stack Vectors in
`ResizeObserver::deliverObservations()` to also use WeakPtr instead of GCReacheableRef
was a mistake. The Vectors seemed useless after 301243@main as they contained
WeakPtrs and were unused. I think those vectors have to keep using GCReacheableRef
to make sure the targets and their JS wrappers are kept alive while we deliver the
observations. We need those vectors on the stack because the function clears
`m_activeObservationTargets` before delivering the observations and thus
`JSResizeObserver::visitAdditionalChildren()` will no longer be able to visit the
targets on the GC thread.

Because my fix above may be insufficient and is speculative since we do not have
a reproduction case, I also  added a null check in ResizeObservation::computeTargetLocation()
to avoid the null dereference. I also updated the call site to avoid constructing a
ResizeObserverEntry for a target that has already been destroyed, since there
is no point and it may cause trouble later on since the code may expect the
target to be still alive.

* Source/WebCore/page/ResizeObservation.cpp:
(WebCore::ResizeObservation::computeTargetLocation const):
* Source/WebCore/page/ResizeObserver.cpp:

Source/WebCore/page/ResizeObservation.cpp
@@ -92,6 +92,9 @@ auto ResizeObservation::computeObservedSizes() const -> std::optional<BoxSizes>
 
 LayoutPoint ResizeObservation::computeTargetLocation() const
 {
+    if (!m_target)
+        return { };
+
     if (!m_target->isSVGElement()) {
         if (auto box = m_target->renderBox())
             return LayoutPoint(box->paddingLeft(), box->paddingTop());

Source/WebCore/page/ResizeObserver.cpp
@@ -163,18 +163,36 @@ void ResizeObserver::deliverObservations()
 {
     LOG_WITH_STREAM(ResizeObserver, stream << "ResizeObserver " << this << " deliverObservations");
 
-    auto entries = m_activeObservations.map([](auto& observation) {
-        ASSERT(observation->target());
-        return ResizeObserverEntry::create(observation->target(), observation->computeContentRect(), observation->borderBoxSize(), observation->contentBoxSize());
+    auto entries = WTF::compactMap(m_activeObservations, [](auto& observation) -> RefPtr<ResizeObserverEntry> {
+        RefPtr target = observation->target();
+        ASSERT(target); // The target is supposed to be kept alive via `m_activeObservationTargets` and JSResizeObserver::visitAdditionalChildren().
+        if (!target)
+            return nullptr;
+        return ResizeObserverEntry::create(target.releaseNonNull(), observation->computeContentRect(), observation->borderBoxSize(), observation->contentBoxSize());
     });
     m_activeObservations.clear();
 
-    Vector<WeakPtr<Element, WeakPtrImplWithEventTargetData>> activeObservationTargets;
-    Vector<WeakPtr<Element, WeakPtrImplWithEventTargetData>> targetsWaitingForFirstObservation;
+    // Use GCReachableRef here to make sure the targets and their JS wrappers are kept alive while we deliver.
+    // It is important since m_activeObservationTargets / m_targetsWaitingForFirstObservation will get cleared and
+    // thus JSResizeObserver::visitAdditionalChildren() won't be able to visit them on the GC thread.
+    Vector<GCReachableRef<Element>> activeObservationTargets;
+    Vector<GCReachableRef<Element>> targetsWaitingForFirstObservation;
     {
         Locker locker { m_observationTargetsLock };
-        activeObservationTargets = std::exchange(m_activeObservationTargets, { });
-        targetsWaitingForFirstObservation = std::exchange(m_targetsWaitingForFirstObservation, { });
+        activeObservationTargets = WTF::compactMap(m_activeObservationTargets, [](auto& weakTarget) -> std::optional<GCReachableRef<Element>> {
+            if (weakTarget)
+                return GCReachableRef<Element> { *weakTarget };
+            ASSERT_NOT_REACHED(); // Targets are supposed to be kept alive via JSResizeObserver::visitAdditionalChildren().
+            return std::nullopt;
+        });
+        m_activeObservationTargets = { };
+        targetsWaitingForFirstObservation = WTF::compactMap(m_targetsWaitingForFirstObservation, [](auto& weakTarget) -> std::optional<GCReachableRef<Element>> {
+            if (weakTarget)
+                return GCReachableRef<Element> { *weakTarget };
+            ASSERT_NOT_REACHED(); // Targets are supposed to be kept alive via JSResizeObserver::visitAdditionalChildren().
+            return std::nullopt;
+        });
+        m_targetsWaitingForFirstObservation = { };
     }
 
     if (isNativeCallback()) {

Source/WebCore/page/ResizeObserverEntry.h
@@ -40,27 +40,27 @@ class ResizeObserverSize;
 class ResizeObserverEntry : public RefCounted<ResizeObserverEntry> {
     WTF_MAKE_TZONE_ALLOCATED_INLINE(ResizeObserverEntry);
 public:
-    static Ref<ResizeObserverEntry> create(Element* target, const FloatRect& contentRect, FloatSize borderBoxSize, FloatSize contentBoxSize)
+    static Ref<ResizeObserverEntry> create(Ref<Element>&& target, const FloatRect& contentRect, FloatSize borderBoxSize, FloatSize contentBoxSize)
     {
-        return adoptRef(*new ResizeObserverEntry(target, contentRect, borderBoxSize, contentBoxSize));
+        return adoptRef(*new ResizeObserverEntry(WTFMove(target), contentRect, borderBoxSize, contentBoxSize));
     }
 
-    Element* target() const { return m_target.get(); }
+    Element& target() const { return m_target.get(); }
     DOMRectReadOnly* contentRect() const { return m_contentRect.ptr(); }
     
     const Vector<Ref<ResizeObserverSize>>& borderBoxSize() const { return m_borderBoxSizes; }
     const Vector<Ref<ResizeObserverSize>>& contentBoxSize() const { return m_contentBoxSizes; }
 
 private:
-    ResizeObserverEntry(Element* target, const FloatRect& contentRect, FloatSize borderBoxSize, FloatSize contentBoxSize)
-        : m_target(target)
+    ResizeObserverEntry(Ref<Element>&& target, const FloatRect& contentRect, FloatSize borderBoxSize, FloatSize contentBoxSize)
+        : m_target(WTFMove(target))
         , m_contentRect(DOMRectReadOnly::create(contentRect.x(), contentRect.y(), contentRect.width(), contentRect.height()))
         , m_borderBoxSizes({ ResizeObserverSize::create(borderBoxSize.width(), borderBoxSize.height()) })
         , m_contentBoxSizes({ ResizeObserverSize::create(contentBoxSize.width(), contentBoxSize.height()) })
     {
     }
 
-    const RefPtr<Element> m_target;
+    const Ref<Element> m_target;
     const Ref<DOMRectReadOnly> m_contentRect;
     // The spec is designed to allow mulitple boxes for multicol scenarios, but for now these vectors only ever contain one entry.
     Vector<Ref<ResizeObserverSize>> m_borderBoxSizes;