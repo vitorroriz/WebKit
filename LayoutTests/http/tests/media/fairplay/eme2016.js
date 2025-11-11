vitorroriz/WebKit

commit ffb9c4a28c1adf98cc1e851b5502e6c1438d4a49

[MSE/EME] Video freezes on first frame when transitioning from encrypted to clear (content to ad) https://bugs.webkit.org/show_bug.cgi?id=301749 rdar://162234566
Reviewed by Jer Noble.

When VideoRendererUseDecompressionSessionForProtected preference is off (the default
in user release), when first playing protected content, it will be
enqueued to an AVSampleBufferDisplayLayer directly.
When we switch to clear content, we switch to using a WebCoreDecompressionSession
with an AVSampleBufferVideoRenderer.
When the switch from AVSBDL to WebCoreDecompressionSession occurs we would have
already enqueued several seconds of protected content (the AVSBDL allows
for some large buffer).
So the transition will occur several seconds in the future.
When calculating how much frames we should keep in our decoded queue to maintain
correct watermarks, we looked if we had decoded frames beyond currentTime + 100ms.
But the first decoded frame would be well ahead of that time. We incorrectly
determined that we had sufficient decoded video buffered and stopped decoding.
With the next waterlevel check being scheduled when we expire a frame, we never
resumed from that state and video would stall.

When calculating buffer level we now use the max(currentTime, nextFrame time)
to ensure our decoded buffer is always at least 100ms long even if well in the future.

The tests didn't flag it as the problem only occurred if we had to transition
from an AVSBDL to a WebCoreDecompressionSession which doesn't happen when
VideoRendererUseDecompressionSessionForProtected preference is set (default in tests).

Test: http/tests/media/fairplay/fps-mse-video-mixed.html
* LayoutTests/http/tests/media/fairplay/eme2016.js: Added options to remove logging, necessary for doing a reftest.
* LayoutTests/http/tests/media/fairplay/fps-mse-video-mixed-expected.html: Added.
* LayoutTests/http/tests/media/fairplay/fps-mse-video-mixed.html: Added.

LayoutTests/http/tests/media/fairplay/eme2016.js
@@ -1,11 +1,13 @@
-async function fetchBuffer(url) {
+async function fetchBuffer(url, options) {
     try {
         let result = await fetch(url);
         let buffer = await result.arrayBuffer();
-        consoleWrite(`FETCH: ${url} OK`);
+        if (!options || !options.silent)
+            consoleWrite(`FETCH: ${url} OK`);
         return buffer;
     } catch(e) {
-        consoleWrite(`FETCH: ${url} ERROR`);
+        if (!options || !options.silent)
+            consoleWrite(`FETCH: ${url} ERROR`);
         failTest();
     }
 }
@@ -43,26 +45,30 @@ async function startEME(options) {
     let capabilities = options.capabilities;
 
     let access = await navigator.requestMediaKeySystemAccess("com.apple.fps", capabilities);
-    consoleWrite('PROMISE: requestMediaKeySystemAccess resolved');
+    if (!options.silent)
+        consoleWrite('PROMISE: requestMediaKeySystemAccess resolved');
 
     let keys = await access.createMediaKeys();
-    consoleWrite('PROMISE: createMediaKeys resolved');
-    let certificate = await fetchBuffer('resources/cert.der');
+    if (!options.silent)
+        consoleWrite('PROMISE: createMediaKeys resolved');
+    let certificate = await fetchBuffer('resources/cert.der', options);
     await keys.setServerCertificate(certificate);
-    consoleWrite('PROMISE: keys.setServerCertificate resolved');
+    if (!options.silent)
+        consoleWrite('PROMISE: keys.setServerCertificate resolved');
 
     if (options.setMediaKeys) {
         await video.setMediaKeys(keys);
-        consoleWrite('PROMISE: setMediaKeys() resolved');
+        if (!options.silent)
+            consoleWrite('PROMISE: setMediaKeys() resolved');
     }
 
     return keys;
 }
 
-async function fetchAndAppend(sourceBuffer, url, silent) {
-    let buffer = await fetchBuffer(url);
+async function fetchAndAppend(sourceBuffer, url, options) {
+    let buffer = await fetchBuffer(url, options);
     sourceBuffer.appendBuffer(buffer);
-    await waitFor(sourceBuffer, 'updateend', silent);
+    await waitFor(sourceBuffer, 'updateend', options && options.silent);
 }
 
 async function runAndWaitForLicenseRequest(session, callback) {
@@ -70,7 +76,7 @@ async function runAndWaitForLicenseRequest(session, callback) {
     await callback();
     let message = await licenseRequestPromise;
 
-    let response = await getResponse(message);
+    let response = await getResponse(message, options);
     await session.update(response);
     consoleWrite('PROMISE: session.update() resolved');
 }
@@ -81,33 +87,37 @@ async function fetchAndWaitForLicenseRequest(session, sourceBuffer, url) {
     });
 }
 
-async function fetchAppendAndWaitForEncrypted(video, mediaKeys, sourceBuffer, url) {
-    let updateEndPromise = fetchAndAppend(sourceBuffer, url, true);
-    let encryptedEvent = await waitFor(video, 'encrypted');
+async function fetchAppendAndWaitForEncrypted(video, mediaKeys, sourceBuffer, url, options) {
+    let updateEndPromise = fetchAndAppend(sourceBuffer, url, options);
+    let encryptedEvent = await waitFor(video, 'encrypted', options && options.silent);
 
     let session = mediaKeys.createSession();
     session.generateRequest(encryptedEvent.initDataType, encryptedEvent.initData);
-    let message = await waitFor(session, 'message');
-    let response = await getResponse(message);
+    let message = await waitFor(session, 'message', options && options.silent);
+    let response = await getResponse(message, options);
     await session.update(response);
-    consoleWrite('PROMISE: session.update() resolved');
+    if (!options || !options.silent)
+        consoleWrite('PROMISE: session.update() resolved');
     await updateEndPromise;
-    consoleWrite(`EVENT(updateend)`);
+    if (!options || !options.silent)
+        consoleWrite(`EVENT(updateend)`);
     return session;
 }
 
-async function createBufferAndAppend(mediaSource, type, url) {
+async function createBufferAndAppend(mediaSource, type, url, options) {
     let sourceBuffer = mediaSource.addSourceBuffer(type);
-    consoleWrite('Created sourceBuffer');
-    await fetchAndAppend(sourceBuffer, url);
+    if (!options || !options.silent)
+        consoleWrite('Created sourceBuffer');
+    await fetchAndAppend(sourceBuffer, url, options);
     return sourceBuffer;
 }
 
-async function createBufferAppendAndWaitForEncrypted(video, mediaSource, mediaKeys, type, url) {
+async function createBufferAppendAndWaitForEncrypted(video, mediaSource, mediaKeys, type, url, options) {
     let sourceBuffer = mediaSource.addSourceBuffer(type);
-    consoleWrite('Created sourceBuffer');
+    if (!options || !options.silent)
+        consoleWrite('Created sourceBuffer');
 
-    let session = await fetchAppendAndWaitForEncrypted(video, mediaKeys, sourceBuffer, url);
+    let session = await fetchAppendAndWaitForEncrypted(video, mediaKeys, sourceBuffer, url, options);
 
     return {sourceBuffer, session};
 }

LayoutTests/http/tests/media/fairplay/fps-mse-video-mixed-expected.html
@@ -1,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>fps-mse-video-mixed</title>
+    <script>window.requirePixelDump = true</script>
+    <script src=../../../media-resources/video-test.js></script>
+    <script src=support.js></script>
+    <script src="eme2016.js"></script>
+    <script>
+    window.addEventListener('load', async event => {
+        startTest().then(() => {
+            consoleWrite('TESTDONE');
+            if (window.testRunner)
+                testRunner.notifyDone();
+        });
+    });
+
+    async function startTest() {
+        window.video = document.querySelector('video');
+        let options = { silent: true };
+
+        let mediaSource = new MediaSource;
+        video.srcObject = mediaSource;
+        await waitFor(mediaSource, 'sourceopen');
+
+        let sourceBuffer = mediaSource.addSourceBuffer('video/mp4');
+        sourceBuffer.appendWindowEnd = 1;
+        let fetchPromise = fetchAndAppend(sourceBuffer, '../resources/test-video.mp4', options);
+        let canplayPromise = waitFor(video, 'canplay');
+        await Promise.all([fetchPromise, canplayPromise]);
+        mediaSource.endOfStream();
+
+        video.currentTime = video.duration - .1;
+        await waitFor(video, 'seeked');
+        await shouldResolve(run('video.play()'));
+        await waitFor(video, 'ended');
+    }
+    </script>
+</head>
+<body>
+    <video width="480"></audio>
+</body>
+</html>

LayoutTests/http/tests/media/fairplay/fps-mse-video-mixed.html
@@ -1,0 +1,65 @@
+<!-- webkit-test-runner [ VideoRendererUseDecompressionSessionForProtected=false ] -->
+ <!DOCTYPE html>
+<html>
+<head>
+    <title>fps-mse-video-mixed</title>
+    <script>window.requirePixelDump = true</script>
+    <script src=../../../media-resources/video-test.js></script>
+    <script src=support.js></script>
+    <script src="eme2016.js"></script>
+    <script>
+    window.addEventListener('load', async event => {
+        startTest().then(() => {
+            consoleWrite('TESTDONE');
+            if (window.testRunner)
+                testRunner.notifyDone();
+        });
+    });
+
+    async function startTest() {
+        window.video = document.querySelector('video');
+        let options = {video: window.video, setMediaKeys: true, silent: true, capabilities: [{
+            initDataTypes: ['sinf'],
+            videoCapabilities: [{ contentType: 'video/mp4', robustness: '' }],
+            distinctiveIdentifier: 'not-allowed',
+            persistentState: 'not-allowed',
+            sessionTypes: ['temporary'],
+        }]};
+        let keys = await startEME(options);
+
+        let mediaSource = new MediaSource;
+        video.srcObject = mediaSource;
+        await waitFor(mediaSource, 'sourceopen');
+
+        // Appending Encrypted Video Header.
+
+        let {sourceBuffer: sourceBuffer, session: session} = await createBufferAppendAndWaitForEncrypted(video, mediaSource, keys, 'video/mp4', 'content/elementary-stream-video-header-keyid-1.m4v', options);
+
+        // Appending Encrypted Video Payload.
+
+        let fetchPromise = fetchAndAppend(sourceBuffer, 'content/elementary-stream-video-payload.m4v', options);
+        let canplayPromise = waitFor(video, 'canplay');
+        await Promise.all([fetchPromise, canplayPromise]);
+
+        // Appending Clear Video Payload at end of buffered data.
+
+        let timeSeek = sourceBuffer.buffered.end(0) - 1;
+        sourceBuffer.appendWindowStart = sourceBuffer.buffered.end(0);
+        sourceBuffer.appendWindowEnd = sourceBuffer.buffered.end(0) + 1;
+        sourceBuffer.timestampOffset = sourceBuffer.appendWindowStart;
+
+        await fetchAndAppend(sourceBuffer, '../resources/test-video.mp4', options);
+        mediaSource.endOfStream();
+
+        // Seek in encrypted buffer.
+        video.currentTime = timeSeek;
+        await waitFor(video, 'seeked');
+        await shouldResolve(run('video.play()'));
+        await waitFor(video, 'ended');
+    }
+    </script>
+</head>
+<body>
+    <video width="480"></audio>
+</body>
+</html>

Source/WebCore/platform/graphics/cocoa/VideoMediaSampleRenderer.mm
@@ -468,16 +468,18 @@ void VideoMediaSampleRenderer::decodeNextSampleIfNeeded()
     if (m_gotDecodingError || !decompressionSession)
         return;
 
-    while (!m_compressedSampleQueue.isEmpty() && !m_isDecodingSample) {
-        RetainPtr timebase = this->timebase();
-        MediaTime currentTime = timebase ? PAL::toMediaTime(PAL::CMTimebaseGetTime(timebase.get())) : MediaTime::invalidTime();
-        double playbackRate = timebase ? PAL::CMTimebaseGetRate(timebase.get()) : 0;
+    RetainPtr timebase = this->timebase();
+    MediaTime currentTime = timebase ? PAL::toMediaTime(PAL::CMTimebaseGetTime(timebase.get())) : MediaTime::invalidTime();
+    double playbackRate = timebase ? PAL::CMTimebaseGetRate(timebase.get()) : 0;
+    RefPtr nextDecodedSample = this->nextDecodedSample();
+    auto nextDecodedSampleTime = nextDecodedSample ? nextDecodedSample->presentationTime() : MediaTime::zeroTime();
+    auto lowWaterMarkTime = currentTime.isValid() ? std::max(currentTime, nextDecodedSampleTime) + DecodeLowWaterMark : MediaTime::invalidTime();
+    auto highWaterMarkTime = currentTime.isValid() ? std::max(currentTime, nextDecodedSampleTime) + DecodeHighWaterMark : MediaTime::invalidTime();
 
+    while (!m_compressedSampleQueue.isEmpty() && !m_isDecodingSample) {
         WebCoreDecompressionSession::DecodingFlags decodingFlags;
 
         if (currentTime.isValid() && !m_wasProtected && !m_decompressionSessionWasBlocked) {
-            auto lowWaterMarkTime = currentTime + DecodeLowWaterMark;
-            auto highWaterMarkTime = currentTime + DecodeHighWaterMark;
             auto endTime = lastDecodedSampleTime();
             if (endTime.isValid() && endTime > highWaterMarkTime) {
                 auto [sample, upcomingMinimum, flushId, blocked] = m_compressedSampleQueue.first();
@@ -766,10 +768,10 @@ void VideoMediaSampleRenderer::purgeDecodedSampleQueue(FlushId flushId)
 #endif
         }
     }
-    if (samplesPurged) {
+    if (samplesPurged)
         maybeBecomeReadyForMoreMediaData();
+    if (samplesPurged || decodedSamplesCount() == 1)
         decodeNextSampleIfNeeded();
-    }
 }
 
 bool VideoMediaSampleRenderer::purgeDecodedSampleQueueUntilTime(const MediaTime& currentTime)

commit 9acb0a39dad968a4693d2ba90a7f8b2bc3b9383f

REGRESSION(302409@main): Broke http/tests/media/fairplay/fps-mse-unmuxed-multiple-keys.html https://bugs.webkit.org/show_bug.cgi?id=302295 rdar://164409827
Reviewed by Youenn Fablet.

An error was introduced in 302409@main which made all tests using eme2016.js to
log the `updateend` event twice.

* LayoutTests/http/tests/media/fairplay/eme2016.js:
(async fetchAndSilentAppend): Added new method as the ability to configure logging via `options` isn't sufficient to force waitFor to not log.
* LayoutTests/http/tests/media/fairplay/fps-mse-attach-cdm-after-key-exchange-expected.txt: Rebase expectations
* LayoutTests/http/tests/media/fairplay/legacy-fairplay-mse-v2-expected.txt: Rebase expectations
* LayoutTests/http/tests/media/fairplay/legacy-fairplay-mse-v3-expected.txt: Rebase expectations

Canonical link: https://commits.webkit.org/302870@main

LayoutTests/http/tests/media/fairplay/eme2016.js
@@ -66,19 +66,26 @@ async function startEME(options) {
 }
 
 async function fetchAndAppend(sourceBuffer, url, options) {
-    let buffer = await fetchBuffer(url, options);
+    const buffer = await fetchBuffer(url, options);
     sourceBuffer.appendBuffer(buffer);
     await waitFor(sourceBuffer, 'updateend', options && options.silent);
 }
 
-async function runAndWaitForLicenseRequest(session, callback) {
+async function fetchAndSilentAppend(sourceBuffer, url, options) {
+    const buffer = await fetchBuffer(url, options);
+    sourceBuffer.appendBuffer(buffer);
+    await waitFor(sourceBuffer, 'updateend', true);
+}
+
+async function runAndWaitForLicenseRequest(session, callback, options) {
     var licenseRequestPromise = waitFor(session, 'message');
     await callback();
     let message = await licenseRequestPromise;
 
     let response = await getResponse(message, options);
     await session.update(response);
-    consoleWrite('PROMISE: session.update() resolved');
+    if (!options || options.silent)
+        consoleWrite('PROMISE: session.update() resolved');
 }
 
 async function fetchAndWaitForLicenseRequest(session, sourceBuffer, url) {
@@ -88,7 +95,7 @@ async function fetchAndWaitForLicenseRequest(session, sourceBuffer, url) {
 }
 
 async function fetchAppendAndWaitForEncrypted(video, mediaKeys, sourceBuffer, url, options) {
-    let updateEndPromise = fetchAndAppend(sourceBuffer, url, options);
+    let updateEndPromise = fetchAndSilentAppend(sourceBuffer, url, options);
     let encryptedEvent = await waitFor(video, 'encrypted', options && options.silent);
 
     let session = mediaKeys.createSession();

LayoutTests/http/tests/media/fairplay/fps-mse-attach-cdm-after-key-exchange-expected.txt
@@ -1,3 +1,4 @@
+
 PROMISE: requestMediaKeySystemAccess resolved
 PROMISE: createMediaKeys resolved
 FETCH: resources/cert.der OK

LayoutTests/http/tests/media/fairplay/legacy-fairplay-mse-v2-expected.txt
@@ -7,6 +7,7 @@ FETCH: content/elementary-stream-video-header-keyid-1.m4v OK
 EVENT(webkitneedkey)
 EVENT(webkitkeymessage)
 EXPECTED (uInt8ArrayToString(event.message) == 'certificate') OK
+EVENT(updateend)
 FETCH: resources/cert.der OK
 EVENT(webkitkeymessage)
 PROMISE: licenseResponse resolved

LayoutTests/http/tests/media/fairplay/legacy-fairplay-mse-v3-expected.txt
@@ -7,6 +7,7 @@ FETCH: content/elementary-stream-video-header-keyid-1.m4v OK
 EVENT(webkitneedkey)
 EVENT(webkitkeymessage)
 EXPECTED (uInt8ArrayToString(event.message) == 'certificate') OK
+EVENT(updateend)
 FETCH: resources/cert.der OK
 EVENT(webkitkeymessage)
 PROMISE: licenseResponse resolved