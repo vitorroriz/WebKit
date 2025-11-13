569f9f07502847a509d1592019b451ab0b15cfd8
Said Abou-Hallawa <said@apple.com>
2025-11-12 19:31:16 -0800

    Offscreen canvas allows leaking cross-origin image https://bugs.webkit.org/show_bug.cgi?id=297566 rdar://157055145
    Reviewed by Kimmo Kinnunen.
    
    If a cross-origin image is drawn to an ImageBitmap and it is transferred to an
    OffscreenCanvas, the context of the output canvas should be tainted.
    
    * LayoutTests/http/tests/security/offscreen-canvas-remote-read-remote-image-expected.txt: Added.
    * LayoutTests/http/tests/security/offscreen-canvas-remote-read-remote-image.html: Added.
    * Source/WebCore/html/OffscreenCanvas.cpp:
    (WebCore::OffscreenCanvas::commitToPlaceholderCanvas):
    * Source/WebCore/html/canvas/PlaceholderRenderingContext.cpp:
    (WebCore::PlaceholderRenderingContextSource::setPlaceholderBuffer):
    (WebCore::PlaceholderRenderingContext::setPlaceholderBuffer):
    * Source/WebCore/html/canvas/PlaceholderRenderingContext.h:
    
    Originally-landed-as: 297297.288@safari-7622-branch (bdeaba2e1881). rdar://164280686
    Canonical link: https://commits.webkit.org/302958@main

--- Source/WebCore/html/OffscreenCanvas.cpp
+++ Source/WebCore/html/OffscreenCanvas.cpp
@@ -421,8 +421,8 @@ void OffscreenCanvas::commitToPlaceholderCanvas()
     RefPtr imageBuffer = m_context->surfaceBufferToImageBuffer(CanvasRenderingContext::SurfaceBuffer::DisplayBuffer);
     if (!imageBuffer)
         return;
-    m_placeholderSource->setPlaceholderBuffer(*imageBuffer, m_context->isOpaque());
-    }
+    m_placeholderSource->setPlaceholderBuffer(*imageBuffer, m_context->canvasBase().originClean(), m_context->isOpaque());
+}
 
 void OffscreenCanvas::scheduleCommitToPlaceholderCanvas()
 {
