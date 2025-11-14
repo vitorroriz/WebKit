73ab719b0b4d2ee1473e3297074c7515c4368505
Sosuke Suzuki <sosuke@bun.sh>
2025-11-14 00:47:23 -0800

    [JSC] Implement `Promise.race` in C++ https://bugs.webkit.org/show_bug.cgi?id=300569
    Reviewed by Yusuke Suzuki.
    
    This reverts commit 302934@main to reland 302567@main.
    
    This patch fixes to  crash from original patch by throwing a
    TypeError when elements of the argument's iterable are not thenable.
    
    ------
    This patch changes to implement `Promise.race` in C++.
    
    * JSTests/microbenchmarks/promise-race.js: Added.
    * JSTests/stress/new-promise-capabilities-requires-constructor.js:
    (shouldThrow):
    * JSTests/stress/promise-race-empty-args.js: Added.
    (shouldBe):
    * JSTests/stress/promise-race-fast-path-not-thenable.js: Added.
    (shouldThrowAsync):
    (shouldThrowAsync.async var):
    * JSTests/stress/promise-race-slow-path-non-thenable.js: Added.
    (shouldThrowAsync):
    (shouldThrowAsync.async const):
    (shouldThrowAsync.Promise.resolve):
    * JSTests/stress/promsie-race-resolve-getter-throws.js: Added.
    (shouldBe):
    * Source/JavaScriptCore/builtins/PromiseConstructor.js:
    (any):
    (race): Deleted.
    * Source/JavaScriptCore/runtime/JSGlobalObject.cpp:
    (JSC::JSGlobalObject::init):
    * Source/JavaScriptCore/runtime/JSGlobalObject.h:
    (JSC::JSGlobalObject::promiseResolveWatchpointSet):
    * Source/JavaScriptCore/runtime/JSPromiseConstructor.cpp:
    (JSC::isFastPromiseConstructor):
    (JSC::promiseRaceSlow):
    (JSC::JSC_DEFINE_HOST_FUNCTION):
    
    Canonical link: https://commits.webkit.org/303034@main

--- Source/JavaScriptCore/runtime/JSGlobalObject.h
+++ Source/JavaScriptCore/runtime/JSGlobalObject.h
@@ -495,6 +495,7 @@ public:
     InlineWatchpointSet m_mapSetWatchpointSet { IsWatched };
     InlineWatchpointSet m_setAddWatchpointSet { IsWatched };
     InlineWatchpointSet m_promiseThenWatchpointSet { IsWatched };
+    InlineWatchpointSet m_promiseResolveWatchpointSet { IsWatched };
     InlineWatchpointSet m_promiseSpeciesWatchpointSet { ClearWatchpoint };
     InlineWatchpointSet m_setPrimordialPropertiesWatchpointSet { IsWatched };
     InlineWatchpointSet m_arraySpeciesWatchpointSet { ClearWatchpoint };
@@ -568,6 +569,7 @@ public:
     InlineWatchpointSet& setAddWatchpointSet() { return m_setAddWatchpointSet; }
     InlineWatchpointSet& setPrimordialPropertiesWatchpointSet() { return m_setPrimordialPropertiesWatchpointSet; }
     InlineWatchpointSet& promiseThenWatchpointSet() { return m_promiseThenWatchpointSet; }
+    InlineWatchpointSet& promiseResolveWatchpointSet() { return m_promiseResolveWatchpointSet; }
     InlineWatchpointSet& arraySpeciesWatchpointSet() { return m_arraySpeciesWatchpointSet; }
     InlineWatchpointSet& promiseSpeciesWatchpointSet() { return m_promiseSpeciesWatchpointSet; }
     InlineWatchpointSet& arrayPrototypeChainIsSaneWatchpointSet() { return m_arrayPrototypeChainIsSaneWatchpointSet; }
