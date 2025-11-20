vitorroriz/WebKit

commit 7c6fd84331d9c12fb903ba57e7c8751f8c27372f

Implement BFCache interoperability for WebTransport https://bugs.webkit.org/show_bug.cgi?id=255973 rdar://108827020
Reviewed by Matthew Finkel.

In https://github.com/w3c/webtransport/pull/503 we agreed it would be best
to clean up all the WebTransport objects when entering the b/f cache.
This implements that proposal.

Tests: Tools/TestWebKitAPI/NetworkConnection.h
       Tools/TestWebKitAPI/NetworkConnection.mm
       Tools/TestWebKitAPI/Tests/WebKitCocoa/WebTransport.mm
       Tools/TestWebKitAPI/WebTransportServer.mm

* Source/WebCore/Modules/webtransport/WebTransport.cpp:
(WebCore::WebTransport::WebTransport):
(WebCore::WebTransport::cleanupContext):
* Source/WebCore/Modules/webtransport/WebTransport.h:
* Source/WebCore/dom/ScriptExecutionContext.cpp:
(WebCore::ScriptExecutionContext::createdWebTransport):
(WebCore::ScriptExecutionContext::suspendActiveDOMObjects):
* Source/WebCore/dom/ScriptExecutionContext.h:
* Tools/TestWebKitAPI/NetworkConnection.h:
* Tools/TestWebKitAPI/NetworkConnection.mm:
(TestWebKitAPI::ConnectionGroup::markAsFailed):
(TestWebKitAPI::ConnectionGroup::awaitableFailure):
* Tools/TestWebKitAPI/Tests/WebKitCocoa/WebTransport.mm:
(TestWebKitAPI::TEST(WebTransport, BackForwardCache)):
* Tools/TestWebKitAPI/WebTransportServer.mm:
(TestWebKitAPI::WebTransportServer::WebTransportServer):

Canonical link: https://commits.webkit.org/303295@main

Tools/TestWebKitAPI/NetworkConnection.mm
@@ -214,6 +214,7 @@ struct ConnectionGroup::Data : public ThreadSafeRefCounted<ConnectionGroup::Data
     RetainPtr<nw_connection_group_t> group;
     CompletionHandler<void(Connection)> connectionHandler;
     Vector<Connection> connections;
+    CompletionHandler<void()> failureCompletionHandler;
 };
 
 ConnectionGroup::ConnectionGroup(nw_connection_group_t group)
@@ -222,6 +223,18 @@ ConnectionGroup::ConnectionGroup(nw_connection_group_t group)
 ConnectionGroup::~ConnectionGroup() = default;
 
 ConnectionGroup::ConnectionGroup(const ConnectionGroup&) = default;
+
+void ConnectionGroup::markAsFailed()
+{
+    m_data->failureCompletionHandler();
+}
+
+Awaitable<void> ConnectionGroup::awaitableFailure()
+{
+    co_return co_await AwaitableFromCompletionHandler<void> { [data = m_data] (auto completionHandler) {
+        data->failureCompletionHandler = WTFMove(completionHandler);
+    } };
+}
 
 Connection ConnectionGroup::createWebTransportConnection(ConnectionType type) const
 {

Source/WebCore/Modules/webtransport/WebTransport.cpp
@@ -140,6 +140,7 @@ WebTransport::WebTransport(ScriptExecutionContext& context, JSDOMGlobalObject& g
     , m_receiveStreamSource(WTFMove(receiveStreamSource))
     , m_bidirectionalStreamSource(WTFMove(bidirectionalStreamSource))
 {
+    context.createdWebTransport(*this);
 }
 
 WebTransport::~WebTransport() = default;
@@ -306,6 +307,21 @@ void WebTransport::cleanupWithSessionError()
         WebTransportErrorSource::Session,
         std::nullopt
     }), std::nullopt);
+}
+
+void WebTransport::cleanupContext(ScriptExecutionContext& context)
+{
+    // https://www.w3.org/TR/webtransport/#web-transport-context-cleanup-steps
+    if (m_state == State::Connected) {
+        m_state = State::Failed;
+        if (auto session = std::exchange(m_session, nullptr))
+            session->terminate(0, { });
+        context.postTask([protectedThis = Ref { *this }] (auto&) {
+            protectedThis->cleanupWithSessionError();
+        });
+    }
+    if (m_state == State::Connecting)
+        m_state = State::Failed;
 }
 
 void WebTransport::close(WebTransportCloseInfo&& closeInfo)

Source/WebCore/Modules/webtransport/WebTransport.h
@@ -93,8 +93,8 @@ public:
     Ref<WebTransportSendGroup> createSendGroup();
 
     RefPtr<WebTransportSession> session();
-
     void datagramsWritableCreated(WebTransportDatagramsWritable&);
+    void cleanupContext(ScriptExecutionContext&);
 
 private:
     WebTransport(ScriptExecutionContext&, JSDOMGlobalObject&, Ref<ReadableStream>&&, Ref<ReadableStream>&&, WebTransportCongestionControl, Ref<WebTransportDatagramDuplexStream>&&, Ref<DatagramSource>&&, Ref<WebTransportReceiveStreamSource>&&, Ref<WebTransportBidirectionalStreamSource>&&);

Source/WebCore/dom/ScriptExecutionContext.cpp
@@ -71,6 +71,7 @@
 #include "SocketProvider.h"
 #include "WebCoreJSClientData.h"
 #include "WebCoreOpaqueRoot.h"
+#include "WebTransport.h"
 #include "WorkerGlobalScope.h"
 #include "WorkerLoaderProxy.h"
 #include "WorkerNavigator.h"
@@ -353,6 +354,11 @@ URL ScriptExecutionContext::currentSourceURL(CallStackPosition position) const
     return sourceURL;
 }
 
+void ScriptExecutionContext::createdWebTransport(WebTransport& transport)
+{
+    m_activeWebTransports.add(transport);
+}
+
 void ScriptExecutionContext::suspendActiveDOMObjects(ReasonForSuspension why)
 {
     checkConsistency();
@@ -366,6 +372,11 @@ void ScriptExecutionContext::suspendActiveDOMObjects(ReasonForSuspension why)
     }
 
     m_activeDOMObjectsAreSuspended = true;
+
+    if (why == ReasonForSuspension::BackForwardCache) {
+        for (auto& webTransport : m_activeWebTransports)
+            webTransport.cleanupContext(*this);
+    }
 
     forEachActiveDOMObject([why](auto& activeDOMObject) {
         activeDOMObject.suspend(why);

Source/WebCore/dom/ScriptExecutionContext.h
@@ -35,6 +35,7 @@
 #include <wtf/Function.h>
 #include <wtf/HashSet.h>
 #include <wtf/ObjectIdentifier.h>
+#include <wtf/ThreadSafeWeakHashSet.h>
 #include <wtf/WeakHashSet.h>
 #include <wtf/text/WTFString.h>
 
@@ -101,6 +102,7 @@ class ServiceWorkerContainer;
 class SocketProvider;
 class WeakPtrImplWithEventTargetData;
 class WebCoreOpaqueRoot;
+class WebTransport;
 enum class AdvancedPrivacyProtections : uint16_t;
 enum class CrossOriginMode : bool;
 enum class LoadedFromOpaqueSource : bool;
@@ -385,6 +387,8 @@ public:
 
     bool isAlwaysOnLoggingAllowed() const;
 
+    void createdWebTransport(WebTransport&);
+
 protected:
     class AddConsoleMessageTask : public Task {
     public:
@@ -465,6 +469,7 @@ private:
 
     RefPtr<GuaranteedSerialFunctionDispatcher> m_nativePromiseDispatcher;
     WeakHashSet<NativePromiseRequest> m_nativePromiseRequests;
+    ThreadSafeWeakHashSet<WebTransport> m_activeWebTransports;
 };
 
 WebCoreOpaqueRoot root(ScriptExecutionContext*);

Tools/TestWebKitAPI/NetworkConnection.h
@@ -28,6 +28,7 @@
 #import "CoroutineUtilities.h"
 #import <Network/Network.h>
 #import <wtf/CompletionHandler.h>
+#import <wtf/CoroutineUtilities.h>
 
 namespace TestWebKitAPI {
 
@@ -77,6 +78,7 @@ public:
     Connection createWebTransportConnection(ConnectionType) const;
     ReceiveIncomingConnectionOperation receiveIncomingConnection() const;
     void cancel();
+    Awaitable<void> awaitableFailure();
 
 private:
     friend class WebTransportServer;
@@ -84,6 +86,7 @@ private:
     ConnectionGroup(nw_connection_group_t);
     void receiveIncomingConnection(Connection);
     void receiveIncomingConnection(CompletionHandler<void(Connection)>&&);
+    void markAsFailed();
 
     struct Data;
     Ref<Data> m_data;

Tools/TestWebKitAPI/Tests/WebKitCocoa/WebTransport.mm
@@ -770,6 +770,50 @@ TEST(WebTransport, ServerConnectionTermination)
     EXPECT_WK_STREQ([webView _test_waitForAlert], canLoadnw_webtransport_metadata_get_session_closed() ? "successfully read closeInfo (0, )" : "caught WebTransportError");
 }
 
+TEST(WebTransport, BackForwardCache)
+{
+    bool serverConnectionTerminatedByClient { false };
+    WebTransportServer echoServer([&](ConnectionGroup group) -> ConnectionTask {
+        auto datagramConnection = group.createWebTransportConnection(ConnectionGroup::ConnectionType::Datagram);
+        auto request = co_await datagramConnection.awaitableReceiveBytes();
+        co_await datagramConnection.awaitableSend(WTFMove(request));
+        co_await group.awaitableFailure();
+        serverConnectionTerminatedByClient = true;
+    });
+
+    NSString *mainHTML = [NSString stringWithFormat:@""
+        "<script>async function test() {"
+        "  try {"
+        "    let t = new WebTransport('https://127.0.0.1:%d/');"
+        "    await t.ready;"
+        "    let w = t.datagrams.createWritable().getWriter();"
+        "    await w.write(new TextEncoder().encode('abc'));"
+        "    let r = t.datagrams.readable.getReader();"
+        "    const { value, done } = await r.read();"
+        "    alert('successfully read ' + new TextDecoder().decode(value));"
+        "  } catch (e) { alert('caught ' + e); }"
+        "}; test();"
+        "</script>", echoServer.port()];
+
+    HTTPServer loadingServer({
+        { "/"_s, { mainHTML } },
+        { "/other"_s, { @"<script>alert('loaded')</script>" } }
+    });
+
+    auto configuration = adoptNS([WKWebViewConfiguration new]);
+    enableWebTransport(configuration.get());
+    auto webView = adoptNS([[WKWebView alloc] initWithFrame:CGRectZero configuration:configuration.get()]);
+    auto delegate = adoptNS([TestNavigationDelegate new]);
+    [delegate allowAnyTLSCertificate];
+    [webView setNavigationDelegate:delegate.get()];
+
+    [webView loadRequest:loadingServer.request()];
+    EXPECT_WK_STREQ([webView _test_waitForAlert], "successfully read abc");
+    [webView loadRequest:loadingServer.request("/other"_s)];
+    EXPECT_WK_STREQ([webView _test_waitForAlert], "loaded");
+    Util::run(&serverConnectionTerminatedByClient);
+}
+
 } // namespace TestWebKitAPI
 
 #endif // HAVE(WEB_TRANSPORT)

Tools/TestWebKitAPI/WebTransportServer.mm
@@ -77,10 +77,18 @@ WebTransportServer::WebTransportServer(Function<ConnectionTask(ConnectionGroup)>
     nw_listener_set_new_connection_group_handler(listener.get(), [data = m_data] (nw_connection_group_t incomingConnectionGroup) {
         ConnectionGroup connectionGroup = ConnectionGroup(incomingConnectionGroup);
         data->connectionGroups.append(connectionGroup);
+
         nw_connection_group_set_state_changed_handler(incomingConnectionGroup, [connectionGroup, data] (nw_connection_group_state_t state, nw_error_t error) mutable {
-            if (state != nw_connection_group_state_ready)
-                return;
-            data->coroutineHandles.append(data->connectionGroupHandler(connectionGroup).handle);
+            switch (state) {
+            case nw_connection_group_state_ready:
+                data->coroutineHandles.append(data->connectionGroupHandler(connectionGroup).handle);
+                break;
+            case nw_connection_group_state_failed:
+                connectionGroup.markAsFailed();
+                break;
+            default:
+                break;
+            }
         });
 
         nw_connection_group_set_new_connection_handler(incomingConnectionGroup, [connectionGroup] (nw_connection_t incomingConnection) mutable {

commit cce89e6159c62aca0359cf5feae38e7401546b71

WebTransport tests are broken after 303295@main https://bugs.webkit.org/show_bug.cgi?id=302840 rdar://165108097
Reviewed by Alex Christensen.

Ensures failureCompletionHandler is only called once, if set.

* Tools/TestWebKitAPI/NetworkConnection.mm:
(TestWebKitAPI::ConnectionGroup::markAsFailed):

Canonical link: https://commits.webkit.org/303310@main

Tools/TestWebKitAPI/NetworkConnection.mm
@@ -226,7 +226,8 @@ ConnectionGroup::ConnectionGroup(const ConnectionGroup&) = default;
 
 void ConnectionGroup::markAsFailed()
 {
-    m_data->failureCompletionHandler();
+    if (auto handler = std::exchange(m_data->failureCompletionHandler, nullptr))
+        handler();
 }
 
 Awaitable<void> ConnectionGroup::awaitableFailure()