4d68d564249872f4cf9ff099ea1631052e31da36
Anne van Kesteren <annevk@annevk.nl>
2025-12-03 09:29:49 -0800

    Adjust shadowrootcustomelementregistry attribute serialization https://bugs.webkit.org/show_bug.cgi?id=302851
    Reviewed by Ryosuke Niwa.
    
    Since attachShadow() no longer looks at the host's custom element
    registry, but rather the host document registry, we should do something
    similar when serializing.
    
    WPT PR: https://github.com/web-platform-tests/wpt/pull/56419
    
    Canonical link: https://commits.webkit.org/303841@main

--- Source/WebCore/editing/MarkupAccumulator.cpp
+++ Source/WebCore/editing/MarkupAccumulator.cpp
@@ -31,6 +31,7 @@
 #include "CDATASection.h"
 #include "Comment.h"
 #include "CommonAtomStrings.h"
+#include "CustomElementRegistry.h"
 #include "DocumentFragment.h"
 #include "DocumentLoader.h"
 #include "DocumentType.h"
@@ -423,7 +424,19 @@ void MarkupAccumulator::startAppendingNode(const Node& node, Namespaces* namespa
             m_markup.append(" shadowrootserializable=\"\""_s);
         if (shadowRoot->isClonable())
             m_markup.append(" shadowrootclonable=\"\""_s);
-        if (shadowRoot->protectedHost()->customElementRegistry() != shadowRoot->registryForBindings())
+        bool shouldAppendRegistryAttribute = [&] {
+            Ref document = shadowRoot->document();
+            if (document->usesNullCustomElementRegistry() && shadowRoot->usesNullCustomElementRegistry())
+                return false;
+
+            RefPtr documentRegistry = document->customElementRegistry();
+            RefPtr shadowRegistry = shadowRoot->customElementRegistry();
+            bool documentHasGlobalRegistry = (documentRegistry && !documentRegistry->isScoped()) || document->window();
+            bool shadowHasGlobalRegistry = (shadowRegistry && !shadowRegistry->isScoped())
+                || (!shadowRegistry && !shadowRoot->usesNullCustomElementRegistry() && document->window());
+            return !(documentHasGlobalRegistry && shadowHasGlobalRegistry);
+        }();
+        if (shouldAppendRegistryAttribute)
             m_markup.append(" shadowrootcustomelementregistry=\"\""_s);
         m_markup.append('>');
     } else
