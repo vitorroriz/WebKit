vitorroriz/WebKit

commit 50d7e0b0b808afca93e5ede9cd7c0d44b1ed8130

Prepare the rest of WebCore for making the AtomString(const String&) constructor explicit https://bugs.webkit.org/show_bug.cgi?id=239917
Reviewed by Darin Adler.

Prepare the rest of WebCore for making the AtomString(const String&) constructor explicit.
This helps find suboptimal patterns in our code base, where we may be doing unnecessary
String allocations.

* Source/WebCore/PAL/pal/FileSizeFormatter.cpp:
(PAL::fileSizeDescription):
* Source/WebCore/PAL/pal/FileSizeFormatter.h:
* Source/WebCore/PAL/pal/cocoa/FileSizeFormatterCocoa.mm:
(PAL::fileSizeDescription):
* Source/WebCore/Modules/airplay/WebKitPlaybackTargetAvailabilityEvent.idl:
* Source/WebCore/Modules/encryptedmedia/MediaKeyMessageEvent.idl:
* Source/WebCore/Modules/encryptedmedia/MediaKeySession.idl:
* Source/WebCore/Modules/encryptedmedia/legacy/WebKitMediaKeyMessageEvent.idl:
* Source/WebCore/Modules/encryptedmedia/legacy/WebKitMediaKeyNeededEvent.idl:
* Source/WebCore/Modules/mediarecorder/MediaRecorderErrorEvent.idl:
* Source/WebCore/Modules/mediastream/MediaStreamTrackEvent.idl:
* Source/WebCore/Modules/mediastream/OverconstrainedErrorEvent.idl:
* Source/WebCore/Modules/mediastream/RTCErrorEvent.idl:
* Source/WebCore/Modules/mediastream/RTCPeerConnectionIceErrorEvent.idl:
* Source/WebCore/Modules/mediastream/RTCPeerConnectionIceEvent.idl:
* Source/WebCore/Modules/mediastream/RTCRtpSFrameTransformErrorEvent.idl:
* Source/WebCore/Modules/mediastream/RTCTrackEvent.idl:
* Source/WebCore/Modules/mediastream/RTCTransformEvent.idl:
* Source/WebCore/Modules/notifications/NotificationEvent.idl:
* Source/WebCore/Modules/paymentrequest/MerchantValidationEvent.idl:
* Source/WebCore/Modules/paymentrequest/PaymentMethodChangeEvent.idl:
* Source/WebCore/Modules/paymentrequest/PaymentRequestUpdateEvent.idl:
* Source/WebCore/Modules/speech/SpeechRecognitionErrorEvent.idl:
* Source/WebCore/Modules/speech/SpeechRecognitionEvent.idl:
* Source/WebCore/Modules/speech/SpeechSynthesisEvent.idl:

Source/WTF/wtf/text/AtomString.h
@@ -56,15 +56,13 @@ public:
     AtomString(const StaticStringImpl*);
     AtomString(StringImpl*);
     AtomString(const String&);
+    AtomString(String&&);
     AtomString(StringImpl* baseString, unsigned start, unsigned length);
 
     // FIXME: AtomString doesn’t always have AtomStringImpl, so one of those two names needs to change.
     AtomString(UniquedStringImpl* uid);
 
-    AtomString(ASCIILiteral literal)
-        : m_string(AtomStringImpl::addLiteral(literal.characters(), literal.length()))
-    {
-    }
+    AtomString(ASCIILiteral);
 
     // We have to declare the copy constructor and copy assignment operator as well, otherwise
     // they'll be implicitly deleted by adding the move constructor and move assignment operator.
@@ -254,6 +252,11 @@ inline AtomString::AtomString(const String& string)
 {
 }
 
+inline AtomString::AtomString(String&& string)
+    : m_string(AtomStringImpl::add(string.releaseImpl()))
+{
+}
+
 inline AtomString::AtomString(StringImpl* baseString, unsigned start, unsigned length)
     : m_string(AtomStringImpl::add(baseString, start, length))
 {
@@ -298,6 +301,11 @@ inline const AtomString& starAtom() { return starAtomData.get(); }
 inline const AtomString& xmlAtom() { return xmlAtomData.get(); }
 inline const AtomString& xmlnsAtom() { return xmlnsAtomData.get(); }
 
+inline AtomString::AtomString(ASCIILiteral literal)
+    : m_string(literal.length() ? AtomStringImpl::add(literal.characters(), literal.length()) : Ref { *emptyAtom().impl() })
+{
+}
+
 inline AtomString AtomString::fromUTF8(const char* characters, size_t length)
 {
     if (!characters)
@@ -314,6 +322,15 @@ inline AtomString AtomString::fromUTF8(const char* characters)
     if (!*characters)
         return emptyAtom();
     return fromUTF8Internal(characters, nullptr);
+}
+
+inline AtomString String::toExistingAtomString() const
+{
+    if (isNull())
+        return { };
+    if (impl()->isAtom())
+        return Ref { static_cast<AtomStringImpl&>(*impl()) };
+    return AtomStringImpl::lookUp(impl());
 }
 
 // AtomStringHash is the default hash for AtomString

commit 55173525c593682868c44d1771e573d69ec5dbb9

AtomString(ASCIILiteral) constructor should not copy the string data. https://bugs.webkit.org/show_bug.cgi?id=303457 rdar://165744213
Reviewed by Marcus Plutowski.

It should create an AtomStringImpl that just points to the ASCIILiteral's data as its backing
store instead.  This used to be how it worked, but was regressed by 250160@main back in 2022.

Test: Tools/TestWebKitAPI/Tests/WTF/AtomString.cpp
* Source/WTF/wtf/text/AtomString.h:
(WTF::AtomString::AtomString):
* Tools/TestWebKitAPI/Tests/WTF/AtomString.cpp:
(TestWebKitAPI::TEST(WTF, AtomStringCreationFromLiteral)):

Canonical link: https://commits.webkit.org/303880@main

Source/WTF/wtf/text/AtomString.h
@@ -254,7 +254,7 @@ inline const AtomString& nullAtom() { SUPPRESS_MEMORY_UNSAFE_CAST return *reinte
 inline const AtomString& emptyAtom() { SUPPRESS_MEMORY_UNSAFE_CAST return *reinterpret_cast<const AtomString*>(&emptyAtomData); }
 
 inline AtomString::AtomString(ASCIILiteral literal)
-    : m_string(literal.length() ? AtomStringImpl::add(literal.span8()) : Ref { *emptyAtom().impl() })
+    : m_string(literal.length() ? AtomStringImpl::add(literal) : Ref { *emptyAtom().impl() })
 {
 }
 

Tools/TestWebKitAPI/Tests/WTF/AtomString.cpp
@@ -36,6 +36,13 @@ TEST(WTF, AtomStringCreationFromLiteral)
     ASSERT_EQ(strlen("Template Literal"), stringWithTemplate.length());
     ASSERT_TRUE(stringWithTemplate == "Template Literal"_s);
     ASSERT_TRUE(stringWithTemplate.string().is8Bit());
+
+    ASCIILiteral literal("Source literal");
+    AtomString stringFromLiteral(literal);
+    ASSERT_EQ(strlen("Source literal"), stringFromLiteral.length());
+    ASSERT_TRUE(stringFromLiteral == "Source literal"_s);
+    ASSERT_TRUE(stringFromLiteral.string().is8Bit());
+    ASSERT_TRUE(std::bit_cast<uintptr_t>(stringFromLiteral.impl()->span8().data()) == std::bit_cast<uintptr_t>(literal.span().data()));
 }
 
 TEST(WTF, AtomStringCreationFromLiteralUniqueness)