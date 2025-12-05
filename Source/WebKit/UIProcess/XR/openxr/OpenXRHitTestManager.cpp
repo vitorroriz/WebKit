vitorroriz/WebKit

commit e4fdd06314551926b4600d2e1c696a853804c676

[WebXR Hit Test][OpenXR] Integrate with the XR_ANDROID_raycast extension https://bugs.webkit.org/show_bug.cgi?id=303081
Reviewed by Sergio Villar Senin.

This is an initial implementation, not tested yet.

* Source/WebCore/platform/xr/PlatformXR.h:
* Source/WebKit/SourcesGTK.txt:
* Source/WebKit/SourcesWPE.txt:
* Source/WebKit/UIProcess/XR/openxr/OpenXRExtensions.cpp:
(WebKit::OpenXRExtensions::loadMethods):
* Source/WebKit/UIProcess/XR/openxr/OpenXRExtensions.h:
* Source/WebKit/UIProcess/XR/openxr/OpenXRHitTestManager.cpp: Added.
(WebKit::OpenXRHitTestManager::OpenXRHitTestManager):
(WebKit::OpenXRHitTestManager::requestHitTest):
* Source/WebKit/UIProcess/XR/openxr/OpenXRHitTestManager.h: Added.
* Source/WebKit/UIProcess/XR/openxr/PlatformXROpenXR.cpp:
(WebKit::OpenXRCoordinator::requestHitTestSource):
(WebKit::OpenXRCoordinator::requestTransientInputHitTestSource):
(WebKit::OpenXRCoordinator::populateFrameData):

Canonical link: https://commits.webkit.org/303655@main

Source/WebKit/UIProcess/XR/openxr/OpenXRHitTestManager.cpp
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2025 Igalia, S.L.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * aint with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "config.h"
+#include "OpenXRHitTestManager.h"
+
+#if ENABLE(WEBXR) && USE(OPENXR)
+
+#include "OpenXRExtensions.h"
+#include "OpenXRUtils.h"
+
+namespace WebKit {
+
+OpenXRHitTestManager::OpenXRHitTestManager(XrSession session)
+    : m_session(session)
+{
+#if defined(XR_ANDROID_raycast)
+    auto createInfo = createOpenXRStruct<XrTrackableTrackerCreateInfoANDROID, XR_TYPE_TRACKABLE_TRACKER_CREATE_INFO_ANDROID>();
+    createInfo.trackableType = XR_TRACKABLE_TYPE_PLANE_ANDROID;
+    CHECK_XRCMD(OpenXRExtensions::singleton().methods().xrCreateTrackableTrackerANDROID(session, &createInfo, &m_trackableTracker));
+#endif
+}
+
+Vector<PlatformXR::FrameData::HitTestResult> OpenXRHitTestManager::requestHitTest(const PlatformXR::Ray& ray, XrSpace space, XrTime time)
+{
+#if defined(XR_ANDROID_raycast)
+    if (m_trackableTracker == XR_NULL_HANDLE)
+        return { };
+
+    constexpr int maxHitTestResults = 2;
+    auto raycastInfo = createOpenXRStruct<XrRaycastInfoANDROID, XR_TYPE_RAYCAST_INFO_ANDROID>();
+    raycastInfo.maxResults = maxHitTestResults;
+    raycastInfo.trackerCount = 1;
+    raycastInfo.trackers = &m_trackableTracker;
+    raycastInfo.origin = XrVector3f { ray.origin.x(), ray.origin.y(), ray.origin.z() };
+    raycastInfo.trajectory = XrVector3f { ray.direction.x(), ray.direction.y(), ray.direction.z() };
+    raycastInfo.space = space;
+    raycastInfo.time = time;
+
+    XrRaycastHitResultANDROID xrResults[maxHitTestResults];
+    auto xrHitResults = createOpenXRStruct<XrRaycastHitResultsANDROID, XR_TYPE_RAYCAST_HIT_RESULTS_ANDROID>();
+    xrHitResults.resultsCapacityInput = maxHitTestResults;
+    xrHitResults.results = xrResults;
+
+    CHECK_XRCMD(OpenXRExtensions::singleton().methods().xrRaycastANDROID(m_session, &raycastInfo, &xrHitResults));
+
+    Vector<PlatformXR::FrameData::HitTestResult> results;
+    for (const auto xrResult : xrResults)
+        results.append(XrPosefToPose(xrResult.pose));
+    return results;
+#else
+    return { };
+#endif
+}
+
+} // namespace WebKit
+
+#endif // ENABLE(WEBXR) && USE(OPENXR)

Source/WebKit/UIProcess/XR/openxr/OpenXRHitTestManager.h
@@ -1,0 +1,47 @@
+/*
+ * Copyright (C) 2025 Igalia, S.L.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * aint with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#pragma once
+
+#if ENABLE(WEBXR) && USE(OPENXR)
+
+#include <WebCore/PlatformXR.h>
+#include <openxr/openxr.h>
+#include <wtf/Noncopyable.h>
+#include <wtf/TZoneMalloc.h>
+
+namespace WebKit {
+
+class OpenXRHitTestManager {
+    WTF_MAKE_TZONE_ALLOCATED(OpenXRHitTestManager);
+    WTF_MAKE_NONCOPYABLE(OpenXRHitTestManager);
+public:
+    OpenXRHitTestManager(XrSession);
+    Vector<PlatformXR::FrameData::HitTestResult> requestHitTest(const PlatformXR::Ray&, XrSpace, XrTime);
+
+private:
+    XrSession m_session { XR_NULL_HANDLE };
+#if defined(XR_ANDROID_raycast)
+    XrTrackableTrackerANDROID m_trackableTracker { XR_NULL_HANDLE };
+#endif
+};
+
+} // namespace WebKit
+
+#endif // ENABLE(WEBXR) && USE(OPENXR)

Source/WebKit/UIProcess/XR/openxr/PlatformXROpenXR.cpp
@@ -24,6 +24,7 @@
 
 #include "APIUIClient.h"
 #include "OpenXRExtensions.h"
+#include "OpenXRHitTestManager.h"
 #include "OpenXRInput.h"
 #include "OpenXRLayer.h"
 #include "OpenXRUtils.h"
@@ -65,8 +66,9 @@ struct OpenXRCoordinator::RenderState {
 #if ENABLE(WEBXR_HIT_TEST)
     PlatformXR::HitTestSource nextHitTestSource { 1 };
     PlatformXR::TransientInputHitTestSource nextTransientInputHitTestSource { 1 };
-    HashSet<PlatformXR::HitTestSource> hitTestSources;
-    HashSet<PlatformXR::TransientInputHitTestSource> transientInputHitTestSources;
+    HashMap<PlatformXR::HitTestSource, UniqueRef<PlatformXR::HitTestOptions>> hitTestSources;
+    HashMap<PlatformXR::TransientInputHitTestSource, UniqueRef<PlatformXR::TransientInputHitTestOptions>> transientInputHitTestSources;
+    std::unique_ptr<OpenXRHitTestManager> hitTestManager;
 #endif
 };
 
@@ -365,7 +367,7 @@ void OpenXRCoordinator::submitFrame(WebPageProxy& page, Vector<XRDeviceLayer>&& 
 }
 
 #if ENABLE(WEBXR_HIT_TEST)
-void OpenXRCoordinator::requestHitTestSource(WebPageProxy& page, const PlatformXR::HitTestOptions&, CompletionHandler<void(WebCore::ExceptionOr<PlatformXR::HitTestSource>)>&& completionHandler)
+void OpenXRCoordinator::requestHitTestSource(WebPageProxy& page, const PlatformXR::HitTestOptions& options, CompletionHandler<void(WebCore::ExceptionOr<PlatformXR::HitTestSource>)>&& completionHandler)
 {
     ASSERT(RunLoop::isMain());
     WTF::switchOn(m_state,
@@ -383,8 +385,11 @@ void OpenXRCoordinator::requestHitTestSource(WebPageProxy& page, const PlatformX
                 return;
             }
 
-            active.renderQueue->dispatch([renderState = active.renderState, completionHandler = WTFMove(completionHandler)]() mutable {
-                auto addResult = renderState->hitTestSources.add(renderState->nextHitTestSource);
+            auto copiedOptions = makeUniqueRef<PlatformXR::HitTestOptions>(options);
+            active.renderQueue->dispatch([this, renderState = active.renderState, options = WTFMove(copiedOptions), completionHandler = WTFMove(completionHandler)]() mutable {
+                if (!renderState->hitTestManager)
+                    renderState->hitTestManager = makeUnique<OpenXRHitTestManager>(m_session);
+                auto addResult = renderState->hitTestSources.add(renderState->nextHitTestSource, WTFMove(options));
                 ASSERT_UNUSED(addResult.isNewEntry, addResult);
                 callOnMainRunLoop([source = renderState->nextHitTestSource, completionHandler = WTFMove(completionHandler)] mutable {
                     completionHandler(source);
@@ -419,7 +424,7 @@ void OpenXRCoordinator::deleteHitTestSource(WebPageProxy& page, PlatformXR::HitT
         });
 }
 
-void OpenXRCoordinator::requestTransientInputHitTestSource(WebPageProxy& page, const PlatformXR::TransientInputHitTestOptions&, CompletionHandler<void(WebCore::ExceptionOr<PlatformXR::TransientInputHitTestSource>)>&& completionHandler)
+void OpenXRCoordinator::requestTransientInputHitTestSource(WebPageProxy& page, const PlatformXR::TransientInputHitTestOptions& options, CompletionHandler<void(WebCore::ExceptionOr<PlatformXR::TransientInputHitTestSource>)>&& completionHandler)
 {
     ASSERT(RunLoop::isMain());
     WTF::switchOn(m_state,
@@ -437,8 +442,11 @@ void OpenXRCoordinator::requestTransientInputHitTestSource(WebPageProxy& page, c
                 return;
             }
 
-            active.renderQueue->dispatch([renderState = active.renderState, completionHandler = WTFMove(completionHandler)]() mutable {
-                auto addResult = renderState->transientInputHitTestSources.add(renderState->nextTransientInputHitTestSource);
+            auto copiedOptions = makeUniqueRef<PlatformXR::TransientInputHitTestOptions>(options);
+            active.renderQueue->dispatch([this, renderState = active.renderState, options = WTFMove(copiedOptions), completionHandler = WTFMove(completionHandler)]() mutable {
+                if (!renderState->hitTestManager)
+                    renderState->hitTestManager = makeUnique<OpenXRHitTestManager>(m_session);
+                auto addResult = renderState->transientInputHitTestSources.add(renderState->nextTransientInputHitTestSource, WTFMove(options));
                 ASSERT_UNUSED(addResult.isNewEntry, addResult);
                 callOnMainRunLoop([source = renderState->nextTransientInputHitTestSource, completionHandler = WTFMove(completionHandler)] mutable {
                     completionHandler(source);
@@ -908,10 +916,21 @@ PlatformXR::FrameData OpenXRCoordinator::populateFrameData(Box<RenderState> rend
     }
 
 #if ENABLE(WEBXR_HIT_TEST)
-    for (auto source : renderState->hitTestSources)
-        frameData.hitTestResults.add(source, Vector<PlatformXR::FrameData::HitTestResult> { });
-    for (auto source : renderState->transientInputHitTestSources)
-        frameData.transientInputHitTestResults.add(source, Vector<PlatformXR::FrameData::TransientInputHitTestResult> { });
+    for (auto& pair : renderState->hitTestSources)
+        frameData.hitTestResults.add(pair.key, renderState->hitTestManager->requestHitTest(pair.value->offsetRay, m_localSpace, renderState->frameState.predictedDisplayTime));
+    for (auto& pair : renderState->transientInputHitTestSources) {
+        Vector<PlatformXR::FrameData::TransientInputHitTestResult> results;
+        for (const auto& inputSource : frameData.inputSources) {
+            if (inputSource.profiles.contains(pair.value->profile)) {
+                PlatformXR::FrameData::TransientInputHitTestResult result = {
+                    inputSource.handle,
+                    renderState->hitTestManager->requestHitTest(pair.value->offsetRay, m_localSpace, renderState->frameState.predictedDisplayTime)
+                };
+                results.append(WTFMove(result));
+            }
+        }
+        frameData.transientInputHitTestResults.add(pair.key, WTFMove(results));
+    }
 #endif
 
     auto toXREnvironmentBlendMode = [](XrEnvironmentBlendMode mode) {

Source/WebCore/platform/xr/PlatformXR.h
@@ -300,11 +300,13 @@ struct Ray {
 };
 
 struct HitTestOptions {
+    WTF_DEPRECATED_MAKE_STRUCT_FAST_ALLOCATED(HitTestOptions);
     WebCore::TransformationMatrix nativeOrigin;
     Vector<WebCore::XRHitTestTrackableType> entityTypes;
     Ray offsetRay;
 };
 struct TransientInputHitTestOptions {
+    WTF_DEPRECATED_MAKE_STRUCT_FAST_ALLOCATED(TransientInputHitTestOptions);
     String profile;
     Vector<WebCore::XRHitTestTrackableType> entityTypes;
     Ray offsetRay;

Source/WebKit/UIProcess/XR/openxr/OpenXRExtensions.cpp
@@ -94,6 +94,14 @@ bool OpenXRExtensions::loadMethods(XrInstance instance)
         RELEASE_ASSERT(m_methods->xrLocateHandJointsEXT);
     }
 #endif
+#if defined(XR_ANDROID_raycast)
+    if (isExtensionSupported(XR_ANDROID_RAYCAST_EXTENSION_NAME ""_span)) {
+        xrGetInstanceProcAddr(instance, "xrRaycastANDROID", reinterpret_cast<PFN_xrVoidFunction*>(&m_methods->xrRaycastANDROID));
+        xrGetInstanceProcAddr(instance, "xrCreateTrackableTrackerANDROID", reinterpret_cast<PFN_xrVoidFunction*>(&m_methods->xrCreateTrackableTrackerANDROID));
+        RELEASE_ASSERT(m_methods->xrRaycastANDROID);
+        RELEASE_ASSERT(m_methods->xrCreateTrackableTrackerANDROID);
+    }
+#endif
     return true;
 }
 

Source/WebKit/UIProcess/XR/openxr/OpenXRExtensions.h
@@ -57,6 +57,10 @@ public:
     PFN_xrDestroyHandTrackerEXT xrDestroyHandTrackerEXT { nullptr };
     PFN_xrLocateHandJointsEXT xrLocateHandJointsEXT { nullptr };
 #endif
+#if defined(XR_ANDROID_raycast)
+    PFN_xrRaycastANDROID xrRaycastANDROID { nullptr };
+    PFN_xrCreateTrackableTrackerANDROID xrCreateTrackableTrackerANDROID { nullptr };
+#endif
 };
 
 class OpenXRExtensions final {

commit a834360626713ddf04272782042d28d623513feb

REGRESSION(303655@main): [WebXR Hit Test][OpenXR] Unreviewed build fix for !ENABLE(WEBXR_HIT_TEST) https://bugs.webkit.org/show_bug.cgi?id=303081
The build without ENABLE_WEBXR_HIT_TEST was broken after 303655@main.

* Source/WebKit/UIProcess/XR/openxr/OpenXRHitTestManager.cpp:
* Source/WebKit/UIProcess/XR/openxr/OpenXRHitTestManager.h:
* Source/WebKit/UIProcess/XR/openxr/PlatformXROpenXR.cpp:
(WebKit::OpenXRCoordinator::requestHitTestSource):
(WebKit::OpenXRCoordinator::requestTransientInputHitTestSource):

Canonical link: https://commits.webkit.org/303978@main

Source/WebKit/UIProcess/XR/openxr/OpenXRHitTestManager.cpp
@@ -20,7 +20,7 @@
 #include "config.h"
 #include "OpenXRHitTestManager.h"
 
-#if ENABLE(WEBXR) && USE(OPENXR)
+#if ENABLE(WEBXR_HIT_TEST) && USE(OPENXR)
 
 #include "OpenXRExtensions.h"
 #include "OpenXRUtils.h"
@@ -71,4 +71,4 @@ Vector<PlatformXR::FrameData::HitTestResult> OpenXRHitTestManager::requestHitTes
 
 } // namespace WebKit
 
-#endif // ENABLE(WEBXR) && USE(OPENXR)
+#endif // ENABLE(WEBXR_HIT_TEST) && USE(OPENXR)

Source/WebKit/UIProcess/XR/openxr/OpenXRHitTestManager.h
@@ -19,7 +19,7 @@
 
 #pragma once
 
-#if ENABLE(WEBXR) && USE(OPENXR)
+#if ENABLE(WEBXR_HIT_TEST) && USE(OPENXR)
 
 #include <WebCore/PlatformXR.h>
 #include <openxr/openxr.h>
@@ -44,4 +44,4 @@ private:
 
 } // namespace WebKit
 
-#endif // ENABLE(WEBXR) && USE(OPENXR)
+#endif // ENABLE(WEBXR_HIT_TEST) && USE(OPENXR)

Source/WebKit/UIProcess/XR/openxr/PlatformXROpenXR.cpp
@@ -393,8 +393,10 @@ void OpenXRCoordinator::requestHitTestSource(WebPageProxy& page, const PlatformX
 
             auto copiedOptions = makeUniqueRef<PlatformXR::HitTestOptions>(options);
             active.renderQueue->dispatch([this, renderState = active.renderState, options = WTFMove(copiedOptions), completionHandler = WTFMove(completionHandler)]() mutable {
+#if ENABLE(WEBXR_HIT_TEST)
                 if (!renderState->hitTestManager)
                     renderState->hitTestManager = makeUnique<OpenXRHitTestManager>(m_session);
+#endif
                 auto addResult = renderState->hitTestSources.add(renderState->nextHitTestSource, WTFMove(options));
                 ASSERT_UNUSED(addResult.isNewEntry, addResult);
                 callOnMainRunLoop([source = renderState->nextHitTestSource, completionHandler = WTFMove(completionHandler)] mutable {
@@ -450,8 +452,10 @@ void OpenXRCoordinator::requestTransientInputHitTestSource(WebPageProxy& page, c
 
             auto copiedOptions = makeUniqueRef<PlatformXR::TransientInputHitTestOptions>(options);
             active.renderQueue->dispatch([this, renderState = active.renderState, options = WTFMove(copiedOptions), completionHandler = WTFMove(completionHandler)]() mutable {
+#if ENABLE(WEBXR_HIT_TEST)
                 if (!renderState->hitTestManager)
                     renderState->hitTestManager = makeUnique<OpenXRHitTestManager>(m_session);
+#endif
                 auto addResult = renderState->transientInputHitTestSources.add(renderState->nextTransientInputHitTestSource, WTFMove(options));
                 ASSERT_UNUSED(addResult.isNewEntry, addResult);
                 callOnMainRunLoop([source = renderState->nextTransientInputHitTestSource, completionHandler = WTFMove(completionHandler)] mutable {