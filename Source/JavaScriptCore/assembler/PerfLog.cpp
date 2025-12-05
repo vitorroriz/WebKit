vitorroriz/WebKit

commit 2863e6fd4a249f630e18af723a5af691369d190a

Make PerfLog use sandboxed temp directory https://bugs.webkit.org/show_bug.cgi?id=303346
Reviewed by Yusuke Suzuki.

/tmp is blocked by the sandbox on macOS. This allows jit dumps to
be collected without disabling sandboxing by default.

Canonical link: https://commits.webkit.org/303755@main

Source/JavaScriptCore/assembler/PerfLog.cpp
@@ -37,10 +37,14 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <wtf/DataLog.h>
+#include <wtf/FileSystem.h>
 #include <wtf/MonotonicTime.h>
 #include <wtf/PageBlock.h>
 #include <wtf/ProcessID.h>
 #include <wtf/StringPrintStream.h>
+#include <wtf/text/MakeString.h>
+#include <wtf/text/StringConcatenate.h>
+#include <wtf/text/StringConcatenateNumbers.h>
 #include <wtf/TZoneMallocInlines.h>
 
 #if OS(LINUX)
@@ -174,24 +178,15 @@ static inline uint32_t getCurrentThreadID()
 PerfLog::PerfLog()
 {
     {
-        StringPrintStream filename;
-        if (auto* optionalDirectory = Options::jitDumpDirectory())
-            filename.print(optionalDirectory);
-        else
-            filename.print("/tmp");
-        filename.print("/jit-", getCurrentProcessID(), ".dump");
-        m_fd = open(filename.toCString().data(), O_CREAT | O_TRUNC | O_RDWR, 0666);
-        RELEASE_ASSERT(m_fd != -1);
+        m_file = FileSystem::createDumpFile(makeString("jit"_s, getCurrentThreadID(), "-"_s, WTF::getCurrentProcessID(), ".dump"_s), String::fromUTF8(Options::jitDumpDirectory()));
+        RELEASE_ASSERT(m_file);
 
 #if OS(LINUX)
         // Linux perf command records this mmap operation in perf.data as a metadata to the JIT perf annotations.
         // We do not use this mmap-ed memory region actually.
-        m_marker = mmap(nullptr, pageSize(), PROT_READ | PROT_EXEC, MAP_PRIVATE, m_fd, 0);
+        m_marker = mmap(nullptr, pageSize(), PROT_READ | PROT_EXEC, MAP_PRIVATE, m_file.platformHandle(), 0);
         RELEASE_ASSERT(m_marker != MAP_FAILED);
 #endif
-
-        m_file = fdopen(m_fd, "wb");
-        RELEASE_ASSERT(m_file);
     }
 
     JITDump::FileHeader header;
@@ -199,19 +194,19 @@ PerfLog::PerfLog()
     header.pid = getCurrentProcessID();
 
     Locker locker { m_lock };
-    write(locker, &header, sizeof(JITDump::FileHeader));
+    write(locker, WTF::unsafeMakeSpan(std::bit_cast<uint8_t*>(&header), sizeof(JITDump::FileHeader)));
     flush(locker);
 }
 
-void PerfLog::write(const AbstractLocker&, const void* data, size_t size)
+void PerfLog::write(const AbstractLocker&, std::span<const uint8_t> data)
 {
-    size_t result = fwrite(data, size, 1, m_file);
-    RELEASE_ASSERT(result == 1);
+    auto result = m_file.write(data);
+    RELEASE_ASSERT(result && *result == data.size());
 }
 
 void PerfLog::flush(const AbstractLocker&)
 {
-    fflush(m_file);
+    m_file.flush();
 }
 
 void PerfLog::log(const CString& name, MacroAssemblerCodeRef<LinkBufferPtrTag> code)
@@ -239,9 +234,9 @@ void PerfLog::log(const CString& name, MacroAssemblerCodeRef<LinkBufferPtrTag> c
         record.codeSize = size;
         record.codeIndex = logger.m_codeIndex++;
 
-        logger.write(locker, &record, sizeof(JITDump::CodeLoadRecord));
-        logger.write(locker, name.data(), name.length() + 1);
-        logger.write(locker, executableAddress, size);
+        logger.write(locker, unsafeMakeSpan(std::bit_cast<char*>(&record), sizeof(JITDump::CodeLoadRecord)));
+        logger.write(locker, name.spanIncludingNullTerminator());
+        logger.write(locker, unsafeMakeSpan(executableAddress, size));
         logger.flush(locker);
 
         dataLogLnIf(PerfLogInternal::verbose, name, " [", record.codeIndex, "] ", RawPointer(executableAddress), "-", RawPointer(executableAddress + size), " ", size);

Source/JavaScriptCore/assembler/PerfLog.h
@@ -32,6 +32,7 @@
 
 #include "LinkBuffer.h"
 #include <stdio.h>
+#include <wtf/FileHandle.h>
 #include <wtf/Lock.h>
 #include <wtf/NeverDestroyed.h>
 #include <wtf/TZoneMalloc.h>
@@ -50,13 +51,13 @@ private:
     PerfLog();
     static PerfLog& singleton();
 
-    void write(const AbstractLocker&, const void*, size_t) WTF_REQUIRES_LOCK(m_lock);
+    void write(const AbstractLocker&, std::span<const uint8_t>) WTF_REQUIRES_LOCK(m_lock);
+    void write(const AbstractLocker& locker, std::span<const char> span) WTF_REQUIRES_LOCK(m_lock) { write(locker, unsafeMakeSpan(std::bit_cast<const uint8_t*>(span.data()), span.size_bytes())); }
     void flush(const AbstractLocker&) WTF_REQUIRES_LOCK(m_lock);
 
-    FILE* m_file { nullptr };
+    WTF::FileSystemImpl::FileHandle m_file { };
     void* m_marker { nullptr };
     uint64_t m_codeIndex { 0 };
-    int m_fd { -1 };
     Lock m_lock;
 };
 

Source/JavaScriptCore/runtime/ProfilerSupport.cpp
@@ -30,6 +30,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <wtf/FileSystem.h>
 #include <wtf/MonotonicTime.h>
 #include <wtf/ProcessID.h>
 #include <wtf/StringPrintStream.h>

Source/WTF/wtf/FileSystem.cpp
@@ -35,6 +35,7 @@
 #include <wtf/Scope.h>
 #include <wtf/StdLibExtras.h>
 #include <wtf/text/CString.h>
+#include <wtf/text/MakeString.h>
 #include <wtf/text/ParsingUtilities.h>
 #include <wtf/text/StringBuilder.h>
 
@@ -47,6 +48,12 @@
 
 #if HAVE(STD_FILESYSTEM) || HAVE(STD_EXPERIMENTAL_FILESYSTEM)
 #include <wtf/StdFilesystem.h>
+#endif
+
+#if OS(WINDOWS)
+    constexpr char pathSeparator = '\\';
+#else
+    constexpr char pathSeparator = '/';
 #endif
 
 namespace WTF::FileSystemImpl {
@@ -236,12 +243,6 @@ String decodeFromFilename(const String& inputString)
 
 String lastComponentOfPathIgnoringTrailingSlash(const String& path)
 {
-#if OS(WINDOWS)
-    char pathSeparator = '\\';
-#else
-    char pathSeparator = '/';
-#endif
-
     auto position = path.reverseFind(pathSeparator);
     if (position == notFound)
         return path;
@@ -711,6 +712,15 @@ String createTemporaryFile(StringView prefix, StringView suffix)
 {
     auto [path, handle] = openTemporaryFile(prefix, suffix);
     return path;
+}
+
+FileHandle createDumpFile(StringView filename, StringView path)
+{
+    if (path.isEmpty()) {
+        auto [p, handle] = openTemporaryFile(nullString(), filename);
+        return WTFMove(handle);
+    }
+    return openFile(makeString(path, pathSeparator, filename), FileOpenMode::Truncate);
 }
 
 #if !PLATFORM(PLAYSTATION)

Source/WTF/wtf/FileSystem.h
@@ -109,6 +109,7 @@ WTF_EXPORT_PRIVATE std::optional<int32_t> getFileDeviceId(const String&);
 WTF_EXPORT_PRIVATE bool createSymbolicLink(const String& targetPath, const String& symbolicLinkPath);
 WTF_EXPORT_PRIVATE String createTemporaryZipArchive(const String& directory);
 WTF_EXPORT_PRIVATE String extractTemporaryZipArchive(const String& filePath);
+WTF_EXPORT_PRIVATE FileHandle createDumpFile(StringView filename, StringView path = StringView());
 
 enum class FileType { Regular, Directory, SymbolicLink };
 WTF_EXPORT_PRIVATE std::optional<FileType> fileType(const String&);

commit 4839d1bb13c89917f8531f9f59ed2b3a8a48f7cf

Unreviewed, jitdump file name gets broken from 303755@main https://bugs.webkit.org/show_bug.cgi?id=303594 rdar://165883884
* Source/JavaScriptCore/assembler/PerfLog.cpp:
(JSC::PerfLog::PerfLog):

Canonical link: https://commits.webkit.org/303949@main

Source/JavaScriptCore/assembler/PerfLog.cpp
@@ -178,7 +178,7 @@ static inline uint32_t getCurrentThreadID()
 PerfLog::PerfLog()
 {
     {
-        m_file = FileSystem::createDumpFile(makeString("jit"_s, getCurrentThreadID(), "-"_s, WTF::getCurrentProcessID(), ".dump"_s), String::fromUTF8(Options::jitDumpDirectory()));
+        m_file = FileSystem::createDumpFile(makeString("jit-"_s, getCurrentThreadID(), "-"_s, WTF::getCurrentProcessID(), ".dump"_s), String::fromUTF8(Options::jitDumpDirectory()));
         RELEASE_ASSERT(m_file);
 
 #if OS(LINUX)