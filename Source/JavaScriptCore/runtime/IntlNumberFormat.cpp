5a730d13b69ab0c9cb791f50e6560e868f88242b
Keita Nonaka <iKonnyaku40@gmail.com>
2025-12-04 16:23:33 -0800

    [JSC] Align `Intl.NumberFormat` with ECMA-402 https://bugs.webkit.org/show_bug.cgi?id=303270
    Reviewed by Yusuke Suzuki and Darin Adler.
    
    This patch fixes `Intl.NumberFormat` to conform to ECMA-402[1],
    ensuring the correct `minimumFractionDigits` and `maximumFractionDigits` are applied.
    
    [1]: https://tc39.es/ecma402/#sec-intl.numberformat
    
    * JSTests/stress/intl-numberformat-fd-handling-v2.js:
    (shouldBe.JSON.stringify.fmt.resolvedOptions):
    * JSTests/test262/expectations.yaml:
    * Source/JavaScriptCore/runtime/IntlNumberFormat.cpp:
    (JSC::IntlNumberFormat::initializeNumberFormat):
    
    Canonical link: https://commits.webkit.org/303943@main

--- Source/JavaScriptCore/runtime/IntlNumberFormat.cpp
+++ Source/JavaScriptCore/runtime/IntlNumberFormat.cpp
@@ -283,7 +283,7 @@ IGNORE_GCC_WARNINGS_END
     return "unknown"_s;
 }
 
-// https://tc39.github.io/ecma402/#sec-initializenumberformat
+// https://tc39.es/ecma402/#sec-intl.numberformat
 void IntlNumberFormat::initializeNumberFormat(JSGlobalObject* globalObject, JSValue locales, JSValue optionsValue)
 {
     VM& vm = globalObject->vm();
@@ -372,11 +372,18 @@ void IntlNumberFormat::initializeNumberFormat(JSGlobalObject* globalObject, JSVa
     m_unitDisplay = intlOption<UnitDisplay>(globalObject, options, Identifier::fromString(vm, "unitDisplay"_s), { { "short"_s, UnitDisplay::Short }, { "narrow"_s, UnitDisplay::Narrow }, { "long"_s, UnitDisplay::Long } }, "unitDisplay must be either \"short\", \"narrow\", or \"long\""_s, UnitDisplay::Short);
     RETURN_IF_EXCEPTION(scope, void());
 
-    unsigned minimumFractionDigitsDefault = (m_style == Style::Currency) ? currencyDigits : 0;
-    unsigned maximumFractionDigitsDefault = (m_style == Style::Currency) ? currencyDigits : (m_style == Style::Percent) ? 0 : 3;
-
     m_notation = intlOption<IntlNotation>(globalObject, options, Identifier::fromString(vm, "notation"_s), { { "standard"_s, IntlNotation::Standard }, { "scientific"_s, IntlNotation::Scientific }, { "engineering"_s, IntlNotation::Engineering }, { "compact"_s, IntlNotation::Compact } }, "notation must be either \"standard\", \"scientific\", \"engineering\", or \"compact\""_s, IntlNotation::Standard);
     RETURN_IF_EXCEPTION(scope, void());
+
+    unsigned minimumFractionDigitsDefault;
+    unsigned maximumFractionDigitsDefault;
+    if (m_style == Style::Currency && m_notation == IntlNotation::Standard) {
+        minimumFractionDigitsDefault = currencyDigits;
+        maximumFractionDigitsDefault = currencyDigits;
+    } else {
+        minimumFractionDigitsDefault = 0;
+        maximumFractionDigitsDefault = m_style == Style::Percent ? 0 : 3;
+    }
 
     setNumberFormatDigitOptions(globalObject, this, options, minimumFractionDigitsDefault, maximumFractionDigitsDefault, m_notation);
     RETURN_IF_EXCEPTION(scope, void());
