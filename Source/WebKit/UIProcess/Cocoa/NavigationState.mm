vitorroriz/WebKit

commit 44309b26d25492c0a42cee0fc77d0cf06c235919

The page load activity is only taken on the main frame WebProcess https://bugs.webkit.org/show_bug.cgi?id=303595 rdar://165884028
Reviewed by Sihui Liu.

The page load activity is only taken on the main frame WebProcess. It should also be taken on all iframe WebContent processes.
This patch moves the network activity member of NavigationState to the WebProcessActivityState class, which is used by both
WebPageProxy and RemotePageProxy. A new iframe WebContent process will have a page load activity taken if the main frame
WebContent process already has one.

* Source/WebKit/UIProcess/Cocoa/NavigationState.h:
* Source/WebKit/UIProcess/Cocoa/NavigationState.mm:
(WebKit::NavigationState::releaseNetworkActivity):
(WebKit::NavigationState::didChangeIsLoading):
(WebKit::NavigationState::didSwapWebProcesses):
* Source/WebKit/UIProcess/ProcessThrottler.cpp:
(WebKit::ProcessThrottler::addActivity):
(WebKit::ProcessThrottler::expectedThrottleState):
* Source/WebKit/UIProcess/RemotePageProxy.cpp:
(WebKit::RemotePageProxy::injectPageIntoNewProcess):
* Source/WebKit/UIProcess/WebPageProxy.cpp:
(WebKit::WebPageProxy::takeNetworkActivity):
(WebKit::WebPageProxy::dropNetworkActivity):
(WebKit::WebPageProxy::hasValidNetworkActivity const):
* Source/WebKit/UIProcess/WebPageProxy.h:
* Source/WebKit/UIProcess/WebProcessActivityState.cpp:
(WebKit::WebProcessActivityState::takeNetworkActivity):
(WebKit::WebProcessActivityState::dropNetworkActivity):
(WebKit::WebProcessActivityState::hasValidNetworkActivity const):
* Source/WebKit/UIProcess/WebProcessActivityState.h:

Canonical link: https://commits.webkit.org/304007@main

Source/WebKit/UIProcess/Cocoa/NavigationState.mm
@@ -1578,9 +1578,6 @@ void NavigationState::willChangeIsLoading()
 #if USE(RUNNINGBOARD)
 void NavigationState::releaseNetworkActivity(NetworkActivityReleaseReason reason)
 {
-    if (!m_networkActivity)
-        return;
-
     switch (reason) {
     case NetworkActivityReleaseReason::LoadCompleted:
         RELEASE_LOG(ProcessSuspension, "%p NavigationState is releasing background process assertion because a page load completed", this);
@@ -1589,7 +1586,11 @@ void NavigationState::releaseNetworkActivity(NetworkActivityReleaseReason reason
         RELEASE_LOG(ProcessSuspension, "%p NavigationState is releasing background process assertion because the screen was locked", this);
         break;
     }
-    m_networkActivity = nullptr;
+
+    auto webView = this->webView();
+    if (webView)
+        webView->_page->dropNetworkActivity();
+
     m_releaseNetworkActivityTimer.stop();
 }
 #endif
@@ -1597,9 +1598,11 @@ void NavigationState::releaseNetworkActivity(NetworkActivityReleaseReason reason
 void NavigationState::didChangeIsLoading()
 {
     auto webView = this->webView();
+    if (!webView)
+        return;
 
 #if USE(RUNNINGBOARD)
-    if (webView && webView->_page->pageLoadState().isLoading()) {
+    if (webView->_page->pageLoadState().isLoading()) {
 #if PLATFORM(IOS_FAMILY)
         // We do not start a network activity if a load starts after the screen has been locked.
         if (UIApplication.sharedApplication.isSuspendedUnderLock)
@@ -1610,11 +1613,11 @@ void NavigationState::didChangeIsLoading()
             RELEASE_LOG(ProcessSuspension, "%p - NavigationState keeps its process network assertion because a new page load started", this);
             m_releaseNetworkActivityTimer.stop();
         }
-        if (!m_networkActivity) {
+        if (!webView->_page->hasValidNetworkActivity()) {
             RELEASE_LOG(ProcessSuspension, "%p - NavigationState is taking a process network assertion because a page load started", this);
-            m_networkActivity = webView->_page->legacyMainFrameProcess().protectedThrottler()->backgroundActivity("Page Load"_s);
+            webView->_page->takeNetworkActivity();
         }
-    } else if (m_networkActivity) {
+    } else if (webView->_page->hasValidNetworkActivity()) {
         // The application is visible so we delay releasing the background activity for 3 seconds to give it a chance to start another navigation
         // before suspending the WebContent process <rdar://problem/27910964>.
         RELEASE_LOG(ProcessSuspension, "%p - NavigationState will release its process network assertion soon because the page load completed", this);
@@ -1744,8 +1747,8 @@ void NavigationState::didSwapWebProcesses()
 #if USE(RUNNINGBOARD)
     // Transfer our background assertion from the old process to the new one.
     auto webView = this->webView();
-    if (m_networkActivity && webView)
-        m_networkActivity = webView->_page->legacyMainFrameProcess().protectedThrottler()->backgroundActivity("Page Load"_s);
+    if (webView && webView->_page->hasValidNetworkActivity())
+        webView->_page->takeNetworkActivity();
 #endif
 }
 

Source/WebKit/UIProcess/WebProcessActivityState.cpp
@@ -27,6 +27,7 @@
 #include "WebProcessActivityState.h"
 
 #include "APIPageConfiguration.h"
+#include "Logging.h"
 #include "RemotePageProxy.h"
 #include "WebPageProxy.h"
 #include "WebProcessPool.h"
@@ -104,6 +105,12 @@ void WebProcessActivityState::takeMutedCaptureAssertion()
     });
 }
 
+void WebProcessActivityState::takeNetworkActivity()
+{
+    RELEASE_LOG(Process, "Taking network activity on WebProcess with PID %d", protectedProcess()->processID());
+    m_networkActivity = protectedProcess()->protectedThrottler()->backgroundActivity("Page Load"_s);
+}
+
 void WebProcessActivityState::reset()
 {
     m_isVisibleActivity = nullptr;
@@ -116,6 +123,7 @@ void WebProcessActivityState::reset()
 #if PLATFORM(IOS_FAMILY)
     m_openingAppLinkActivity = nullptr;
 #endif
+    m_networkActivity = nullptr;
 }
 
 void WebProcessActivityState::dropVisibleActivity()
@@ -144,6 +152,12 @@ void WebProcessActivityState::dropMutedCaptureAssertion()
     m_isMutedCaptureAssertion = nullptr;
 }
 
+void WebProcessActivityState::dropNetworkActivity()
+{
+    RELEASE_LOG(Process, "Dropping network activity on WebProcess with PID %d", protectedProcess()->processID());
+    m_networkActivity = nullptr;
+}
+
 bool WebProcessActivityState::hasValidVisibleActivity() const
 {
     return m_isVisibleActivity && m_isVisibleActivity->isValid();
@@ -157,6 +171,11 @@ bool WebProcessActivityState::hasValidAudibleActivity() const
 bool WebProcessActivityState::hasValidCapturingActivity() const
 {
     return m_isCapturingActivity && m_isCapturingActivity->isValid();
+}
+
+bool WebProcessActivityState::hasValidNetworkActivity() const
+{
+    return m_networkActivity && m_networkActivity->isValid();
 }
 
 bool WebProcessActivityState::hasValidMutedCaptureAssertion() const

Source/WebKit/UIProcess/Cocoa/NavigationState.h
@@ -296,7 +296,6 @@ private:
     } m_historyDelegateMethods;
 
 #if USE(RUNNINGBOARD)
-    RefPtr<ProcessThrottler::BackgroundActivity> m_networkActivity;
     RunLoop::Timer m_releaseNetworkActivityTimer;
 #endif
 };

Source/WebKit/UIProcess/RemotePageProxy.cpp
@@ -104,6 +104,9 @@ void RemotePageProxy::injectPageIntoNewProcess()
         m_process->setRunningBoardThrottlingEnabled();
 #endif
 
+    if (page->hasValidNetworkActivity())
+        m_processActivityState->takeNetworkActivity();
+
     Ref drawingArea = *page->drawingArea();
     m_drawingArea = RemotePageDrawingAreaProxy::create(drawingArea.get(), m_process);
 #if ENABLE(FULLSCREEN_API)

Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -606,6 +606,14 @@ void WebPageProxy::takeMutedCaptureAssertion()
     });
 }
 
+void WebPageProxy::takeNetworkActivity()
+{
+    m_mainFrameProcessActivityState->takeNetworkActivity();
+    protectedBrowsingContextGroup()->forEachRemotePage(*this, [](auto& remotePageProxy) {
+        remotePageProxy.processActivityState().takeNetworkActivity();
+    });
+}
+
 #if ENABLE(WEB_PROCESS_SUSPENSION_DELAY)
 void WebPageProxy::takeAccessibilityActivityWhenInWindow()
 {
@@ -669,6 +677,14 @@ void WebPageProxy::dropMutedCaptureAssertion()
     });
 }
 
+void WebPageProxy::dropNetworkActivity()
+{
+    m_mainFrameProcessActivityState->dropNetworkActivity();
+    protectedBrowsingContextGroup()->forEachRemotePage(*this, [](auto& remotePageProxy) {
+        remotePageProxy.processActivityState().dropNetworkActivity();
+    });
+}
+
 bool WebPageProxy::hasValidVisibleActivity() const
 {
     bool hasValidVisibleActivity = m_mainFrameProcessActivityState->hasValidVisibleActivity();
@@ -703,6 +719,15 @@ bool WebPageProxy::hasValidMutedCaptureAssertion() const
         hasValidMutedCaptureAssertion &= remotePageProxy.processActivityState().hasValidMutedCaptureAssertion();
     });
     return hasValidMutedCaptureAssertion;
+}
+
+bool WebPageProxy::hasValidNetworkActivity() const
+{
+    bool hasValidNetworkActivity = m_mainFrameProcessActivityState->hasValidNetworkActivity();
+    protectedBrowsingContextGroup()->forEachRemotePage(*this, [&](auto& remotePageProxy) {
+        hasValidNetworkActivity &= remotePageProxy.processActivityState().hasValidNetworkActivity();
+    });
+    return hasValidNetworkActivity;
 }
 
 #if PLATFORM(IOS_FAMILY)

Source/WebKit/UIProcess/WebPageProxy.h
@@ -2871,6 +2871,10 @@ public:
 
     void networkRequestsInProgressDidChange();
 
+    void takeNetworkActivity();
+    void dropNetworkActivity();
+    bool hasValidNetworkActivity() const;
+
 private:
     WebPageProxy(PageClient&, WebProcessProxy&, Ref<API::PageConfiguration>&&);
     void platformInitialize();

Source/WebKit/UIProcess/WebProcessActivityState.h
@@ -46,17 +46,20 @@ public:
     void takeAudibleActivity();
     void takeCapturingActivity();
     void takeMutedCaptureAssertion();
+    void takeNetworkActivity();
 
     void reset();
     void dropVisibleActivity();
     void dropAudibleActivity();
     void dropCapturingActivity();
     void dropMutedCaptureAssertion();
+    void dropNetworkActivity();
 
     bool hasValidVisibleActivity() const;
     bool hasValidAudibleActivity() const;
     bool hasValidCapturingActivity() const;
     bool hasValidMutedCaptureAssertion() const;
+    bool hasValidNetworkActivity() const;
 
 #if PLATFORM(IOS_FAMILY)
     void takeOpeningAppLinkActivity();
@@ -91,6 +94,8 @@ private:
 #if PLATFORM(IOS_FAMILY)
     RefPtr<ProcessThrottlerActivity> m_openingAppLinkActivity;
 #endif
+    RefPtr<ProcessThrottlerActivity> m_networkActivity;
+
 };
 
 } // namespace WebKit

commit e4ade59d3c143f9f86b82aac74978dd9db21c988

After 304007@main, a network activity is no longer taken on process swap https://bugs.webkit.org/show_bug.cgi?id=303767 rdar://166075191
Reviewed by Sihui Liu.

This is caused by the fact that NavigationState::didSwapWebProcesses() is called after the process
swap has happened, so we do no longer have information about whether the previous process had a
network activity. This is resolved by checking if a page load is in progress when the process is
being swapped. Additionally, we don't clear the network activity in WebProcessActivityState::reset(),
since NavigationState has a timer that handles this. This fixes a page load time regression
introduced by 304007@main.

* Source/WebKit/UIProcess/Cocoa/NavigationState.h:
* Source/WebKit/UIProcess/Cocoa/NavigationState.mm:
(WebKit::NavigationState::releaseNetworkActivity):
(WebKit::NavigationState::didChangeIsLoading):
(WebKit::NavigationState::didSwapWebProcesses):
* Source/WebKit/UIProcess/WebProcessActivityState.cpp:
(WebKit::WebProcessActivityState::reset):

Canonical link: https://commits.webkit.org/304180@main

Source/WebKit/UIProcess/Cocoa/NavigationState.mm
@@ -1747,7 +1747,7 @@ void NavigationState::didSwapWebProcesses()
 #if USE(RUNNINGBOARD)
     // Transfer our background assertion from the old process to the new one.
     auto webView = this->webView();
-    if (webView && webView->_page->hasValidNetworkActivity())
+    if (webView && webView->_page->pageLoadState().isLoading())
         webView->_page->takeNetworkActivity();
 #endif
 }

Source/WebKit/UIProcess/WebProcessActivityState.cpp
@@ -123,7 +123,6 @@ void WebProcessActivityState::reset()
 #if PLATFORM(IOS_FAMILY)
     m_openingAppLinkActivity = nullptr;
 #endif
-    m_networkActivity = nullptr;
 }
 
 void WebProcessActivityState::dropVisibleActivity()