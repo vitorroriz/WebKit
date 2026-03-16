vitorroriz/WebKit

commit 9784033bdce3c771fc8e88735612c51c396d423b

[Build Speed] Reduce includes of JSDOMPromiseDeferred.h and make it cheaper rdar://164123237 https://bugs.webkit.org/show_bug.cgi?id=302045
Reviewed by Brent Fulgham and BJ Burg.

Reduce the number of times JSDOMPromiseDeferred.h is included during a build, and for those times
when including that header is unavoidable, reduce the cost of including that header.

For the first part, remove JSDOMPromiseDeferred.h from (almost all) non-Inlines.h headers by
converting those headers to use JSDOMPromiseDeferredForward.h. This also requires any class that
stores a JSDOMPromiseDeferred to use a std::unique_ptr instead of a std::optional. It also required
some classes to have inline methods move to the implementation file (or have implementation files
added where none previously existed). These include SimpleWritableStreamSink, ModuleScriptLoader,
NavigationAPIMethodTracker, NavigationTransition, and WorkletPendingTasks.

For the second part, remove JSDOMConvert.h from JSDOMPromiseDeferred.h. JSDOMConvert.h pulls in
every different mechanism to convert a c++ object to an IDLType, which is very expensive. Instead,
the users of JSDOMPromiseDeferred.h should include only the specific JSDOMConvert*.h header needed
to call resolve() or reject().

Before this change, JSDOMPromiseDeferred.h was the 11th most expensive header in the WebCore Unified
build. It was included 184 times, at an average cost on this machine of 880ms of CPU time, for a total
cost of 2.7m of CPU time spent parsing this header.

After this change, JSDOMPromiseDeferred.h is the 67th most expensive header in the WebCore Unified
build. It is included 120 times, at an average cost on this machine of 430ms of CPU time, for a total
cost of 53s of CPU time spent parsing this header.

All told, frontend parsing time decreased from 8122s to 7876s, a reduction of 3%.

* Source/WebCore/Modules/WebGPU/GPUBuffer.cpp:
(WebCore::GPUBuffer::mapAsync):
(WebCore::GPUBuffer::internalUnmap):

Source/WebCore/Modules/gamepad/GamepadHapticActuator.cpp
@@ -34,6 +34,7 @@
 #include "Gamepad.h"
 #include "GamepadEffectParameters.h"
 #include "GamepadProvider.h"
+#include "JSDOMConvertEnumeration.h"
 #include "JSDOMPromiseDeferred.h"
 #include "Settings.h"
 #include <wtf/CompletionHandler.h>

Source/WebCore/Modules/webcodecs/WebCodecsAudioDecoder.cpp
@@ -32,6 +32,7 @@
 #include "ContextDestructionObserverInlines.h"
 #include "DOMException.h"
 #include "ExceptionOr.h"
+#include "JSDOMConvertDictionary.h"
 #include "JSDOMPromiseDeferred.h"
 #include "JSWebCodecsAudioDecoderSupport.h"
 #include "ScriptExecutionContextInlines.h"

Source/WebCore/Modules/webcodecs/WebCodecsAudioEncoder.cpp
@@ -34,6 +34,7 @@
 #include "DOMException.h"
 #include "ExceptionOr.h"
 #include "FlacEncoderConfig.h"
+#include "JSDOMConvertDictionary.h"
 #include "JSDOMPromiseDeferred.h"
 #include "JSWebCodecsAudioEncoderSupport.h"
 #include "Logging.h"

Source/WebCore/Modules/webcodecs/WebCodecsVideoDecoder.cpp
@@ -35,6 +35,7 @@
 #include "HTMLImageElement.h"
 #include "HTMLVideoElement.h"
 #include "ImageBitmap.h"
+#include "JSDOMConvertDictionary.h"
 #include "JSDOMPromiseDeferred.h"
 #include "JSWebCodecsVideoDecoderSupport.h"
 #include "OffscreenCanvas.h"

Source/WebCore/Modules/webcodecs/WebCodecsVideoEncoder.cpp
@@ -31,6 +31,7 @@
 #include "ContextDestructionObserverInlines.h"
 #include "DOMException.h"
 #include "ExceptionOr.h"
+#include "JSDOMConvertDictionary.h"
 #include "JSDOMPromiseDeferred.h"
 #include "JSWebCodecsVideoEncoderSupport.h"
 #include "Logging.h"

commit e9873641801108756188d55f7501d61f1654eaa5

[WPE] Build broken on bot WPE-Linux-RPi4-32bits-Mesa-Release-Perf-Build after 309275@main. https://bugs.webkit.org/show_bug.cgi?id=310019
Reviewed by Claudio Saavedra.

The WPE build broke with 309275@main in the WPE-Linux-RPi4-32bits-Mesa-Release-Perf-Build
bot, the issue is related to missing header includes that trigger in this bot because of
different ordering for the generated unified source files.

* Source/WebCore/Modules/gamepad/GamepadHapticActuator.cpp:
* Source/WebCore/Modules/mediastream/MediaStreamTrackProcessor.cpp:
* Source/WebCore/Modules/webcodecs/WebCodecsAudioDecoder.cpp:
* Source/WebCore/Modules/webcodecs/WebCodecsAudioEncoder.cpp:
* Source/WebCore/Modules/webcodecs/WebCodecsVideoDecoder.cpp:
* Source/WebCore/Modules/webcodecs/WebCodecsVideoEncoder.cpp:

Canonical link: https://commits.webkit.org/309326@main

Source/WebCore/Modules/gamepad/GamepadHapticActuator.cpp
@@ -35,6 +35,7 @@
 #include "GamepadEffectParameters.h"
 #include "GamepadProvider.h"
 #include "JSDOMConvertEnumeration.h"
+#include "JSDOMConvertStrings.h"
 #include "JSDOMPromiseDeferred.h"
 #include "Settings.h"
 #include <wtf/CompletionHandler.h>

Source/WebCore/Modules/mediastream/MediaStreamTrackProcessor.cpp
@@ -38,6 +38,7 @@
 #include "OffscreenCanvas.h"
 #include "ReadableStream.h"
 #include "SVGImageElement.h"
+#include <wtf/Scope.h>
 #include <wtf/Seconds.h>
 #include <wtf/TZoneMallocInlines.h>
 

Source/WebCore/Modules/webcodecs/WebCodecsAudioDecoder.cpp
@@ -43,6 +43,7 @@
 #include "WebCodecsErrorCallback.h"
 #include "WebCodecsUtilities.h"
 
+#include <wtf/Scope.h>
 #include <wtf/TZoneMallocInlines.h>
 
 namespace WebCore {

Source/WebCore/Modules/webcodecs/WebCodecsAudioEncoder.cpp
@@ -51,6 +51,7 @@
 #include "WebCodecsUtilities.h"
 #include <JavaScriptCore/ArrayBuffer.h>
 #include <JavaScriptCore/ConsoleTypes.h>
+#include <wtf/Scope.h>
 #include <wtf/StdLibExtras.h>
 #include <wtf/TZoneMallocInlines.h>
 #include <wtf/text/MakeString.h>

Source/WebCore/Modules/webcodecs/WebCodecsVideoDecoder.cpp
@@ -49,6 +49,7 @@
 #include "WebCodecsVideoFrame.h"
 #include "WebCodecsVideoFrameOutputCallback.h"
 #include <wtf/ASCIICType.h>
+#include <wtf/Scope.h>
 #include <wtf/TZoneMallocInlines.h>
 
 namespace WebCore {

Source/WebCore/Modules/webcodecs/WebCodecsVideoEncoder.cpp
@@ -48,6 +48,7 @@
 #include "WebCodecsVideoFrame.h"
 #include <JavaScriptCore/ArrayBuffer.h>
 #include <JavaScriptCore/ConsoleTypes.h>
+#include <wtf/Scope.h>
 #include <wtf/StdLibExtras.h>
 #include <wtf/TZoneMallocInlines.h>
 #include <wtf/text/MakeString.h>