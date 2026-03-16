vitorroriz/WebKit

commit c8a6727eec001e27fb9d2fb9cc3fc8af2b1e7f69

[GStreamer] Video rendering support for Qualcomm decoder https://bugs.webkit.org/show_bug.cgi?id=307151
Reviewed by Xabier Rodriguez-Calvar and Nikolas Zimmermann.

The buffers produced by the Qualcomm decoder contain a single GstMemory which stores the
GBM FD pointing to the decoded frame. The frame format is YUV (NV12). As this is stored
in a single memory the existing DMABuf/YUV layer buffers cannot be used for rendering. So
we rely on the EXT_YUV_target OpenGL ES extension to convert it to a RGB texture for
rendering.

Manually tested on a device shipping the Qualcomm GStreamer decoder.

* Source/WebCore/platform/graphics/egl/GLContext.cpp:
(WebCore::GLContext::glExtensions const):
* Source/WebCore/platform/graphics/egl/GLContext.h:
* Source/WebCore/platform/graphics/gstreamer/VideoFrameGStreamer.cpp:
(WebCore::videoFormatToDRMFourcc):
(WebCore::VideoFrameGStreamer::infoFromCaps):
* Source/WebCore/platform/graphics/texmap/BitmapTexture.cpp:
(WebCore::BitmapTexture::determineRenderTargetAndBinding):
(WebCore::BitmapTexture::BitmapTexture):
(WebCore::BitmapTexture::createTexture):
(WebCore::BitmapTexture::allocateTexture):
(WebCore::BitmapTexture::allocateTextureFromMemoryMappedGPUBuffer):
(WebCore::BitmapTexture::reset):
(WebCore::BitmapTexture::updateContents):
(WebCore::BitmapTexture::createFboIfNeeded):
(WebCore::BitmapTexture::bindAsSurface):
(WebCore::BitmapTexture::copyFromExternalTexture):
(WebCore::BitmapTexture::colorConvertFlags const):
* Source/WebCore/platform/graphics/texmap/BitmapTexture.h:
* Source/WebCore/platform/graphics/texmap/TextureMapper.cpp:
(WebCore::TextureMapper::drawTexturedQuadWithProgram):
(WebCore::TextureMapper::drawTextureExternalOESYUV):
* Source/WebCore/platform/graphics/texmap/TextureMapper.h:
* Source/WebCore/platform/graphics/texmap/TextureMapperShaderProgram.cpp:
(WebCore::STRINGIFY):
(WebCore::TextureMapperShaderProgram::create):
* Source/WebCore/platform/graphics/texmap/TextureMapperShaderProgram.h:

Source/WebCore/platform/graphics/texmap/TextureMapperShaderProgram.cpp
@@ -89,7 +89,7 @@ static const char* vertexTemplateLT320Vars =
         varying float v_antialias;
         varying vec4 v_nonProjectedPosition;
     );
-
+// clang-format off
 static const char* vertexTemplateCommon =
     STRINGIFY(
         uniform mat4 u_modelViewMatrix;
@@ -158,7 +158,7 @@ static const char* vertexTemplateCommon =
             gl_Position = u_projectionMatrix * v_nonProjectedPosition;
         }
     );
-
+// clang-format on
 #define ANTIALIASING_TEX_COORD_DIRECTIVE \
     GLSL_DIRECTIVE(if defined(ENABLE_Antialiasing)) \
         GLSL_DIRECTIVE(define transformTexCoord fragmentTransformTexCoord) \
@@ -187,6 +187,22 @@ static const char* vertexTemplateCommon =
         GLSL_DIRECTIVE(define SamplerExternalOESType sampler2D) \
     GLSL_DIRECTIVE(endif)
 
+// clang-format off
+#define EXT_YUV_TARGET_DIRECTIVE \
+    STRINGIFY(\n) \
+    GLSL_DIRECTIVE(ifdef ENABLE_TextureExternalOESYUV) \
+        GLSL_DIRECTIVE(extension GL_EXT_YUV_target : require) \
+        GLSL_DIRECTIVE(define SamplerExternalOESYUVType __samplerExternal2DY2YEXT) \
+    GLSL_DIRECTIVE(else) \
+        GLSL_DIRECTIVE(if __VERSION__ >= 300) \
+            GLSL_DIRECTIVE(extension GL_OES_EGL_image_external_essl3 : require) \
+        GLSL_DIRECTIVE(else) \
+            GLSL_DIRECTIVE(extension GL_OES_EGL_image_external : require) \
+        GLSL_DIRECTIVE(endif) \
+        GLSL_DIRECTIVE(define SamplerExternalOESYUVType samplerExternalOES) \
+    GLSL_DIRECTIVE(endif)
+// clang-format on
+
 // The max number of stacked rounded rectangle clips allowed is 10, which is also the
 // max number of transforms that we can get. We need 3 components for each rounded
 // rectangle so we need 30 components to receive the 10 rectangles.
@@ -200,12 +216,15 @@ static const char* vertexTemplateCommon =
 // Common header for all versions. We define the matrices variables here to keep the precision
 // directives scope: the first one applies to the matrices variables and the next one to the
 // rest of them.
+// clang-format off
 static const char* fragmentTemplateHeaderCommon =
     ANTIALIASING_TEX_COORD_DIRECTIVE
     BLUR_CONSTANTS
     ROUNDED_RECT_CONSTANTS
     OES_EGL_IMAGE_EXTERNAL_DIRECTIVE
-    TEXTURE_SPACE_MATRIX_PRECISION_DIRECTIVE
+    TEXTURE_SPACE_MATRIX_PRECISION_DIRECTIVE;
+
+static const char* fragmentTemplateHeaderCommonVariables =
     STRINGIFY(
         precision TextureSpaceMatrixPrecision float;
     )
@@ -224,6 +243,38 @@ static const char* fragmentTemplateLT320Vars =
         varying vec2 v_texCoord;
         varying vec2 v_transformedTexCoord;
         varying vec4 v_nonProjectedPosition;
+    );
+
+static const char* fragmentTemplateYUVToRGB =
+    "\n"
+    GLSL_DIRECTIVE(ifdef ENABLE_TextureExternalOESYUV)
+    STRINGIFY(
+        void applyTextureExternalOESYUV(inout vec4 color, vec2 texCoord)
+        {
+            vec4 contentColor = vec4(yuv_2_rgb(texture2D(s_yuvToRgbSampler, texCoord).rgb, itu_601).rgb, 1.0);
+            color = sourceOver(contentColor, color);
+        }
+    )
+    "\n"
+    GLSL_DIRECTIVE(else)
+    STRINGIFY(
+        void applyTextureExternalOESYUV(inout vec4 color, vec2 texCoord)
+        {
+            vec4 contentColor = texture2D(s_yuvToRgbSampler, texCoord);
+            color = sourceOver(contentColor, color);
+        }
+    )
+    "\n"
+    GLSL_DIRECTIVE(endif);
+
+static const char* fragmentTemplateYUVToRGBNoop =
+    "\n"
+    STRINGIFY(
+        void applyTextureExternalOESYUV(inout vec4 color, vec2 texCoord)
+        {
+            vec4 contentColor = texture2D(s_yuvToRgbSampler, texCoord);
+            color = sourceOver(contentColor, color);
+        }
     );
 
 // PQ tone-mapping function with conditional highp precision.
@@ -292,6 +343,7 @@ static const char* fragmentTemplateCommon =
         uniform sampler2D s_samplerA;
         uniform sampler2D s_contentTexture;
         uniform SamplerExternalOESType s_externalOESTexture;
+        uniform SamplerExternalOESYUVType s_yuvToRgbSampler;
         uniform float u_opacity;
         uniform float u_filterAmount;
         uniform mat4 u_yuvToRgb;
@@ -338,6 +390,8 @@ static const char* fragmentTemplateCommon =
         void applyPremultiply(inout vec4 color) { color = vec4(color.rgb * color.a, color.a); }
 
         void applyToneMapPQ(inout vec4 color);
+
+        void applyTextureExternalOESYUV(inout vec4 color, vec2 texCoord);
 
         vec3 yuvToRgb(float y, float u, float v)
         {
@@ -624,10 +678,12 @@ static const char* fragmentTemplateCommon =
             applyTextureCopyIfNeeded(color, texCoord);
             applyBlurFilterIfNeeded(color, texCoord);
             applyTextureExternalOESIfNeeded(color, texCoord);
+            applyTextureExternalOESYUVIfNeeded(color, texCoord);
             applyRoundedRectClipIfNeeded(color);
             gl_FragColor = color;
         }
     );
+// clang-format on
 
 Ref<TextureMapperShaderProgram> TextureMapperShaderProgram::create(TextureMapperShaderProgram::Options options)
 {
@@ -664,6 +720,7 @@ Ref<TextureMapperShaderProgram> TextureMapperShaderProgram::create(TextureMapper
     SET_APPLIER_FROM_OPTIONS(ManualRepeat);
     SET_APPLIER_FROM_OPTIONS(ClampUVBounds);
     SET_APPLIER_FROM_OPTIONS(TextureExternalOES);
+    SET_APPLIER_FROM_OPTIONS(TextureExternalOESYUV);
     SET_APPLIER_FROM_OPTIONS(RoundedRectClip);
     SET_APPLIER_FROM_OPTIONS(Premultiply);
 
@@ -701,6 +758,13 @@ Ref<TextureMapperShaderProgram> TextureMapperShaderProgram::create(TextureMapper
     // Append the common header.
     fragmentShaderBuilder.append(unsafeSpan(fragmentTemplateHeaderCommon));
 
+    if (GLContext::current()->glExtensions().EXT_YUV_target)
+        fragmentShaderBuilder.append(unsafeSpan(EXT_YUV_TARGET_DIRECTIVE));
+    else
+        fragmentShaderBuilder.append(unsafeSpan(GLSL_DIRECTIVE(define SamplerExternalOESYUVType sampler2D)));
+
+    fragmentShaderBuilder.append(unsafeSpan(fragmentTemplateHeaderCommonVariables));
+
     // Append the appropriate input/output variable definitions.
     fragmentShaderBuilder.append(unsafeSpan(fragmentTemplateLT320Vars));
 
@@ -709,6 +773,11 @@ Ref<TextureMapperShaderProgram> TextureMapperShaderProgram::create(TextureMapper
 
     // Append the PQ tone mapping function.
     fragmentShaderBuilder.append(unsafeSpan(fragmentTemplateToneMapPq));
+
+    if (GLContext::current()->glExtensions().EXT_YUV_target)
+        fragmentShaderBuilder.append(unsafeSpan(fragmentTemplateYUVToRGB));
+    else
+        fragmentShaderBuilder.append(unsafeSpan(fragmentTemplateYUVToRGBNoop));
 
     return adoptRef(*new TextureMapperShaderProgram(vertexShaderBuilder.toString(), fragmentShaderBuilder.toString()));
 }

Source/WebCore/platform/graphics/egl/GLContext.cpp
@@ -641,6 +641,7 @@ const GLContext::GLExtensions& GLContext::glExtensions() const
         m_glExtensions.EXT_unpack_subimage = isExtensionSupported(extensionsString, "GL_EXT_unpack_subimage");
         m_glExtensions.APPLE_sync = isExtensionSupported(extensionsString, "GL_APPLE_sync");
         m_glExtensions.OES_packed_depth_stencil = isExtensionSupported(extensionsString, "GL_OES_packed_depth_stencil");
+        m_glExtensions.EXT_YUV_target = isExtensionSupported(extensionsString, "GL_EXT_YUV_target");
     });
     return m_glExtensions;
 }

Source/WebCore/platform/graphics/egl/GLContext.h
@@ -101,6 +101,7 @@ public:
         bool EXT_unpack_subimage { false };
         bool APPLE_sync { false };
         bool OES_packed_depth_stencil { false };
+        bool EXT_YUV_target { false };
     };
     const GLExtensions& glExtensions() const;
 

Source/WebCore/platform/graphics/gstreamer/VideoFrameGStreamer.cpp
@@ -40,10 +40,12 @@
 #include <skia/core/SkData.h>
 #include <skia/core/SkImage.h>
 
-#if USE(GBM) && GST_CHECK_VERSION(1, 24, 0)
+#if USE(GBM)
 #include <drm_fourcc.h>
+#if GST_CHECK_VERSION(1, 24, 0)
 #include <gst/video/video-info-dma.h>
-#endif
+#endif // GST_CHECK_VERSION(1, 24, 0)
+#endif // USE(GBM)
 
 #if USE(GSTREAMER_GL)
 #include <gst/allocators/gstdmabuf.h>
@@ -69,13 +71,52 @@ static void ensureVideoFrameDebugCategoryInitialized()
     });
 }
 
+#if USE(GBM)
+static std::optional<uint32_t> videoFormatToDRMFourcc(GstVideoFormat format)
+{
+    switch (format) {
+    case GST_VIDEO_FORMAT_BGRx:
+        return DRM_FORMAT_XRGB8888;
+    case GST_VIDEO_FORMAT_RGBx:
+        return DRM_FORMAT_XBGR8888;
+    case GST_VIDEO_FORMAT_BGRA:
+        return DRM_FORMAT_ARGB8888;
+    case GST_VIDEO_FORMAT_RGBA:
+        return DRM_FORMAT_ABGR8888;
+    case GST_VIDEO_FORMAT_I420:
+        return DRM_FORMAT_YUV420;
+    case GST_VIDEO_FORMAT_YV12:
+        return DRM_FORMAT_YVU420;
+    case GST_VIDEO_FORMAT_NV12:
+        return DRM_FORMAT_NV12;
+    case GST_VIDEO_FORMAT_NV21:
+        return DRM_FORMAT_NV21;
+    case GST_VIDEO_FORMAT_Y444:
+        return DRM_FORMAT_YUV444;
+    case GST_VIDEO_FORMAT_Y41B:
+        return DRM_FORMAT_YUV411;
+    case GST_VIDEO_FORMAT_Y42B:
+        return DRM_FORMAT_YUV422;
+    case GST_VIDEO_FORMAT_P010_10LE:
+        return DRM_FORMAT_P010;
+    default:
+        break;
+    }
+
+    ASSERT_NOT_REACHED_WITH_MESSAGE("Un-handled video format: %s", gst_video_format_to_string(format));
+    GST_ERROR("Un-handled video format: %s", gst_video_format_to_string(format));
+    return std::nullopt;
+}
+#endif
+
 VideoFrameGStreamer::Info VideoFrameGStreamer::infoFromCaps(const GRefPtr<GstCaps>& caps)
 {
     GstVideoInfo videoInfo;
     gst_video_info_from_caps(&videoInfo, caps.get());
 
     std::optional<DMABufFormat> dmabufFormat;
-#if USE(GBM) && GST_CHECK_VERSION(1, 24, 0)
+#if USE(GBM)
+#if GST_CHECK_VERSION(1, 24, 0)
     if (gst_video_is_dma_drm_caps(caps.get())) {
         GstVideoInfoDmaDrm drmVideoInfo;
         if (!gst_video_info_dma_drm_from_caps(&drmVideoInfo, caps.get()))
@@ -86,7 +127,12 @@ VideoFrameGStreamer::Info VideoFrameGStreamer::infoFromCaps(const GRefPtr<GstCap
 
         dmabufFormat = { drmVideoInfo.drm_fourcc, drmVideoInfo.drm_modifier };
     }
-#endif
+#else
+    auto fourccFromFormat = videoFormatToDRMFourcc(GST_VIDEO_INFO_FORMAT(&videoInfo));
+    ASSERT(fourccFromFormat);
+    dmabufFormat = { *fourccFromFormat, DRM_FORMAT_MOD_INVALID };
+#endif // GST_CHECK_VERSION(1, 24, 0)
+#endif // USE(GBM)
     return { videoInfo, dmabufFormat };
 }
 
@@ -707,40 +753,6 @@ void VideoFrameGStreamer::setMemoryTypeFromCaps()
 }
 
 #if USE(GBM) && GST_CHECK_VERSION(1, 24, 0)
-static uint32_t videoFormatToDRMFourcc(GstVideoFormat format)
-{
-    switch (format) {
-    case GST_VIDEO_FORMAT_BGRx:
-        return DRM_FORMAT_XRGB8888;
-    case GST_VIDEO_FORMAT_RGBx:
-        return DRM_FORMAT_XBGR8888;
-    case GST_VIDEO_FORMAT_BGRA:
-        return DRM_FORMAT_ARGB8888;
-    case GST_VIDEO_FORMAT_RGBA:
-        return DRM_FORMAT_ABGR8888;
-    case GST_VIDEO_FORMAT_I420:
-        return DRM_FORMAT_YUV420;
-    case GST_VIDEO_FORMAT_YV12:
-        return DRM_FORMAT_YVU420;
-    case GST_VIDEO_FORMAT_NV12:
-        return DRM_FORMAT_NV12;
-    case GST_VIDEO_FORMAT_NV21:
-        return DRM_FORMAT_NV21;
-    case GST_VIDEO_FORMAT_Y444:
-        return DRM_FORMAT_YUV444;
-    case GST_VIDEO_FORMAT_Y41B:
-        return DRM_FORMAT_YUV411;
-    case GST_VIDEO_FORMAT_Y42B:
-        return DRM_FORMAT_YUV422;
-    case GST_VIDEO_FORMAT_P010_10LE:
-        return DRM_FORMAT_P010;
-    default:
-        break;
-    }
-
-    return 0;
-}
-
 RefPtr<DMABufBuffer> VideoFrameGStreamer::getDMABuf()
 {
     if (m_memoryType != MemoryType::DMABuf)
@@ -775,8 +787,14 @@ RefPtr<DMABufBuffer> VideoFrameGStreamer::getDMABuf()
     IntSize size(videoMeta->width, videoMeta->height);
     const auto& videoInfo = info();
     auto dmabufFormat = this->dmaBufFormat();
-    uint32_t fourcc = dmabufFormat ? dmabufFormat->first : videoFormatToDRMFourcc(GST_VIDEO_INFO_FORMAT(&videoInfo));
     uint64_t modifier = dmabufFormat ? dmabufFormat->second : DRM_FORMAT_MOD_INVALID;
+    uint32_t fourcc = 0;
+    if (dmabufFormat)
+        fourcc = dmabufFormat->first;
+    else if (auto fourccFromFormat = videoFormatToDRMFourcc(GST_VIDEO_INFO_FORMAT(&videoInfo)))
+        fourcc = *fourccFromFormat;
+    ASSERT(fourcc);
+
     RefPtr dmabuf = DMABufBuffer::create(size, fourcc, WTF::move(fds), WTF::move(offsets), WTF::move(strides), modifier);
 
     DMABufBuffer::ColorSpace colorSpace = DMABufBuffer::ColorSpace::Bt601;

Source/WebCore/platform/graphics/texmap/BitmapTexture.cpp
@@ -69,6 +69,18 @@ static const GLenum s_pixelDataType = GL_UNSIGNED_BYTE;
 
 namespace WebCore {
 
+void BitmapTexture::determineRenderTargetAndBinding()
+{
+    if (m_flags.contains(Flags::ExternalOESRenderTarget)) {
+        m_renderTarget = GL_TEXTURE_EXTERNAL_OES;
+        m_binding = GL_TEXTURE_BINDING_EXTERNAL_OES;
+        return;
+    }
+
+    m_binding = GL_TEXTURE_BINDING_2D;
+    m_renderTarget = GL_TEXTURE_2D;
+}
+
 GLenum BitmapTexture::textureFormat() const
 {
     return m_flags.contains(Flags::UseBGRALayout) ? GL_BGRA : GL_RGBA;
@@ -88,6 +100,7 @@ BitmapTexture::BitmapTexture(const IntSize& size, OptionSet<Flags> flags)
     , m_size(size)
     , m_pixelFormat(PixelFormat::RGBA8)
 {
+    determineRenderTargetAndBinding();
 #if USE(GBM)
     if (m_flags.contains(Flags::BackedByDMABuf)) {
         OptionSet<MemoryMappedGPUBuffer::BufferFlag> bufferFlags;
@@ -117,29 +130,30 @@ BitmapTexture::BitmapTexture(const IntSize& size, OptionSet<Flags> flags)
 #endif
 
     GLint boundTexture = 0;
-    glGetIntegerv(GL_TEXTURE_BINDING_2D, &boundTexture);
+    glGetIntegerv(m_binding, &boundTexture);
 
     allocateTexture();
 
-    glBindTexture(GL_TEXTURE_2D, boundTexture);
+    glBindTexture(m_renderTarget, boundTexture);
 }
 
 void BitmapTexture::createTexture()
 {
     ASSERT(!m_id);
     glGenTextures(1, &m_id);
-    glBindTexture(GL_TEXTURE_2D, m_id);
+    glBindTexture(m_renderTarget, m_id);
+
     GLenum filter = m_flags.contains(Flags::NearestFiltering) ? GL_NEAREST : GL_LINEAR;
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filter);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filter);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(m_renderTarget, GL_TEXTURE_MIN_FILTER, filter);
+    glTexParameteri(m_renderTarget, GL_TEXTURE_MAG_FILTER, filter);
+    glTexParameteri(m_renderTarget, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(m_renderTarget, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 }
 
 void BitmapTexture::allocateTexture()
 {
     createTexture();
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_size.width(), m_size.height(), 0, textureFormat(), s_pixelDataType, nullptr);
+    glTexImage2D(m_renderTarget, 0, GL_RGBA, m_size.width(), m_size.height(), 0, textureFormat(), s_pixelDataType, nullptr);
 }
 
 #if USE(GBM)
@@ -155,7 +169,7 @@ bool BitmapTexture::allocateTextureFromMemoryMappedGPUBuffer()
 
     if (auto eglImage = m_memoryMappedGPUBuffer->createEGLImageFromDMABuf()) {
         createTexture();
-        glEGLImageTargetTexture2DOES(GL_TEXTURE_2D, eglImage);
+        glEGLImageTargetTexture2DOES(m_renderTarget, eglImage);
 
         auto& display = WebCore::PlatformDisplay::sharedDisplay();
         display.destroyEGLImage(eglImage);
@@ -169,13 +183,15 @@ bool BitmapTexture::allocateTextureFromMemoryMappedGPUBuffer()
 BitmapTexture::BitmapTexture(EGLImage image, OptionSet<Flags> flags)
     : m_flags(flags)
 {
+    determineRenderTargetAndBinding();
+
     GLint boundTexture = 0;
-    glGetIntegerv(GL_TEXTURE_BINDING_2D, &boundTexture);
+    glGetIntegerv(m_binding, &boundTexture);
 
     createTexture();
-    glEGLImageTargetTexture2DOES(GL_TEXTURE_2D, image);
+    glEGLImageTargetTexture2DOES(m_renderTarget, image);
 
-    glBindTexture(GL_TEXTURE_2D, boundTexture);
+    glBindTexture(m_renderTarget, boundTexture);
 }
 #endif
 
@@ -234,8 +250,10 @@ void BitmapTexture::reset(const IntSize& size, OptionSet<Flags> flags)
         return;
     m_size = size;
 
+    determineRenderTargetAndBinding();
+
     GLint boundTexture = 0;
-    glGetIntegerv(GL_TEXTURE_BINDING_2D, &boundTexture);
+    glGetIntegerv(m_binding, &boundTexture);
 
 #if USE(GBM)
     if (m_memoryMappedGPUBuffer) {
@@ -249,15 +267,15 @@ void BitmapTexture::reset(const IntSize& size, OptionSet<Flags> flags)
         m_memoryMappedGPUBuffer = MemoryMappedGPUBuffer::create(m_size, m_memoryMappedGPUBuffer->flags());
 
         if (allocateTextureFromMemoryMappedGPUBuffer()) {
-            glBindTexture(GL_TEXTURE_2D, boundTexture);
+            glBindTexture(m_renderTarget, boundTexture);
             return;
         }
     }
 #endif
 
-    glBindTexture(GL_TEXTURE_2D, m_id);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_size.width(), m_size.height(), 0, textureFormat(), s_pixelDataType, nullptr);
-    glBindTexture(GL_TEXTURE_2D, boundTexture);
+    glBindTexture(m_renderTarget, m_id);
+    glTexImage2D(m_renderTarget, 0, GL_RGBA, m_size.width(), m_size.height(), 0, textureFormat(), s_pixelDataType, nullptr);
+    glBindTexture(m_renderTarget, boundTexture);
 }
 
 void BitmapTexture::updateContents(const void* srcData, const IntRect& targetRect, const IntPoint& sourceOffset, int bytesPerLine, PixelFormat pixelFormat)
@@ -283,7 +301,7 @@ void BitmapTexture::updateContents(const void* srcData, const IntRect& targetRec
     }
 #endif
 
-    glBindTexture(GL_TEXTURE_2D, m_id);
+    glBindTexture(m_renderTarget, m_id);
 
     const unsigned bytesPerPixel = 4;
     auto data = static_cast<const uint8_t*>(srcData);
@@ -315,7 +333,7 @@ void BitmapTexture::updateContents(const void* srcData, const IntRect& targetRec
         adjustedSourceOffset = IntPoint(0, 0);
     }
 
-    glBindTexture(GL_TEXTURE_2D, m_id);
+    glBindTexture(m_renderTarget, m_id);
 
     if (supportsUnpackSubimage) {
         // Use the OpenGL sub-image extension, now that we know it's available.
@@ -324,7 +342,7 @@ void BitmapTexture::updateContents(const void* srcData, const IntRect& targetRec
         glPixelStorei(GL_UNPACK_SKIP_PIXELS, adjustedSourceOffset.x());
     }
 
-    glTexSubImage2D(GL_TEXTURE_2D, 0, targetRect.x(), targetRect.y(), targetRect.width(), targetRect.height(), textureFormat(), s_pixelDataType, data);
+    glTexSubImage2D(m_renderTarget, 0, targetRect.x(), targetRect.y(), targetRect.width(), targetRect.height(), textureFormat(), s_pixelDataType, data);
 
     if (supportsUnpackSubimage) {
         glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
@@ -441,7 +459,7 @@ void BitmapTexture::createFboIfNeeded()
 
     glGenFramebuffers(1, &m_fbo);
     glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, id(), 0);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, m_renderTarget, id(), 0);
     if (m_flags.contains(Flags::DepthBuffer))
         initializeDepthBuffer();
     m_shouldClear = true;
@@ -449,7 +467,7 @@ void BitmapTexture::createFboIfNeeded()
 
 void BitmapTexture::bindAsSurface()
 {
-    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindTexture(m_renderTarget, 0);
     createFboIfNeeded();
     glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
     glViewport(0, 0, m_size.width(), m_size.height());
@@ -491,24 +509,26 @@ void BitmapTexture::copyFromExternalTexture(GLuint sourceTextureID, const IntRec
     GLint boundFramebuffer = 0;
     GLint boundActiveTexture = 0;
 
-    glGetIntegerv(GL_TEXTURE_BINDING_2D, &boundTexture);
+    determineRenderTargetAndBinding();
+
+    glGetIntegerv(m_binding, &boundTexture);
     glGetIntegerv(GL_FRAMEBUFFER_BINDING, &boundFramebuffer);
     glGetIntegerv(GL_ACTIVE_TEXTURE, &boundActiveTexture);
 
-    glBindTexture(GL_TEXTURE_2D, sourceTextureID);
+    glBindTexture(m_renderTarget, sourceTextureID);
 
     GLuint copyFbo = 0;
     glGenFramebuffers(1, &copyFbo);
     glBindFramebuffer(GL_FRAMEBUFFER, copyFbo);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, sourceTextureID, 0);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, m_renderTarget, sourceTextureID, 0);
 
     glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, id());
-    glCopyTexSubImage2D(GL_TEXTURE_2D, 0, targetRect.x(), targetRect.y(), sourceOffset.width(), sourceOffset.height(), targetRect.width(), targetRect.height());
+    glBindTexture(m_renderTarget, id());
+    glCopyTexSubImage2D(m_renderTarget, 0, targetRect.x(), targetRect.y(), sourceOffset.width(), sourceOffset.height(), targetRect.width(), targetRect.height());
 
-    glBindTexture(GL_TEXTURE_2D, boundTexture);
+    glBindTexture(m_renderTarget, boundTexture);
     glBindFramebuffer(GL_FRAMEBUFFER, boundFramebuffer);
-    glBindTexture(GL_TEXTURE_2D, boundTexture);
+    glBindTexture(m_renderTarget, boundTexture);
     glActiveTexture(boundActiveTexture);
     glDeleteFramebuffers(1, &copyFbo);
 }
@@ -522,7 +542,7 @@ OptionSet<TextureMapperFlags> BitmapTexture::colorConvertFlags() const
         return { };
 
     // Our GL textures are stored in RGBA format. If we received an update in BGRA format, we write that BGRA data into
-    // the RGBA GL texture without pixel format conversions, but instead use a shader problem to transparently handle
+    // the RGBA GL texture without pixel format conversions, but instead use a shader program to transparently handle
     // the color conversion on-the-fly, when painting the texture.
 #if CPU(LITTLE_ENDIAN)
     return TextureMapperFlags::ShouldConvertTextureBGRAToRGBA;

Source/WebCore/platform/graphics/texmap/BitmapTexture.h
@@ -62,6 +62,7 @@ public:
 #endif
         UseBGRALayout = 1 << 5,
         NearestFiltering = 1 << 6,
+        ExternalOESRenderTarget = 1 << 7,
     };
 
     static Ref<BitmapTexture> create(const IntSize& size, OptionSet<Flags> flags = { })
@@ -121,6 +122,8 @@ private:
     void clearIfNeeded();
     void createFboIfNeeded();
 
+    void determineRenderTargetAndBinding();
+
     GLenum textureFormat() const;
     void createTexture();
     void allocateTexture();
@@ -131,6 +134,8 @@ private:
     OptionSet<Flags> m_flags;
     IntSize m_size;
     GLuint m_id { 0 };
+    GLenum m_renderTarget { GL_TEXTURE_2D };
+    GLenum m_binding { GL_TEXTURE_BINDING_2D };
     GLuint m_fbo { 0 };
     GLuint m_depthBufferObject { 0 };
     GLuint m_stencilBufferObject { 0 };

Source/WebCore/platform/graphics/texmap/TextureMapper.cpp
@@ -936,8 +936,8 @@ void TextureMapper::drawTexturedQuadWithProgram(TextureMapperShaderProgram& prog
         glUniform1i(textureAndSampler.second, i);
 
         if (repeatWrap) {
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            glTexParameteri(target, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(target, GL_TEXTURE_WRAP_T, GL_REPEAT);
         }
     }
 
@@ -956,8 +956,8 @@ void TextureMapper::drawTexturedQuadWithProgram(TextureMapperShaderProgram& prog
     if (repeatWrap) {
         for (auto& textureAndSampler : texturesAndSamplers) {
             glBindTexture(target, textureAndSampler.first);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+            glTexParameteri(target, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+            glTexParameteri(target, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         }
     }
 }
@@ -1582,6 +1582,13 @@ void TextureMapper::drawTextureExternalOES(GLuint texture, OptionSet<TextureMapp
     flags.add(TextureMapperFlags::ShouldUseExternalOESTextureRect);
     Ref<TextureMapperShaderProgram> program = data().getShaderProgram(TextureMapperShaderProgram::Option::TextureExternalOES);
     drawTexturedQuadWithProgram(program.get(), { { texture, program->externalOESTextureLocation() } }, flags, targetRect, modelViewMatrix, opacity);
+}
+
+void TextureMapper::drawTextureExternalOESYUV(GLuint texture, OptionSet<TextureMapperFlags> flags, const FloatRect& targetRect, const TransformationMatrix& modelViewMatrix, float opacity)
+{
+    flags.add(TextureMapperFlags::ShouldUseExternalOESTextureRect);
+    Ref<TextureMapperShaderProgram> program = data().getShaderProgram(TextureMapperShaderProgram::Option::TextureExternalOESYUV);
+    drawTexturedQuadWithProgram(program.get(), { { texture, program->externalOESYUVTextureLocation() } }, flags, targetRect, modelViewMatrix, opacity);
 }
 
 Ref<TextureMapperGPUBuffer> TextureMapper::acquireBufferFromPool(size_t size, TextureMapperGPUBuffer::Type type)

Source/WebCore/platform/graphics/texmap/TextureMapper.h
@@ -86,6 +86,7 @@ public:
     void drawTextureSemiPlanarYUV(const std::array<GLuint, 2>& textures, bool uvReversed, const std::array<GLfloat, 16>& yuvToRgbMatrix, OptionSet<TextureMapperFlags>, const FloatRect& targetRect, const TransformationMatrix& modelViewMatrix, float opacity, TransferFunction, AllEdgesExposed = AllEdgesExposed::Yes);
     void drawTexturePackedYUV(GLuint texture, const std::array<GLfloat, 16>& yuvToRgbMatrix, OptionSet<TextureMapperFlags>, const FloatRect& targetRect, const TransformationMatrix& modelViewMatrix, float opacity, TransferFunction, AllEdgesExposed = AllEdgesExposed::Yes);
     void drawTextureExternalOES(GLuint texture, OptionSet<TextureMapperFlags>, const FloatRect&, const TransformationMatrix& modelViewMatrix, float opacity);
+    void drawTextureExternalOESYUV(GLuint texture, OptionSet<TextureMapperFlags>, const FloatRect&, const TransformationMatrix& modelViewMatrix, float opacity);
     void drawSolidColor(const FloatRect&, const TransformationMatrix&, const Color&, bool);
     void clearColor(const Color&);
 

commit 420f7f61cec26c35cd9a8fc78cded180f587d924

[TextureMapper] Fix fallback path when TextureExternalOESYUV disabled https://bugs.webkit.org/show_bug.cgi?id=309988
Reviewed by Philippe Normand.

This was a regression introduced with this feature in c8a6727eec001e
likely just a copy-paste mistake from OES_EGL_IMAGE_EXTERNAL_DIRECTIVE().

When disabled the fallback type for SamplerExternalOESYUVType is sampler2D.

This was reproduced on a mediatek device with libmali which does not support
these extensions. Previously textures would be black boxes.

* Source/WebCore/platform/graphics/texmap/TextureMapperShaderProgram.cpp:

Canonical link: https://commits.webkit.org/309321@main

Source/WebCore/platform/graphics/texmap/TextureMapperShaderProgram.cpp
@@ -194,12 +194,7 @@ static const char* vertexTemplateCommon =
         GLSL_DIRECTIVE(extension GL_EXT_YUV_target : require) \
         GLSL_DIRECTIVE(define SamplerExternalOESYUVType __samplerExternal2DY2YEXT) \
     GLSL_DIRECTIVE(else) \
-        GLSL_DIRECTIVE(if __VERSION__ >= 300) \
-            GLSL_DIRECTIVE(extension GL_OES_EGL_image_external_essl3 : require) \
-        GLSL_DIRECTIVE(else) \
-            GLSL_DIRECTIVE(extension GL_OES_EGL_image_external : require) \
-        GLSL_DIRECTIVE(endif) \
-        GLSL_DIRECTIVE(define SamplerExternalOESYUVType samplerExternalOES) \
+        GLSL_DIRECTIVE(define SamplerExternalOESYUVType sampler2D) \
     GLSL_DIRECTIVE(endif)
 // clang-format on
 