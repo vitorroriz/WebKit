4919a733f5ff342b5fe7c819401bd1078721e543
Anne van Kesteren <annevk@annevk.nl>
2026-03-16 06:09:16 -0700

    Rename crypto/cocoa files from *Mac to *Cocoa https://bugs.webkit.org/show_bug.cgi?id=310002
    Reviewed by Chris Dumez.
    
    A prior change had already moved these files from the mac/ to cocoa/
    folder. This removes the final bit of potential confusion about their
    scope.
    
    Canonical link: https://commits.webkit.org/309318@main

--- Source/WebCore/crypto/cocoa/CryptoAlgorithmPBKDF2Cocoa.cpp
+++ Source/WebCore/crypto/cocoa/CryptoAlgorithmPBKDF2Cocoa.cpp
@@ -32,7 +32,7 @@
 
 namespace WebCore {
 
-namespace CryptoAlgorithmPBKDF2MacInternal {
+namespace CryptoAlgorithmPBKDF2CocoaInternal {
 static CCPseudoRandomAlgorithm NODELETE commonCryptoHMACAlgorithm(CryptoAlgorithmIdentifier hashFunction)
 {
     switch (hashFunction) {
@@ -60,7 +60,7 @@ ExceptionOr<Vector<uint8_t>> CryptoAlgorithmPBKDF2::platformDeriveBits(const Cry
     // CCKeyDerivationPBKDF returns an error if the key pointer is null, even if the key length is 0. For this reason, we need to make sure we pass the empty string
     // instead of a null pointer in this case.
     auto keySpan = spanReinterpretCast<const char>(key.key().span());
-    if (CCKeyDerivationPBKDF(kCCPBKDF2, keySpan.data() ? keySpan.data() : "", key.key().size(), parameters.saltVector().span().data(), parameters.saltVector().size(), CryptoAlgorithmPBKDF2MacInternal::commonCryptoHMACAlgorithm(parameters.hashIdentifier), parameters.iterations, result.mutableSpan().data(), length / 8))
+    if (CCKeyDerivationPBKDF(kCCPBKDF2, keySpan.data() ? keySpan.data() : "", key.key().size(), parameters.saltVector().span().data(), parameters.saltVector().size(), CryptoAlgorithmPBKDF2CocoaInternal::commonCryptoHMACAlgorithm(parameters.hashIdentifier), parameters.iterations, result.mutableSpan().data(), length / 8))
         return Exception { ExceptionCode::OperationError };
     return WTF::move(result);
 }
