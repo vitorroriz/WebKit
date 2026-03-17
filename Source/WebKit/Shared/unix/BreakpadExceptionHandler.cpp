vitorroriz/WebKit

commit 2d4ca326601eb0a377673f8200349913aae83c65

Turn on thread-safe statics everywhere except bmalloc and JSC https://bugs.webkit.org/show_bug.cgi?id=303020
Reviewed by Geoffrey Garen.

Turn on thread-safe statics everywhere except bmalloc and JSC. This
results in safer & simpler code without any noticeable performance
regression on the benchmarks we track.

* Configurations/CommonBase.xcconfig:
* Source/WTF/Configurations/Base.xcconfig:
* Source/WTF/wtf/Assertions.cpp:
* Source/WTF/wtf/CryptographicallyRandomNumber.cpp:
* Source/WTF/wtf/CurrentTime.cpp:
* Source/WTF/wtf/FastMalloc.cpp:
(WTF::MallocCallTracker::singleton):
* Source/WTF/wtf/Language.cpp:
(WTF::observerMap):
* Source/WTF/wtf/Logger.cpp:
(WTF::Logger::observers):
(WTF::Logger::messageHandlerObservers):
* Source/WTF/wtf/MainThreadDispatcher.cpp:
(WTF::MainThreadDispatcher::singleton):
* Source/WTF/wtf/MemoryPressureHandler.cpp:
(WTF::MemoryPressureHandler::singleton):
* Source/WTF/wtf/MemoryPressureHandler.h:
* Source/WTF/wtf/NumberOfCores.cpp:
(WTF::numberOfPhysicalProcessorCores):
* Source/WTF/wtf/OSRandomSource.cpp:
(WTF::cryptographicallyRandomValuesFromOS):
* Source/WTF/wtf/ParkingLot.cpp:
* Source/WTF/wtf/RAMSize.cpp:
(WTF::ramSize):
* Source/WTF/wtf/RunLoop.cpp:
(WTF::RunLoop::runLoopHolder):
* Source/WTF/wtf/SequesteredImmortalHeap.cpp:
(WTF::SequesteredImmortalHeap::instance):
* Source/WTF/wtf/StatisticsManager.cpp:
(WTF::StatisticsManager::singleton):
(WTF::StatisticsManager::~StatisticsManager):
* Source/WTF/wtf/StatisticsManager.h:
* Source/WTF/wtf/Threading.cpp:
(WTF::Thread::allThreads):
(WTF::Thread::initializeInThread):
* Source/WTF/wtf/TranslatedProcess.cpp:
(WTF::isX86BinaryRunningOnARM):
* Source/WTF/wtf/URL.cpp:
(WTF::aboutBlankURL):
(WTF::aboutSrcDocURL):
* Source/WTF/wtf/URLParser.cpp:
(WTF::URLParser::internationalDomainNameTranscoder):
* Source/WTF/wtf/UUID.cpp:
(WTF::bootSessionUUIDString):
* Source/WTF/wtf/WorkQueue.cpp:
(WTF::ConcurrentWorkQueue::apply):
(WTF::WorkQueue::mainSingleton):

Source/WebKit/Shared/unix/BreakpadExceptionHandler.cpp
@@ -38,8 +38,6 @@ namespace WebKit {
 
 void installBreakpadExceptionHandler()
 {
-    static std::once_flag onceFlag;
-    static MainThreadLazyNeverDestroyed<google_breakpad::ExceptionHandler> exceptionHandler;
     static String breakpadMinidumpDir = String::fromUTF8(getenv("BREAKPAD_MINIDUMP_DIR"));
 
 #ifdef BREAKPAD_MINIDUMP_DIR
@@ -55,11 +53,10 @@ void installBreakpadExceptionHandler()
         return;
     }
 
-    std::call_once(onceFlag, []() {
-        exceptionHandler.construct(google_breakpad::MinidumpDescriptor(breakpadMinidumpDir.utf8().data()), nullptr,
-            [](const google_breakpad::MinidumpDescriptor&, void*, bool succeeded) -> bool {
-                return succeeded;
-            }, nullptr, true, -1);
+    static MainRunLoopNeverDestroyed<google_breakpad::ExceptionHandler> exceptionHandler = google_breakpad::MinidumpDescriptor(breakpadMinidumpDir.utf8().data()), nullptr,
+        [](const google_breakpad::MinidumpDescriptor&, void*, bool succeeded) -> bool {
+            return succeeded;
+        }, nullptr, true, -1);
     });
 }
 }

commit 936727f2151494a6c02c711f9b0a6a9738302e54

[WPE] Fix breakpad support broken since 303476@main https://bugs.webkit.org/show_bug.cgi?id=310098
Reviewed by Philippe Normand.

This replaces StringImpl::createFromCString with String::fromUTF8 when
getting the minidump directory from the environment, and then
MainRunLoopNeverDestroyed with NeverDestroyed, as the former asserts
when used before the run loop is created.

Tested by disabling the sandbox, as otherwise the directory access is
restricted.

* Source/WebKit/Shared/unix/BreakpadExceptionHandler.cpp:
(WebKit::installBreakpadExceptionHandler):

Canonical link: https://commits.webkit.org/309414@main

Source/WebKit/Shared/unix/BreakpadExceptionHandler.cpp
@@ -29,8 +29,6 @@
 #if ENABLE(BREAKPAD)
 
 #include <breakpad/client/linux/handler/exception_handler.h>
-#include <mutex>
-#include <signal.h>
 #include <wtf/FileSystem.h>
 #include <wtf/NeverDestroyed.h>
 
@@ -42,7 +40,7 @@ void installBreakpadExceptionHandler()
 
 #ifdef BREAKPAD_MINIDUMP_DIR
     if (breakpadMinidumpDir.isEmpty())
-        breakpadMinidumpDir = StringImpl::createFromCString(BREAKPAD_MINIDUMP_DIR);
+        breakpadMinidumpDir = String::fromUTF8(BREAKPAD_MINIDUMP_DIR);
 #endif
 
     if (breakpadMinidumpDir.isEmpty())
@@ -53,12 +51,10 @@ void installBreakpadExceptionHandler()
         return;
     }
 
-    static MainRunLoopNeverDestroyed<google_breakpad::ExceptionHandler> exceptionHandler = google_breakpad::MinidumpDescriptor(breakpadMinidumpDir.utf8().data()), nullptr,
+    static NeverDestroyed<google_breakpad::ExceptionHandler> exceptionHandler(google_breakpad::MinidumpDescriptor(breakpadMinidumpDir.utf8().data()), nullptr,
         [](const google_breakpad::MinidumpDescriptor&, void*, bool succeeded) -> bool {
             return succeeded;
         }, nullptr, true, -1);
-    });
-}
 }
 #endif
 