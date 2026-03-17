659a9e578eb17fa5e758a10bccdfce411d9ddfc8
Ahmad Saleem <ahmad.saleem792+github@gmail.com>
2026-03-17 12:48:39 -0700

    [WebVTT] Support text-decoration-{text,color,style} on CSS ::cue pseudo-element https://bugs.webkit.org/show_bug.cgi?id=118915 rdar://172728187
    Reviewed by Eric Carlson.
    
    The ::cue pseudo-element spec [1] defines that the properties
    corresponding to the text-decoration shorthand apply to ::cue.
    Previously only text-decoration-line was allowed. Add the missing
    text-decoration-color, text-decoration-style and
    text-decoration-thickness properties to the ::cue and ::cue()
    allowlists.
    
    [1] https://w3c.github.io/webvtt/#the-cue-pseudo-element
    
    * LayoutTests/media/track/track-cue-text-decoration-expected.txt: Added.
    * LayoutTests/media/track/track-cue-text-decoration.html: Added.
    * Source/WebCore/style/PropertyAllowlist.cpp:
    (WebCore::Style::isValidCueStyleProperty):
    (WebCore::Style::isValidCueSelectorStyleProperty):
    
    Canonical link: https://commits.webkit.org/309425@main

--- Source/WebCore/style/PropertyAllowlist.cpp
+++ Source/WebCore/style/PropertyAllowlist.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2025 Apple Inc. All rights reserved.
+ * Copyright (C) 2021-2026 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -141,7 +141,10 @@ bool isValidCueStyleProperty(CSSPropertyID id)
     case CSSPropertyWhiteSpace:
     case CSSPropertyWhiteSpaceCollapse:
     case CSSPropertyTextCombineUpright:
+    case CSSPropertyTextDecorationColor:
     case CSSPropertyTextDecorationLine:
+    case CSSPropertyTextDecorationStyle:
+    case CSSPropertyTextDecorationThickness:
     case CSSPropertyTextShadow:
     case CSSPropertyTextWrapMode:
     case CSSPropertyTextWrapStyle:
@@ -193,7 +196,10 @@ bool isValidCueSelectorStyleProperty(CSSPropertyID id)
     case CSSPropertyWhiteSpace:
     case CSSPropertyWhiteSpaceCollapse:
     case CSSPropertyTextCombineUpright:
+    case CSSPropertyTextDecorationColor:
     case CSSPropertyTextDecorationLine:
+    case CSSPropertyTextDecorationStyle:
+    case CSSPropertyTextDecorationThickness:
     case CSSPropertyTextShadow:
     case CSSPropertyTextWrapMode:
     case CSSPropertyTextWrapStyle:
