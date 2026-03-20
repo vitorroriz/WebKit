455735f9ee78b76dd55392499bb6693616a0a01c
Rupin Mittal <rupin@apple.com>
2026-03-20 09:16:46 -0700

    [Site Isolation] Simplify logic in LocalDOMWindow::createWindow and DOMWindow::isInsecureScriptAccess https://bugs.webkit.org/show_bug.cgi?id=310297 rdar://172931973
    Reviewed by Chris Dumez.
    
    When LocalDOMWindow::createWindow() calls DOMWindow::isInsecureScriptAccess(),
    it handles the local and remote cases separately. But that's not necessary
    because we can call isInsecureScriptAccess from a DOMWindow regardless of if
    it's local or remote.
    
    Similarly, when DOMWindow::isInsecureScriptAccess() compares the active window's
    securityOrigin with that of the target frame, it handles the local and remote
    cases separately. But that's not necessary because we can obtain the securityOrigin
    from a Frame regardless of if it's local or remote.
    
    There is no behavior change. This is tested by the two tests that were fixed by
    adding this logic initially in https://commits.webkit.org/308632@main:
    - http/tests/security/xss-DENIED-window-open-javascript-url-with-spaces.html
    - http/tests/security/xss-DENIED-window-open-javascript-url.html
    
    * Source/WebCore/page/DOMWindow.cpp:
    (WebCore::DOMWindow::isInsecureScriptAccess):
    * Source/WebCore/page/LocalDOMWindow.cpp:
    (WebCore::LocalDOMWindow::createWindow):
    
    Canonical link: https://commits.webkit.org/309624@main

--- Source/WebCore/page/DOMWindow.cpp
+++ Source/WebCore/page/DOMWindow.cpp
@@ -1022,20 +1022,12 @@ bool DOMWindow::isInsecureScriptAccess(const LocalDOMWindow& activeWindow, const
         if (&activeWindow == this)
             return false;
 
-        // FIXME: The name canAccess seems to be a roundabout way to ask "can execute script".
-        // Can we name the SecurityOrigin function better to make this more clear?
-
-        RefPtr localDocument = documentIfLocal();
-        if (localDocument && protect(protect(activeWindow.document())->securityOrigin())->isSameOriginDomain(protect(localDocument->securityOrigin())))
-            return false;
-
-        // Although remote frames are defined to host cross origin sites with site isolation,
-        // this is an implementation decision and we should still check that origins match
-        // as the HTML navigation spec describes for navigation to javascript urls
-        // https://html.spec.whatwg.org/#the-javascript:-url-special-case
-        RefPtr remoteFrame = (m_type == DOMWindowType::Remote) ? dynamicDowncast<RemoteDOMWindow>(*this)->frame() : nullptr;
-        if (remoteFrame && protect(protect(activeWindow.document())->securityOrigin())->isSameOriginDomain(remoteFrame->frameDocumentSecurityOriginOrOpaque()))
-            return false;
+        if (RefPtr frame = this->frame()) {
+            if (RefPtr securityOrigin = frame->frameDocumentSecurityOrigin()) {
+                if (protect(protect(activeWindow.document())->securityOrigin())->isSameOriginDomain(*securityOrigin))
+                    return false;
+            }
+        }
     }
 
     activeWindow.printErrorMessage(crossDomainAccessErrorMessage(activeWindow, IncludeTargetOrigin::Yes));
--- Source/WebCore/page/LocalDOMWindow.cpp
+++ Source/WebCore/page/LocalDOMWindow.cpp
@@ -120,7 +120,6 @@
 #include "PseudoElementUtilities.h"
 #include "PushManager.h"
 #include "PushStrategy.h"
-#include "RemoteDOMWindow.h"
 #include "RemoteFrame.h"
 #include "RequestAnimationFrameCallback.h"
 #include "ResourceLoadInfo.h"
@@ -2865,15 +2864,11 @@ ExceptionOr<RefPtr<Frame>> LocalDOMWindow::createWindow(const String& urlString,
         }
     }
 
+    RefPtr window = newFrame->window();
+    if (window && window->isInsecureScriptAccess(activeWindow, completedURL.string()))
+        return noopener ? RefPtr<Frame> { nullptr } : newFrame;
+
     RefPtr localNewFrame = dynamicDowncast<LocalFrame>(newFrame);
-    if (localNewFrame && protect(localNewFrame->document()->window())->isInsecureScriptAccess(activeWindow, completedURL.string()))
-        return noopener ? RefPtr<Frame> { nullptr } : newFrame;
-
-    // For when calling window.open and providing a target window which is already remote
-    RefPtr remoteNewFrame = dynamicDowncast<RemoteFrame>(newFrame);
-    if (remoteNewFrame && protect(remoteNewFrame->window())->isInsecureScriptAccess(activeWindow, completedURL.string()))
-        return noopener ? RefPtr<Frame> { nullptr } : newFrame;
-
     if (prepareDialogFunction && localNewFrame)
         prepareDialogFunction(*protect(localNewFrame->document()->window()));
 
