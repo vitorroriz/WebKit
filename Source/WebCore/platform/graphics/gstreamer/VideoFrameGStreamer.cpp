a88b1835f0c7afe24f19e71af6ea7cca3142cb2d
Mike Gorse <mgorse@suse.com>
2026-03-24 12:58:53 -0700

    Build failure with GSTREAMER_GL disabled https://bugs.webkit.org/show_bug.cgi?id=310584
    Reviewed by Philippe Normand.
    
    VideoFrameGStreamer::MemoryType::GL can be undefined if GSTREAMER_GL is off.
    
    * Source/WebCore/platform/graphics/gstreamer/VideoFrameGStreamer.cpp:
    (WebCore::VideoFrame::copyTo):
    
    Canonical link: https://commits.webkit.org/309859@main

--- Source/WebCore/platform/graphics/gstreamer/VideoFrameGStreamer.cpp
+++ Source/WebCore/platform/graphics/gstreamer/VideoFrameGStreamer.cpp
@@ -586,9 +586,11 @@ void VideoFrame::copyTo(std::span<uint8_t> destination, VideoPixelFormat pixelFo
     case VideoFrameGStreamer::MemoryType::System:
         inputSample = self.sample();
         break;
+#if USE(GSTREAMER_GL)
     case VideoFrameGStreamer::MemoryType::GL:
 #if USE(GBM)
     case VideoFrameGStreamer::MemoryType::DMABuf:
+#endif
 #endif
         inputSample = self.downloadSample();
         break;
