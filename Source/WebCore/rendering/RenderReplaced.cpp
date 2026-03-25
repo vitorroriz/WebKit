c29e2327fadd2c4a6a0042a1e4f1406de102a0b3
Commit Queue <commit-queue@webkit.org>
2026-03-25 04:36:54 -0700

    Unreviewed, reverting 307751@main. https://bugs.webkit.org/show_bug.cgi?id=310667
    Broke video sizing on <http://vimeo.com|vimeo.com>
    
    Reverted changeset:
    
    "[Render Replaced][youtube.com] Resolve percentage height for aspect ratio adjusted widths."
    https://bugs.webkit.org/show_bug.cgi?id=308080
    https://commits.webkit.org/307751@main
    
    * LayoutTests/TestExpectations:
    * LayoutTests/fast/replaced/image-percent-sizing-in-inline-block-expected.html: Removed.
    * LayoutTests/fast/replaced/image-percent-sizing-in-inline-block.html: Removed.
    * Source/WebCore/rendering/RenderReplaced.cpp:
    (WebCore::RenderReplaced::computeAspectRatioAdjustedIntrinsicLogicalWidths const):
    
    Canonical link: https://commits.webkit.org/309905@main

--- Source/WebCore/rendering/RenderReplaced.cpp
+++ Source/WebCore/rendering/RenderReplaced.cpp
@@ -672,26 +672,14 @@ void RenderReplaced::computeAspectRatioAdjustedIntrinsicLogicalWidths(LayoutUnit
     auto computedAspectRatio = computeIntrinsicAspectRatio();
     auto computedIntrinsicLogicalWidth = minLogicalWidth;
 
-    auto resolveHeightForAspectRatio = [&](auto& length, bool canResolvePercentage) -> std::optional<LayoutUnit> {
-        if (auto fixedHeight = length.tryFixed())
-            return LayoutUnit { fixedHeight->resolveZoom(style.usedZoomForLength()) };
+    if (auto fixedLogicalHeight = style.logicalHeight().tryFixed())
+        computedIntrinsicLogicalWidth = fixedLogicalHeight->resolveZoom(style.usedZoomForLength()) * computedAspectRatio;
 
-        if (length.isPercentOrCalculated() && canResolvePercentage)
-            return computePercentageLogicalHeight(length, UpdatePercentageHeightDescendants::No);
-        return std::nullopt;
-    };
+    if (auto fixedLogicalMaxHeight = style.logicalMaxHeight().tryFixed())
+        computedIntrinsicLogicalWidth = std::min(computedIntrinsicLogicalWidth, LayoutUnit { fixedLogicalMaxHeight->resolveZoom(style.usedZoomForLength()) * computedAspectRatio });
 
-    // Resolve height and apply aspect ratio if available
-    if (auto resolvedLogicalHeight = resolveHeightForAspectRatio(style.logicalHeight(), hasReplacedLogicalHeight()))
-        computedIntrinsicLogicalWidth = *resolvedLogicalHeight * computedAspectRatio;
-
-    // Apply max-height constraint
-    if (auto resolvedLogicalMaxHeight = resolveHeightForAspectRatio(style.logicalMaxHeight(), !replacedMaxLogicalHeightComputesAsNone()))
-        computedIntrinsicLogicalWidth = std::min(computedIntrinsicLogicalWidth, LayoutUnit { *resolvedLogicalMaxHeight * computedAspectRatio });
-
-    // Apply min-height constraint
-    if (auto resolvedLogicalMinHeight = resolveHeightForAspectRatio(style.logicalMinHeight(), !replacedMinLogicalHeightComputesAsNone()))
-        computedIntrinsicLogicalWidth = std::max(computedIntrinsicLogicalWidth, LayoutUnit { *resolvedLogicalMinHeight * computedAspectRatio });
+    if (auto fixedLogicalMinHeight = style.logicalMinHeight().tryFixed())
+        computedIntrinsicLogicalWidth = std::max(computedIntrinsicLogicalWidth, LayoutUnit { fixedLogicalMinHeight->resolveZoom(style.usedZoomForLength()) * computedAspectRatio });
 
     minLogicalWidth = computedIntrinsicLogicalWidth;
     maxLogicalWidth = minLogicalWidth;
