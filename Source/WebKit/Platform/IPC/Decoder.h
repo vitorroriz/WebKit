65303ca72a39799147a7d13b681fc34139ea4d52
Chris Dumez <cdumez@apple.com>
2026-03-25 13:44:05 -0700

    IPC::Decoder::markInvalid() should not leave the m_bufferPosition iterator dangling https://bugs.webkit.org/show_bug.cgi?id=310696
    Reviewed by Kimmo Kinnunen.
    
    IPC::Decoder::markInvalid() clears m_buffer and fails to reset m_bufferPosition
    which is an iterator into m_buffer.
    
    * Source/WebKit/Platform/IPC/Decoder.h:
    (IPC::Decoder::markInvalid):
    
    Canonical link: https://commits.webkit.org/309940@main

--- Source/WebKit/Platform/IPC/Decoder.h
+++ Source/WebKit/Platform/IPC/Decoder.h
@@ -126,6 +126,7 @@ public:
     void markInvalid()
     {
         auto buffer = std::exchange(m_buffer, { });
+        m_bufferPosition = m_buffer.begin();
         if (m_bufferDeallocator && !buffer.empty())
             m_bufferDeallocator(WTF::move(buffer));
     }
