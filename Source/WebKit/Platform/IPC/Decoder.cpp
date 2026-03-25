7074c1511c7d230eb442368a12892e2e1056b4ad
Chris Dumez <cdumez@apple.com>
2026-03-25 04:37:29 -0700

    Drop outdated comment in IPC::~Decoder() https://bugs.webkit.org/show_bug.cgi?id=310702
    Reviewed by Kimmo Kinnunen.
    
    When a Decoder is destroyed, m_attachments (a Vector<Attachment>) is
    destroyed, which properly cleans up all remaining attachments via their
    RAII destructors.
    
    The Attachment type aliases to RAII types on all platforms:
    - Darwin: MachSendRight — destructor calls deallocateSendRightSafely(m_port)
      which calls mach_port_deallocate()
    - Unix: UnixFileDescriptor — destructor calls closeWithRetry()
    - Android: Variant<UnixFileDescriptor, RefPtr<AHardwareBuffer>> — both
      properly release
    
    * Source/WebKit/Platform/IPC/Decoder.cpp:
    (IPC::Decoder::~Decoder):
    
    Canonical link: https://commits.webkit.org/309906@main

--- Source/WebKit/Platform/IPC/Decoder.cpp
+++ Source/WebKit/Platform/IPC/Decoder.cpp
@@ -137,7 +137,6 @@ Decoder::~Decoder()
 {
     if (isValid())
         markInvalid();
-    // FIXME: We need to dispose of the mach ports in cases of failure.
 }
 
 ShouldDispatchWhenWaitingForSyncReply Decoder::shouldDispatchMessageWhenWaitingForSyncReply() const
