52682225d4ce7a78cc312ee3fcfaad83953b9fb1
Anne van Kesteren <annevk@annevk.nl>
2026-03-26 07:57:17 -0700

    Address potential race in HTMLFrameElementBase::openURL() https://bugs.webkit.org/show_bug.cgi?id=310792
    Reviewed by Ryosuke Niwa.
    
    m_frameURL can have changed by the time the lambda runs. We already
    capture and use completeURL so we should use that here as well.
    
    Canonical link: https://commits.webkit.org/309991@main

--- Source/WebCore/html/HTMLFrameElementBase.cpp
+++ Source/WebCore/html/HTMLFrameElementBase.cpp
@@ -118,7 +118,7 @@ void HTMLFrameElementBase::openURL(LockHistory lockHistory, LockBackForwardList 
         }
 
         protect(protectedThis->document())->willLoadFrameElement(completeURL);
-        parentFrame->loader().subframeLoader().requestFrame(*protectedThis, protectedThis->m_frameURL, frameName, lockHistory, lockBackForwardList);
+        parentFrame->loader().subframeLoader().requestFrame(*protectedThis, completeURL.string(), frameName, lockHistory, lockBackForwardList);
     };
 
     document->quirks().triggerOptionalStorageAccessIframeQuirk(completeURL, WTF::move(finishOpeningURL));
